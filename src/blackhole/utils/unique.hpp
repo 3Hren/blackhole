7225b101205b2e55d2ba95923b1f7878caa1bdbd
Evgeny Safronov <division494@gmail.com>
2013-12-03 13:38:28 +0400

    Implementation of std::make_unique

---
+++ src/blackhole/utils/unique.hpp
@@ -0,0 +1,12 @@
+#pragma once
+
+#include <memory>
+
+namespace std {
+
+template<typename T, typename... Args>
+std::unique_ptr<T> make_unique(Args&&... args) {
+    return std::unique_ptr<T>(new T(std::forward<Args>(args)...));
+}
+
+}
