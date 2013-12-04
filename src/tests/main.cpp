c888aea004ed15389f16bdfe8f35cb17d52770de
Evgeny Safronov <division494@gmail.com>
2013-12-04 18:44:45 +0400

    Benchmark has been updated.

--- src/tests/main.cpp
+++ src/tests/main.cpp
@@ -1,5 +1,7 @@
 #include "Global.hpp"
 
+#include <memory>
+
 #if 1
 int main(int argc, char** argv) {
     InitGoogleTest(&argc, argv);
@@ -8,18 +10,33 @@ int main(int argc, char** argv) {
 #else
 #include "celero/Celero.h"
 
-const int N = 10000;
+using namespace blackhole;
 
-CELERO_MAIN
-BASELINE(CeleroBenchTest, Baseline, 100, N) {
+const int N = 100000;
+
+//CELERO_MAIN
+
+logger_base_t log_;
+
+int main(int argc, char** argv) {
+    auto f = std::make_unique<formatter::string_t>("[%(timestamp_id)s]: %(message)s");
+    auto s = std::make_unique<sink::file_t<>>("test.log");
+    auto frontend = std::make_unique<frontend_t<formatter::string_t, sink::file_t<>>>(std::move(f), std::move(s));
+    log_.add_frontend(std::move(frontend));
+    celero::Run(argc, argv); return 0;
+}
+
+
+BASELINE(CeleroBenchTest, Baseline, 10, N) {
     (boost::format("Le message: %s") % "le value").str();
 }
 
-using namespace blackhole;
-
-log::
-
-BENCHMARK(CeleroBenchTest, Benchmark, 100, N) {
+BENCHMARK(CeleroBenchTest, Benchmark, 10, N) {
+    auto record = log_.open_record();
+    if (record.valid()) {
+        record.attributes["message"] = utils::format("Le message: %s", "le value");
+        log_.push(std::move(record));
+    }
 }
 
 #endif
