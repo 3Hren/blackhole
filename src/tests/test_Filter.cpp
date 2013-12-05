9773e5e42502f39b849d0cd0f808f42ec070f184
Evgeny Safronov <division494@gmail.com>
2013-12-05 18:11:49 +0400

    Attribute getter tests has been written. Also minor renaming.

--- src/tests/test_Filter.cpp
+++ src/tests/test_Filter.cpp
@@ -1,25 +1,47 @@
 #include "Mocks.hpp"
 
-TEST(Filter, PassesIfHasAttribute) {
+TEST(HasAttribute, ReturnsTrueIfDynamicAttributeExists) {
     auto filter = expr::has_attr<std::int32_t>("custom");
     log::attributes_t attributes = {{"custom", 42}};
     EXPECT_TRUE(filter(attributes));
 }
 
-TEST(Filter, FailsIfHasNotAttribute) {
+TEST(HasAttribute, ReturnsFalseIfDynamicAttributeNotExists) {
     auto filter = expr::has_attr<std::int32_t>("non-existing-attribute");
     log::attributes_t attributes = {{"custom", 42}};
     EXPECT_FALSE(filter(attributes));
 }
 
-TEST(Filter, FailsIfHasAttributeButTypeMismatch) {
+TEST(HasAttribute, ReturnsFalseIfDynamicAttributeExistsButTypeMismatched) {
     auto filter = expr::has_attr<std::int32_t>("custom");
     log::attributes_t attributes = {{"custom", 3.1415}};
     EXPECT_FALSE(filter(attributes));
 }
 
-TEST(Filter, FailsIfBothHasNotAttributeAndTypeMismatch) {
+TEST(HasAttribute, ReturnsTrueIfBothDynamicAttributeNotExistsAndTypeMismatched) {
     auto filter = expr::has_attr<std::int32_t>("non-existing-attribute");
     log::attributes_t attributes = {{"custom", 3.1415}};
     EXPECT_FALSE(filter(attributes));
+}
+
+TEST(GetAttribute, CanExtractAttribute) {
+    auto filter = expr::get_attr<std::int32_t>("custom");
+    log::attributes_t attributes = {{"custom", 42}};
+    EXPECT_EQ(42, filter(attributes));
+}
+
+TEST(GetAttribute, ThrowsExceptionIfCannotExtractAttribute) {
+    /*! @note: It's okay that `get_attr` throws exceptions, cause there is no correct value to be
+               returned if there is no value actually or its type not equal with expected type.
+               You must check `has_attr<T>()` before extracting it.
+               If not, an exception will be thrown and caught inside log core. */
+    auto filter = expr::get_attr<std::int32_t>("non-existing-attribute");
+    log::attributes_t attributes = {{"custom", 42}};
+    EXPECT_THROW(filter(attributes), std::logic_error);
+}
+
+TEST(GetAttribute, ThrowsExceptionIfAttributeHasTypedMismatch) {
+    auto filter = expr::get_attr<std::int32_t>("custom");
+    log::attributes_t attributes = {{"custom", 3.1415}};
+    EXPECT_THROW(filter(attributes), boost::bad_get);
 }
