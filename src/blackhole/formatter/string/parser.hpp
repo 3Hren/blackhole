37bd12e645f2c220561820d6dc2508507d3266bf
Evgeny Safronov <division494@gmail.com>
2013-12-06 13:46:40 +0400

    Pattern parsing class has been simplified for understanding.

--- src/blackhole/formatter/string/parser.hpp
+++ src/blackhole/formatter/string/parser.hpp
@@ -1,5 +1,7 @@
 #pragma once
 
+#include <boost/algorithm/string.hpp>
+
 #include "config.hpp"
 
 namespace blackhole {
@@ -8,48 +10,65 @@ namespace formatter {
 
 namespace string {
 
-struct pattern_parser_t {
-    static config_t parse(const std::string& pattern) {
-        auto current = pattern.begin();
-        auto end = pattern.end();
+static const char VARIADIC_KEY_PREFIX[] = "...";
 
-        std::string fpattern;
-        fpattern.reserve(pattern.length());
-        std::vector<std::string> attribute_names;
+struct pattern_parser_t {
+    static config_t parse(const std::string& input_pattern) {
+        auto current = input_pattern.begin();
+        auto end = input_pattern.end();
+
+        std::string pattern;
+        pattern.reserve(input_pattern.length());
+        std::vector<std::string> keys;
 
         while(current != end) {
-            if ((*current == '%') && (current + 1 != end) && (*(current + 1) == '(')) {
-                fpattern.push_back('%');
+            if (begin_key(current, end)) {
                 current += 2;
+                pattern.push_back('%');
                 std::string key;
                 while (current != end) {
-                    if ((*current == ')') && (current + 1 != end) && (*(current + 1) == 's')) {
+                    if (end_key(current, end)) {
                         break;
                     } else {
                         key.push_back(*current);
                     }
                     current++;
                 }
-                if (boost::starts_with(key, "...")) {
-                    for (auto it = key.begin() + 3; it != key.end(); ++it) {
-                        char ch = *it;
-                        switch (ch) {
-                        case 'L':
-                            *it = '0';
-                            break;
-                        default:
-                            *it = '0';
-                        }
-                    }
+
+                if (boost::starts_with(key, VARIADIC_KEY_PREFIX)) {
+                    handle_variadic_key(&key);
                 }
-                attribute_names.push_back(key);
+                keys.push_back(key);
             } else {
-                fpattern.push_back(*current);
+                pattern.push_back(*current);
             }
+
             current++;
         }
 
-        return { fpattern, attribute_names };
+        return { pattern, keys };
+    }
+
+private:
+    static inline bool begin_key(std::string::const_iterator it, std::string::const_iterator end) {
+        return (*it == '%') && (it + 1 != end) && (*(it + 1) == '(');
+    }
+
+    static inline bool end_key(std::string::const_iterator it, std::string::const_iterator end) {
+        return (*it == ')') && (it + 1 != end) && (*(it + 1) == 's');
+    }
+
+    static void handle_variadic_key(std::string* key) {
+        for (auto it = key->begin() + 3; it != key->end(); ++it) {
+            char ch = *it;
+            switch (ch) {
+            case 'L':
+                *it = '0';
+                break;
+            default:
+                *it = '0';
+            }
+        }
     }
 };
 
