0e7a5e7575ed8fe2c7d2144358f6d3e2252da8f0
Evgeny Safronov <division494@gmail.com>
2013-12-09 21:16:39 +0400

    Some comments has been added.

--- src/tests/Mocks.hpp
+++ src/tests/Mocks.hpp
@@ -68,10 +68,18 @@ namespace socket {
 
 class backend_t {
 public:
-    backend_t(const std::string&, std::uint16_t) {
-    }
+    backend_t(const std::string&, std::uint16_t) {}
 
     MOCK_CONST_METHOD1(write, ssize_t(const std::string&));
+};
+
+class failing_backend_t : public backend_t {
+public:
+    failing_backend_t(const std::string& host, std::uint16_t port) :
+        backend_t(host, port)
+    {
+        throw std::exception();
+    }
 };
 
 } // namespace socket
--- src/tests/test_UdpSink.cpp
+++ src/tests/test_UdpSink.cpp
@@ -115,6 +115,11 @@ TEST(socket_t, TestCanSendMessages) {
 }
 
 TEST(socket_t, ThrowsExceptionOnAnyWriteErrorOccurred) {
+    // This behaviour is normal for blocking udp/tcp socket sink.
+    // When some network error occurs, message is on the half way to be dropped.
+    // In case of UDP socket, handler will try to reopen socket immediately and rewrite again here
+    // and every next message.
+    // In case of TCP socket, handler will try to reconnect on every next message before sending.
     sink::socket_t<NiceMock<mock::socket::backend_t>> sink("localhost", 50030);
     EXPECT_CALL(sink.backend(), write(_))
             .Times(1)
@@ -122,6 +127,9 @@ TEST(socket_t, ThrowsExceptionOnAnyWriteErrorOccurred) {
     EXPECT_THROW(sink.consume("message"), std::exception);
 }
 
-//!@todo: DoSomethingIfCannotCreateSocket
-//!@todo: DoSomethingIfCannotConnect
-//!@todo: DoSomethingOnWriteError
+TEST(socket_t, ThrowsExceptionWhenCannotAcquireResource) {
+    // This is initialization behaviour and cannot be caught by any log backend. If handler cannot
+    // acquire resource needed, it can't continue its work, so it's neccessary to notify upper level
+    // code about it.
+    EXPECT_THROW(sink::socket_t<NiceMock<mock::socket::failing_backend_t>>("localhost", 50030), std::exception); //!@todo: Maybe some kind of typecheck here?
+}
