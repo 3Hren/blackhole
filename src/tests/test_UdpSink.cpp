4406331f9cbaececba734fbace4a9aa2e923cc74
Evgeny Safronov <division494@gmail.com>
2013-12-10 19:05:49 +0400

    Manual tests moved to the separate file.

--- src/tests/test_UdpSink.cpp
+++ src/tests/test_UdpSink.cpp
@@ -33,39 +33,3 @@ TEST(socket_t, ThrowsExceptionWhenCannotAcquireResource) {
     EXPECT_THROW(socket_t("localhost", 50030), std::exception); //!@todo: Maybe some kind of typecheck here?
 }
 
-//#define UDP_MANUAL
-//#define TCP_MANUAL
-
-#ifdef TCP_MANUAL
-TEST(socket_t, ManualTcp) {
-    sink::socket_t<boost::asio::ip::tcp> sink("localhost", 50030);
-    int i = 0;
-    while (true) {
-        try {
-            sink.consume(utils::format("{\"@message\": \"value = %d\"}\n", i));
-        } catch (std::exception& e) {
-            std::cout << utils::format("I've fucked up: %s", e.what()) << std::endl;
-        }
-
-        i++;
-        usleep(1000000);
-    }
-}
-#endif
-
-#ifdef UDP_MANUAL
-TEST(socket_t, ManualUdp) {
-    sink::socket_t<boost::asio::ip::udp> sink("localhost", 50030);
-    int i = 0;
-    while (true) {
-        try {
-            sink.consume(utils::format("{\"@message\": \"value = %d\"}\n", i));
-        } catch (std::exception& e) {
-            std::cout << utils::format("I've fucked up: %s", e.what()) << std::endl;
-        }
-
-        i++;
-        usleep(1000000);
-    }
-}
-#endif
