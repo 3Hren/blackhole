cd06964c3ff263e5defe4468846a78bef09b9cb9
Evgeny Safronov <division494@gmail.com>
2013-12-10 19:43:03 +0400

    Specification update.

--- src/tests/Mocks.hpp
+++ src/tests/Mocks.hpp
@@ -2,45 +2,6 @@
 
 #include "Global.hpp"
 
-//logger &log;
-
-//log.debug("message: %s", "str",
-//          keyword::verbosity=5);
-
-////pattern="[%(timestamp)s] [%(level)s]: %(message)s%(...other)s"
-////[012391232139] [debug] bla: 12313, name: "foo", ...
-
-
-//LOG(log, debug, "le message: %s", "just some message")(
-//    tag::groups(100500),
-//    tag::http::code(404),
-//    { "groups", 100500 }
-//);
-
-
-///* verbosity: info
-// * sink: file
-// *   path: [/]
-// *   format: string
-// *     pattern: [%(...)s] ...
-// *
-// * sink: tcp/udp
-// *   address: 0.0.0.0
-// *   port: 5000
-// *   format: msgpack/json
-// *
-// * sink: syslog
-// *   identity: blah
-// *   format: string
-// *     pattern: [%(...)s] ...
-// */
-
-//! Python
-// log.debug("message from '%s'", "Hell", { "owner": "Satan", "email": "satan@mail.hell" })
-
-
-//auto core = logging::core::instance();
-
 using namespace blackhole;
 
 namespace mock {
