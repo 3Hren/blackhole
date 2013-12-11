5377fa9340dd58a347b9b9a7e3362a7ee24c9610
Evgeny Safronov <division494@gmail.com>
2013-12-11 18:05:44 +0400

    Hiding testing namespace.

--- src/tests/test_SyslogSink.cpp
+++ src/tests/test_SyslogSink.cpp
@@ -1,21 +1,25 @@
 #include "Mocks.hpp"
 
+namespace testing {
+
 enum class level : std::uint8_t { debug, info, warn, error };
+
+} // namespace testing
 
 namespace blackhole { namespace sink {
 
 template<>
-struct priority_traits<level> {
+struct priority_traits<testing::level> {
     static inline
-    priority_t map(level lvl) {
+    priority_t map(testing::level lvl) {
         switch (lvl) {
-        case level::debug:
+        case testing::level::debug:
             return priority_t::debug;
-        case level::info:
+        case testing::level::info:
             return priority_t::info;
-        case level::warn:
+        case testing::level::warn:
             return priority_t::warning;
-        case level::error:
+        case testing::level::error:
             return priority_t::err;
         }
     }
@@ -24,25 +28,25 @@ struct priority_traits<level> {
 } } // namespace blackhole::sink
 
 TEST(syslog_t, Class) {
-    sink::syslog_t<level> sink("identity");
+    sink::syslog_t<testing::level> sink("identity");
     UNUSED(sink);
 }
 
 TEST(syslog_t, ConsumeAcceptsLevel) {
-    sink::syslog_t<level> sink("identity");
-    sink.consume(level::debug, "le message");
+    sink::syslog_t<testing::level> sink("identity");
+    sink.consume(testing::level::debug, "le message");
 }
 
 TEST(syslog_t, MapsLevelViaCustomMapper) {
-    sink::syslog_t<level, mock::syslog::backend_t> sink("identity");
+    sink::syslog_t<testing::level, mock::syslog::backend_t> sink("identity");
     InSequence s;
     EXPECT_CALL(sink.backend(), write(sink::priority_t::debug, std::string("debug message")));
     EXPECT_CALL(sink.backend(), write(sink::priority_t::info, std::string("info message")));
     EXPECT_CALL(sink.backend(), write(sink::priority_t::warning, std::string("warn message")));
     EXPECT_CALL(sink.backend(), write(sink::priority_t::err, std::string("error message")));
 
-    sink.consume(level::debug, "debug message");
-    sink.consume(level::info, "info message");
-    sink.consume(level::warn, "warn message");
-    sink.consume(level::error, "error message");
+    sink.consume(testing::level::debug, "debug message");
+    sink.consume(testing::level::info, "info message");
+    sink.consume(testing::level::warn, "warn message");
+    sink.consume(testing::level::error, "error message");
 }
