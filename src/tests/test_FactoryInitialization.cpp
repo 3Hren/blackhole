38bb76234f353b805116c827f560bf17aad7e5e2
Evgeny Safronov <division494@gmail.com>
2013-12-11 21:38:53 +0400

    Logging frontend factory functions is now static methods of `factory_t` class.
    This change makes code a bit cleaner.

--- src/tests/test_FactoryInitialization.cpp
+++ src/tests/test_FactoryInitialization.cpp
@@ -39,56 +39,57 @@ struct sink_config_t {
     std::map<std::string, std::string> args;
 };
 
-namespace factory {
-
-template<typename Level, typename Formatter, typename Sink>
-std::unique_ptr<base_frontend_t>
-create(std::unique_ptr<Formatter> formatter, std::unique_ptr<Sink> sink) {
-    return std::make_unique<frontend_t<Formatter, Sink, Level>>(std::move(formatter), std::move(sink));
-}
-
-template<typename Level, typename Sink>
-std::unique_ptr<base_frontend_t>
-create(const formatter_config_t& formatter_config, std::unique_ptr<Sink> sink) {
-    if (formatter_config.type == "string") {
-        std::string pattern = formatter_config.args.at("pattern");
-        auto formatter = std::make_unique<formatter::string_t>(pattern);
-        return create<Level>(std::move(formatter), std::move(sink));
-    }
-
-    return std::unique_ptr<base_frontend_t>();
-}
-
 template<typename Level>
-std::unique_ptr<base_frontend_t>
-create(const formatter_config_t& formatter_config, const sink_config_t& sink_config) {
-    if (sink_config.type == "files") {
-        std::string path = sink_config.args.at("path");
-        auto sink = std::make_unique<sink::file_t<>>(path);
-        return create<Level>(formatter_config, std::move(sink));
-    } else if (sink_config.type == "syslog") {
-        std::string identity = sink_config.args.at("identity");
-        auto sink = std::make_unique<sink::syslog_t<Level>>(identity);
-        return create<Level>(formatter_config, std::move(sink));
-    } else if (sink_config.type == "socket") {
-        std::string type = sink_config.args.at("type");
-        if (type == "udp") {
-            std::string host = sink_config.args.at("host");
-            std::uint16_t port = boost::lexical_cast<std::uint16_t>(sink_config.args.at("port"));
-            auto sink = std::make_unique<sink::socket_t<boost::asio::ip::udp>>(host, port);
-            return create<Level>(formatter_config, std::move(sink));
-        } else if (type == "tcp") {
-            std::string host = sink_config.args.at("host");
-            std::uint16_t port = boost::lexical_cast<std::uint16_t>(sink_config.args.at("port"));
-            auto sink = std::make_unique<sink::socket_t<boost::asio::ip::tcp>>(host, port);
-            return create<Level>(formatter_config, std::move(sink));
-        }
+struct factory_t {
+    template<typename Formatter, typename Sink>
+    static
+    std::unique_ptr<base_frontend_t>
+    create(std::unique_ptr<Formatter> formatter, std::unique_ptr<Sink> sink) {
+        return std::make_unique<frontend_t<Formatter, Sink, Level>>(std::move(formatter), std::move(sink));
     }
 
-    return std::unique_ptr<base_frontend_t>();
-}
+    template<typename Sink>
+    static
+    std::unique_ptr<base_frontend_t>
+    create(const formatter_config_t& formatter_config, std::unique_ptr<Sink> sink) {
+        if (formatter_config.type == "string") {
+            std::string pattern = formatter_config.args.at("pattern");
+            auto formatter = std::make_unique<formatter::string_t>(pattern);
+            return create(std::move(formatter), std::move(sink));
+        }
 
-} // namespace factory
+        return std::unique_ptr<base_frontend_t>();
+    }
+
+    static
+    std::unique_ptr<base_frontend_t>
+    create(const formatter_config_t& formatter_config, const sink_config_t& sink_config) {
+        if (sink_config.type == "files") {
+            std::string path = sink_config.args.at("path");
+            auto sink = std::make_unique<sink::file_t<>>(path);
+            return create(formatter_config, std::move(sink));
+        } else if (sink_config.type == "syslog") {
+            std::string identity = sink_config.args.at("identity");
+            auto sink = std::make_unique<sink::syslog_t<Level>>(identity);
+            return create(formatter_config, std::move(sink));
+        } else if (sink_config.type == "socket") {
+            std::string type = sink_config.args.at("type");
+            if (type == "udp") {
+                std::string host = sink_config.args.at("host");
+                std::uint16_t port = boost::lexical_cast<std::uint16_t>(sink_config.args.at("port"));
+                auto sink = std::make_unique<sink::socket_t<boost::asio::ip::udp>>(host, port);
+                return create(formatter_config, std::move(sink));
+            } else if (type == "tcp") {
+                std::string host = sink_config.args.at("host");
+                std::uint16_t port = boost::lexical_cast<std::uint16_t>(sink_config.args.at("port"));
+                auto sink = std::make_unique<sink::socket_t<boost::asio::ip::tcp>>(host, port);
+                return create(formatter_config, std::move(sink));
+            }
+        }
+
+        return std::unique_ptr<base_frontend_t>();
+    }
+};
 
 } // namespace blackhole
 
@@ -103,7 +104,7 @@ TEST(Factory, FileStringsFrontend) {
         { { "path", "/dev/stdout" } }
     };
 
-    EXPECT_TRUE(bool(factory::create<testing::level>(formatter, sink)));
+    EXPECT_TRUE(bool(factory_t<testing::level>::create(formatter, sink)));
 }
 
 TEST(Factory, SyslogStringsFrontend) {
@@ -117,7 +118,7 @@ TEST(Factory, SyslogStringsFrontend) {
         { { "identity", "AppIdentity" } }
     };
 
-    EXPECT_TRUE(bool(factory::create<testing::level>(formatter, sink)));
+    EXPECT_TRUE(bool(factory_t<testing::level>::create(formatter, sink)));
 }
 
 TEST(Factory, UdpSocketStringsFrontend) {
@@ -134,7 +135,7 @@ TEST(Factory, UdpSocketStringsFrontend) {
         }
     };
 
-    EXPECT_TRUE(bool(factory::create<testing::level>(formatter, sink)));
+    EXPECT_TRUE(bool(factory_t<testing::level>::create(formatter, sink)));
 }
 
 TEST(Factory, TcpSocketStringsFrontend) {
@@ -151,5 +152,5 @@ TEST(Factory, TcpSocketStringsFrontend) {
         }
     };
 
-    EXPECT_TRUE(bool(factory::create<testing::level>(formatter, sink)));
+    EXPECT_TRUE(bool(factory_t<testing::level>::create(formatter, sink)));
 }
