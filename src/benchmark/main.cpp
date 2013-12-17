94b0340a1c920a7eafe50fef93eb24d29012e3a8
Evgeny Safronov <division494@gmail.com>
2013-12-17 20:19:36 +0400

    Added benchmark comparing with boost::log.
    Sadly, it is 1.4 times slower :(. Some optimization needed.

--- src/benchmark/main.cpp
+++ src/benchmark/main.cpp
@@ -8,10 +8,81 @@ using namespace blackhole;
 
 #define VS_BOOST
 #ifdef VS_BOOST
+#include <boost/log/utility/setup.hpp>
+#include <boost/log/sources/logger.hpp>
+#include <boost/log/sources/record_ostream.hpp>
+#include <boost/log/sources/severity_logger.hpp>
+#include <boost/log/expressions/formatters.hpp>
 
-const int N = 100000;
+#include <fstream>
+#include <boost/shared_ptr.hpp>
+#include <boost/make_shared.hpp>
+#include <boost/log/core.hpp>
+#include <boost/log/trivial.hpp>
+#include <boost/log/expressions.hpp>
+#include <boost/log/sinks/sync_frontend.hpp>
+#include <boost/log/sinks/text_ostream_backend.hpp>
+#include <boost/log/sources/severity_logger.hpp>
+#include <boost/log/sources/record_ostream.hpp>
+#include <boost/log/utility/setup/common_attributes.hpp>
+#include <boost/log/support/date_time.hpp>
 
-logger_base_t log_;
+namespace logging = boost::log;
+namespace src = boost::log::sources;
+namespace expr = boost::log::expressions;
+namespace sinks = boost::log::sinks;
+namespace attrs = boost::log::attributes;
+namespace keywords = boost::log::keywords;
+
+enum severity_level {
+    debug,
+    info,
+    warning,
+    error,
+    critical
+};
+
+BOOST_LOG_ATTRIBUTE_KEYWORD(severity, "Severity", severity_level);
+BOOST_LOG_ATTRIBUTE_KEYWORD(timestamp, "TimeStamp", boost::posix_time::ptime);
+
+std::ostream& operator<< (std::ostream& strm, severity_level level) {
+    static const char* strings[] = {
+        "DEBUG",
+        "INFO",
+        "WARNING",
+        "ERROR",
+        "CRITICAL"
+    };
+
+    if (static_cast< std::size_t >(level) < sizeof(strings) / sizeof(*strings))
+        strm << strings[level];
+    else
+        strm << static_cast<int>(level);
+
+    return strm;
+}
+
+void init_boost_log() {
+    typedef sinks::synchronous_sink<sinks::text_ostream_backend> text_sink;
+    boost::shared_ptr<text_sink> sink = boost::make_shared<text_sink>();
+    sink->locked_backend()->add_stream(boost::make_shared<std::ofstream>("boost.log"));
+    sink->locked_backend()->auto_flush();
+    sink->set_formatter(expr::format("[%1%] [%2%]: %3%")
+                        % expr::format_date_time(timestamp, "%Y-%m-%d %H:%M:%S")
+                        % expr::attr<severity_level>("Severity")
+                        % expr::smessage);
+
+    logging::core::get()->add_sink(sink);
+    logging::core::get()->set_filter(severity >= info);
+
+    // Add attributes
+    logging::add_common_attributes();
+}
+
+const int N = 10000;
+
+boost::log::sources::severity_logger<severity_level> slg;
+verbose_logger_t<severity_level> log_;
 
 std::string map_timestamp(const std::time_t& time) {
     char mbstr[128];
@@ -21,32 +92,53 @@ std::string map_timestamp(const std::time_t& time) {
     return std::string("?");
 }
 
-int main(int argc, char** argv) {
+std::string map_severity(const severity_level& level) {
+    static const char* strings[] = {
+        "DEBUG",
+        "INFO",
+        "WARNING",
+        "ERROR",
+        "CRITICAL"
+    };
+
+    if (static_cast< std::size_t >(level) < sizeof(strings) / sizeof(*strings))
+        return strings[level];
+    return std::to_string(static_cast<int>(level));
+}
+
+#define BH_LOG(level, msg) \
+    auto record = log_.open_record(level); \
+    if (record.valid()) { \
+        record.attributes["message"] = { msg }; \
+        log_.push(std::move(record)); \
+    }
+
+void init_blackhole_log() {
     mapping::mapper_t mapper;
     mapper.add<std::time_t>("timestamp", &map_timestamp);
+    mapper.add<severity_level>("severity", &map_severity);
 
-    auto f = std::make_unique<formatter::string_t>("[%(timestamp)s]: %(message)s");
+    auto f = std::make_unique<formatter::string_t>("[%(timestamp)s] [%(severity)s]: %(message)s");
     f->set_mapper(std::move(mapper));
-    auto s = std::make_unique<sink::file_t<>>("test.log");
+    auto s = std::make_unique<sink::file_t<>>("blackhole.log");
     auto frontend = std::make_unique<frontend_t<formatter::string_t, sink::file_t<>>>(std::move(f), std::move(s));
     log_.add_frontend(std::move(frontend));
+}
+
+int main(int argc, char** argv) {
+    init_boost_log();
+    init_blackhole_log();
+
     celero::Run(argc, argv);
     return 0;
 }
 
 
-BASELINE(CeleroBenchTest, Baseline, 0, N) {
-    std::time_t time = std::time_t(nullptr);
-    celero::DoNotOptimizeAway(map_timestamp(time));
-    (boost::format("Le message: %s") % "le value").str();
+BASELINE(CeleroBenchTest, BoostLog, 0, N) {
+    BOOST_LOG_SEV(slg, warning) << "Something bad is going on but I can handle it";
 }
 
-BENCHMARK(CeleroBenchTest, Benchmark, 0, N) {
-    auto record = log_.open_record();
-    if (record.valid()) {
-        record.attributes["message"] = { utils::format("Le message: %s", "le value") };
-        log_.push(std::move(record));
-    }
+BENCHMARK(CeleroBenchTest, BlackholeLog, 0, N) {
+    BH_LOG(warning, "Something bad is going on but I can handle it");
 }
-#else
 #endif
