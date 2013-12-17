217d0253b26d322a9f0059ce8a6b51d6c1f03baa
Evgeny Safronov <division494@gmail.com>
2013-12-17 16:15:17 +0400

    More complex exception handler idiom has been implemented.
    Now you can define expected exception types and provide own visitor for processing exceptions through its types. Exception catchers is built in compile-time.
    Old-style is also available.

--- src/blackhole/error/handler.hpp
+++ src/blackhole/error/handler.hpp
@@ -3,6 +3,10 @@
 #include <functional>
 #include <stdexcept>
 
+#include <boost/mpl/vector.hpp>
+#include <boost/mpl/push_back.hpp>
+#include <boost/mpl/reverse.hpp>
+
 #include "blackhole/config.hpp"
 
 namespace blackhole {
@@ -27,6 +31,122 @@ public:
 #endif
     }
 };
+
+namespace aux {
+
+namespace vector {
+
+template<typename... Args>
+struct from_variadic;
+
+template<typename T, typename... Args>
+struct from_variadic<T, Args...> {
+    typedef typename boost::mpl::push_back<
+        typename from_variadic<Args...>::type,
+        T
+    >::type type;
+};
+
+template<typename T>
+struct from_variadic<T> {
+    typedef boost::mpl::vector<T> type;
+};
+
+template<>
+struct from_variadic<> {
+    typedef boost::mpl::vector<> type;
+};
+
+} // namespace vector
+
+} // namespace aux
+
+namespace aux {
+
+template<typename Handler>
+class launcher {
+protected:
+    typedef Handler handler_type;
+    handler_type handler;
+
+public:
+    launcher(const handler_type& handler) :
+        handler(handler)
+    {}
+
+    void operator()() {
+        throw;
+    }
+};
+
+template<typename Exception, typename Base>
+class intermediate : public Base {
+protected:
+    typedef typename Base::handler_type handler_type;
+
+public:
+    intermediate(const handler_type& handler) :
+        Base(handler)
+    {}
+
+    void operator()() {
+        try {
+            Base::operator()();
+        } catch (const Exception& err) {
+            this->handler(err);
+        }
+    }
+};
+
+} // namespace aux
+
+template<typename TypeList, typename Handler>
+class exception_handler :
+    public boost::mpl::fold<
+        TypeList,
+        aux::launcher<Handler>,
+        boost::mpl::bind<boost::mpl::quote2<aux::intermediate>, boost::mpl::_2, boost::mpl::_1>
+    >::type {
+
+    typedef typename boost::mpl::fold<
+        TypeList,
+        aux::launcher<Handler>,
+        boost::mpl::bind<boost::mpl::quote2<aux::intermediate>, boost::mpl::_2, boost::mpl::_1>
+    >::type base_type;
+
+public:
+    exception_handler(Handler handler) :
+        base_type(handler)
+    {}
+
+    void operator()() {
+        base_type::operator()();
+    }
+};
+
+namespace exception {
+
+template<typename Handler>
+struct handler_factory_t {
+    template<typename... Args>
+    static
+    exception_handler<
+        typename boost::mpl::reverse<
+            typename aux::vector::from_variadic<Args...>::type
+        >::type,
+        Handler
+    > make() {
+        typedef exception_handler<
+            typename boost::mpl::reverse<
+                typename aux::vector::from_variadic<Args...>::type
+            >::type,
+            Handler
+        > handler_type;
+        return handler_type(Handler());
+    }
+};
+
+} // namespace exception
 
 } // namespace log
 
--- src/tests/test_ExceptionTrap.cpp
+++ src/tests/test_ExceptionTrap.cpp
@@ -3,7 +3,7 @@
 namespace testing {
 
 struct handler_t {
-    void operator()() {}
+    void operator()(const std::runtime_error&) {}
 };
 
 } // namespace testing
@@ -14,9 +14,13 @@ TEST(exception_trap_t, DoNotCrashWhenRaisedExceptionFromFrontend) {
             .Times(1)
             .WillOnce(Throw(std::runtime_error("error")));
 
+    auto handler = log::exception::handler_factory_t<testing::handler_t>::make<
+        std::runtime_error
+    >();
+
     logger_base_t log;
     log.add_frontend(std::move(frontend));
-    log.set_exception_handler(testing::handler_t());
+    log.set_exception_handler(handler);
     log::record_t record;
     log.push(std::move(record));
 }
