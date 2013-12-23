a7fd9c710a91ec3f7d075379083a075b261bdb59
Evgeny Safronov <division494@gmail.com>
2013-12-23 19:17:59 +0400

    Minor refactoring.

--- src/blackhole/log.hpp
+++ src/blackhole/log.hpp
@@ -4,13 +4,15 @@
 #include "utils/format.hpp"
 #include "utils/nullptr.hpp"
 
+namespace blackhole {
+
 template<typename Log>
-class eater_t {
+class pusher_t {
     Log* log;
     blackhole::log::record_t record;
 
 public:
-    eater_t(Log* log = nullptr, blackhole::log::record_t&& record = blackhole::log::record_t()) :
+    pusher_t(Log* log = nullptr, blackhole::log::record_t&& record = blackhole::log::record_t()) :
         log(log),
         record(record)
     {}
@@ -26,14 +28,17 @@ public:
 };
 
 template<typename Log, typename Level, typename... Args>
-inline eater_t<Log> log_with_attributes(Log& log, Level level, const std::string& message, Args&&... args) {
+inline pusher_t<Log> log_with_attributes(Log& log, Level level, const std::string& message, Args&&... args) {
     blackhole::log::record_t record = log.open_record(level);
     if (record.valid()) {
         record.attributes["message"] = { blackhole::utils::format(message, std::forward<Args>(args)...) };
-        return eater_t<Log>(&log, std::move(record));
+        return pusher_t<Log>(&log, std::move(record));
     }
-    return eater_t<Log>();
+
+    return pusher_t<Log>();
 }
+
+} // namespace blackhole
 
 #define BH_LOG(log, level, ...) \
     do { \
