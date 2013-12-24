ecb4c0e6347df832fa35a91c8acb8c7148e860b0
Evgeny Safronov <division494@gmail.com>
2013-12-24 20:05:42 +0400

    Extending tasks.

--- src/tests/test_Manual.cpp
+++ src/tests/test_Manual.cpp
@@ -37,6 +37,6 @@ TEST(socket_t, ManualUdp) {
 }
 #endif
 
-//!@todo: Make global logger.
+//!@todo: Make global logger for simple usage. Like `repository_t::instance().root()` but with predefined frontend.
 //!@todo: Create logging macro with stream-like api.
 
