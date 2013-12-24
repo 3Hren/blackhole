4cf2b0b278fe9bdf7fb2a4dcdbb419b9fdad9a97
Evgeny Safronov <division494@gmail.com>
2013-12-24 21:32:51 +0400

    Another msgpack formatting test added. New task is waiting.

--- src/tests/test_MsgpackFormatter.cpp
+++ src/tests/test_MsgpackFormatter.cpp
@@ -7,7 +7,7 @@ TEST(msgpack_t, Class) {
     UNUSED(fmt);
 }
 
-TEST(msgpack_t, _) {
+TEST(msgpack_t, FormatSingleAttribute) {
     log::record_t record;
     record.attributes = {
         keyword::message() = "le message"
@@ -18,3 +18,18 @@ TEST(msgpack_t, _) {
 
     EXPECT_EQ("\x81\xa7message\xaale message", fmt.format(record));
 }
+
+TEST(msgpack_t, FormatMultipleAttributes) {
+    log::record_t record;
+    record.attributes = {
+        keyword::message() = "le message",
+        keyword::timestamp() = 100500
+    };
+
+    formatter::msgpack_t fmt;
+    fmt.format(record);
+
+    EXPECT_EQ("\x82\xa9timestamp\xce\x00\x01\x88\x94\xa7message\xaale message", fmt.format(record));
+}
+
+//!@todo: Nested attribute objects. It is needed for logstash.
