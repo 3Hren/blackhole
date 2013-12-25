a6843189534a015496fa724c18069a091ba6c877
Evgeny Safronov <division494@gmail.com>
2013-12-25 18:10:36 +0400

    Rapidjson json builder has been injected.
    Disabled tests are now enabled.

--- src/blackhole/formatter/json.hpp
+++ src/blackhole/formatter/json.hpp
@@ -2,16 +2,58 @@
 
 #include <string>
 
+#include <rapidjson/document.h>
+#include <rapidjson/rapidjson.h>
+#include <rapidjson/stringbuffer.h>
+#include <rapidjson/writer.h>
+
 #include "blackhole/record.hpp"
 
 namespace blackhole {
 
 namespace formatter {
 
+class json_visitor_t : public boost::static_visitor<> {
+    rapidjson::Document &root;
+public:
+    const char* name;
+
+    json_visitor_t(rapidjson::Document& root) :
+        root(root)
+    {}
+
+    template<typename T>
+    void operator ()(T value) {
+        root.AddMember(name, value, root.GetAllocator());
+    }
+
+    void operator ()(std::time_t value) {
+        root.AddMember(name, static_cast<int64_t>(value), root.GetAllocator());
+    }
+
+    void operator ()(const std::string& value) {
+        root.AddMember(name, value.c_str(), root.GetAllocator());
+    }
+};
+
 class json_t {
 public:
-    std::string format(const log::record_t&) const {
-        return std::string();
+    std::string format(const log::record_t& record) const {
+        rapidjson::Document root;
+        root.SetObject();
+
+        json_visitor_t visitor(root);
+        for (auto it = record.attributes.begin(); it != record.attributes.end(); ++it) {
+            const log::attribute_t& attribute = it->second;
+            visitor.name = it->first.c_str();
+            boost::apply_visitor(visitor, attribute.value);
+        }
+
+        rapidjson::GenericStringBuffer<rapidjson::UTF8<>> buffer;
+        rapidjson::Writer<rapidjson::GenericStringBuffer<rapidjson::UTF8<>>> writer(buffer);
+
+        root.Accept(writer);
+        return std::string(buffer.GetString(), buffer.GetSize());
     }
 };
 
--- src/tests/test_JsonFormatter.cpp
+++ src/tests/test_JsonFormatter.cpp
@@ -5,25 +5,29 @@ TEST(json_t, Class) {
     UNUSED(fmt);
 }
 
-TEST_OFF(json_t, FormatSingleAttribute) {
+TEST(json_t, FormatSingleAttribute) {
     log::record_t record;
     record.attributes = {
         keyword::message() = "le message"
     };
 
     formatter::json_t fmt;
-    std::string expected = "{\"message\":\"le message\"}\n";
+    std::string expected = "{\"message\":\"le message\"}";
     EXPECT_EQ(expected, fmt.format(record));
 }
 
-TEST_OFF(json_t, FormatMultipleAttributes) {
+TEST(json_t, FormatMultipleAttributes) {
     log::record_t record;
     record.attributes = {
         keyword::message() = "le message",
-        { "timestamp", log::attribute_t("le timestamp") }
+        keyword::timestamp() = 100500
     };
 
     formatter::json_t fmt;
-    std::string expected = "{\"message\":\"le message\",\"timestamp\":\"le timestamp\"}\n";
-    EXPECT_EQ(expected, fmt.format(record));
+    std::string actual = fmt.format(record);
+
+    rapidjson::Document doc;
+    doc.Parse<0>(actual.c_str());
+    EXPECT_STREQ("le message", doc["message"].GetString());
+    EXPECT_EQ(100500, doc["timestamp"].GetInt());
 }
