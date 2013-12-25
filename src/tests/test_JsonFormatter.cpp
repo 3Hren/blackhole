b4c201fa7e9210cf5b2562fcada98e362fe8548d
Evgeny Safronov <division494@gmail.com>
2013-12-25 18:17:58 +0400

    Additional json-formatter test has been added to check correctness of json tree building.

--- src/tests/test_JsonFormatter.cpp
+++ src/tests/test_JsonFormatter.cpp
@@ -31,3 +31,29 @@ TEST(json_t, FormatMultipleAttributes) {
     EXPECT_STREQ("le message", doc["message"].GetString());
     EXPECT_EQ(100500, doc["timestamp"].GetInt());
 }
+
+TEST(json_t, FormatMultipleComplexAttributes) {
+    log::record_t record;
+    record.attributes = {
+        keyword::message() = "le message",
+        keyword::timestamp() = 100500,
+        attribute::make("@source", "udp://127.0.0.1"),
+        attribute::make("@source_host", "dhcp-218-248-wifi.yandex"),
+        attribute::make("@source_path", "service/storage"),
+        attribute::make("@uuid", "550e8400-e29b-41d4-a716-446655440000")
+    };
+
+    formatter::json_t fmt;
+    std::string actual = fmt.format(record);
+
+    rapidjson::Document doc;
+    doc.Parse<0>(actual.c_str());
+    EXPECT_STREQ("le message", doc["message"].GetString());
+    EXPECT_EQ(100500, doc["timestamp"].GetInt());
+    EXPECT_STREQ("udp://127.0.0.1", doc["@source"].GetString());
+
+    EXPECT_STREQ("udp://127.0.0.1", doc["@source"].GetString());
+    EXPECT_STREQ("dhcp-218-248-wifi.yandex", doc["@source_host"].GetString());
+    EXPECT_STREQ("service/storage", doc["@source_path"].GetString());
+    EXPECT_STREQ("550e8400-e29b-41d4-a716-446655440000", doc["@uuid"].GetString());
+}
