f5e35f203f435a0d0a5c7c880c0cb0e81b8cbbec
Evgeny Safronov <division494@gmail.com>
2013-12-26 19:12:53 +0400

    Multiple attributes mapping test for json formatter has been added.

--- src/tests/test_JsonFormatter.cpp
+++ src/tests/test_JsonFormatter.cpp
@@ -77,3 +77,27 @@ TEST(json_t, SingleAttributeMapping) {
     ASSERT_TRUE(doc.HasMember("timestamp"));
     EXPECT_EQ(100500, doc["timestamp"].GetInt());
 }
+
+TEST(json_t, MultipleAttributeMapping) {
+    log::record_t record;
+    record.attributes = {
+        keyword::message() = "le message",
+        keyword::timestamp() = 100500
+    };
+
+    formatter::json_t::config_type config;
+    config.mapping["message"] = "@message";
+    config.mapping["timestamp"] = "@timestamp";
+
+    formatter::json_t fmt(config);
+    std::string actual = fmt.format(record);
+
+    rapidjson::Document doc;
+    doc.Parse<0>(actual.c_str());
+    ASSERT_TRUE(doc.HasMember("@message"));
+    EXPECT_STREQ("le message", doc["@message"].GetString());
+
+    ASSERT_TRUE(doc.HasMember("@timestamp"));
+    EXPECT_EQ(100500, doc["@timestamp"].GetInt());
+}
+
