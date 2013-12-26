50879e22729115fa25c9dd89d887c99e27e412ee
Evgeny Safronov <division494@gmail.com>
2013-12-26 18:00:41 +0400

    Json formatter tasks has been added.

--- src/blackhole/log.cpp
+++ src/blackhole/log.cpp
@@ -1,5 +1,9 @@
-//!@todo: api: Make fallback logger. Make it configurable.
+//!@todo: json_t/msgpack_t: Attribute mappings.
+//!@todo: json_t/msgpack_t: Attribute tree building.
+//!@todo: aux: More typed configs.
 //!@todo: example: Make example with logstash.
+
+//!@todo: api: Make fallback logger. Make it configurable.
 //!@todo: aux: Make internal exception class with attribute keeping, e.g. line, file or path.
 //!@todo: performance: Current naive implementation of timestamp formatter is suck and have large performance troubles. Fix it.
 //!@todo: performance: Experiment with std::ostringstream or format library for performance check.
