03dd2810e99c4ad4d01363c3355788d350ab23de
Evgeny Safronov <division494@gmail.com>
2014-01-09 17:17:29 +0400

    Default example has been renamed to trivial.

--- src/examples/trivial/main.cpp
+++ src/examples/trivial/main.cpp
@@ -37,7 +37,7 @@ int main(int, char**) {
     verbose_logger_t<level> log = repository_t<level>::instance().trivial();
 
     BH_LOG(log, level::debug,   "[%d] %s - done", 0, "debug");
-    BH_LOG(log, level::info,    "[%d] %s - done",  1, "info");
+    BH_LOG(log, level::info,    "[%d] %s - done", 1, "info");
     BH_LOG(log, level::warning, "[%d] %s - done", 2, "warning");
     BH_LOG(log, level::error,   "[%d] %s - done", 3, "error");
     return 0;
