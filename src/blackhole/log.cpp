cb9ff2599b6e0ebea0ee2cd9fdb8a7d0e5636944
Evgeny Safronov <division494@gmail.com>
2014-01-10 20:46:57 +0400

    Tasks review.

--- src/blackhole/log.cpp
+++ src/blackhole/log.cpp
@@ -1,10 +1,11 @@
 //!@todo: api: Using direct keyword stringify substitution in log macro, i.e. LOG("level=%s", keyword::severity<level>);
-
+//!@todo: performance: Experiment with std::ostringstream or format library for performance check.
+//!@todo: performance & api: Maybe write own json-writer?
 //!@todo: msgpack_t: Attribute mappings.
+
 //!@todo: api: Make fallback logger. Make it configurable.
 //!@todo: aux: Make internal exception class with attribute keeping, e.g. line, file or path.
 //!@todo: performance: Current naive implementation of timestamp formatter is suck and have large performance troubles. Fix it.
-//!@todo: performance: Experiment with std::ostringstream or format library for performance check.
 //!@todo: filter_t: Make || operations in filtering.
 //!@todo: files_t: Make auto_flush flag.
 //!@todo: files_t: Make file rotation.
