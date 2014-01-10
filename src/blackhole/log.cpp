3c0d09f03e453cd02243fc614e91bd0c93085e66
Evgeny Safronov <division494@gmail.com>
2014-01-10 14:56:02 +0400

    Several tasks added.

--- src/blackhole/log.cpp
+++ src/blackhole/log.cpp
@@ -6,7 +6,8 @@
 //!@todo: performance: Current naive implementation of timestamp formatter is suck and have large performance troubles. Fix it.
 //!@todo: performance: Experiment with std::ostringstream or format library for performance check.
 //!@todo: filter_t: Make || operations in filtering.
-//!@todo: api: mapper.add<T>(keyword, function) - use keyword or string.
+//!@todo: api: Using direct keyword stringify substitution in log macro, i.e. LOG("level=%s", keyword::severity<level>);
+//!@todo: api: Mapper.add<T>(keyword, function) - use keyword or string.
 //!@todo: files_t: Make auto_flush flag.
 //!@todo: files_t: Make file rotation.
 //!@todo: files_t: Make file naming by pattern.
