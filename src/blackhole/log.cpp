612327d7b69d4ec62a2624bbce20ed075925d513
Evgeny Safronov <division494@gmail.com>
2014-01-13 20:36:19 +0400

    Task shuffling.

--- src/blackhole/log.cpp
+++ src/blackhole/log.cpp
@@ -1,14 +1,15 @@
 //!@todo: api: Using direct keyword stringify substitution in log macro, i.e. LOG("level=%s", keyword::severity<level>);
+//!@todo: files_t: Make auto_flush flag.
+//!@todo: files_t: Make file rotation.
+//!@todo: files_t: Make file naming by pattern.
+//!@todo: files_t: Create directory if not exists.
+
 //!@todo: performance: Experiment with std::ostringstream or format library for performance check.
 //!@todo: performance & api: Maybe write own json-writer?
-//!@todo: msgpack_t: Attribute mappings.
 
 //!@todo: api: Make fallback logger. Make it configurable.
 //!@todo: aux: Make internal exception class with attribute keeping, e.g. line, file or path.
 //!@todo: performance: Current naive implementation of timestamp formatter is suck and have large performance troubles. Fix it.
 //!@todo: filter_t: Make || operations in filtering.
-//!@todo: files_t: Make auto_flush flag.
-//!@todo: files_t: Make file rotation.
-//!@todo: files_t: Make file naming by pattern.
-//!@todo: files_t: Create directory if not exists.
 //!@todo: socket_t: Make asynchronous TCP backend.
+//!@todo: msgpack_t: Attribute mappings.
