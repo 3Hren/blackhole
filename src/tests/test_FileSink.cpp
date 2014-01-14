d9d1803ec4aea9d194b8e3e9d6b46daf42bd0266
Evgeny Safronov <division494@gmail.com>
2014-01-14 18:33:40 +0400

    Several tests have been added.

--- src/tests/test_FileSink.cpp
+++ src/tests/test_FileSink.cpp
@@ -49,3 +49,18 @@ TEST(file_t, AutoFlushIfSpecified) {
     sink.consume("message");
 }
 
+TEST(file_t, AutoFlushIsDisabledIfSpecified) {
+    sink::file::config_t config("test.log", false);
+    sink::file_t<NiceMock<mock::files::backend_t>> sink(config);
+    EXPECT_CALL(sink.backend(), flush())
+            .Times(0);
+
+    sink.consume("message");
+}
+
+TEST(file_t, AutoFlushIsEnabledByDefault) {
+    sink::file_t<NiceMock<mock::files::backend_t>> sink("test.log");
+    EXPECT_CALL(sink.backend(), flush())
+            .Times(1);
+    sink.consume("message");
+}
