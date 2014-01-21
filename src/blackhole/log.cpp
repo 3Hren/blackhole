9c5ae8a0049fc12334dbc32d280dcfcd53356f93
Evgeny Safronov <division494@gmail.com>
2014-01-21 15:18:47 +0400

    Task reordering.

--- src/blackhole/log.cpp
+++ src/blackhole/log.cpp
@@ -1,14 +1,13 @@
-//!@todo: files_t: Make file rotation.
-//!@todo: files_t: Make file naming by pattern.
-
 //!@todo: example: Make stdout/string example with demonstration of formatting other attribute.
 
 //!@todo: performance: Experiment with std::ostringstream or format library for performance check.
+//!@todo: performance: Current naive implementation of timestamp formatter is suck and have large performance troubles. Fix it.
 //!@todo: performance & api: Maybe write own json-writer?
 
 //!@todo: api: Make fallback logger. Make it configurable.
 //!@todo: aux: Make internal exception class with attribute keeping, e.g. line, file or path.
-//!@todo: performance: Current naive implementation of timestamp formatter is suck and have large performance troubles. Fix it.
-//!@todo: socket_t: Make asynchronous TCP backend.
-//!@todo: msgpack_t: Attribute mappings.
 //!@todo: api: Maybe squish repository_t::init and ::configure methods?
+//!@todo: files_t: Make file naming by pattern.
+//!@todo: msgpack_t: Attribute mappings.
+//!@todo: socket_t: Make asynchronous TCP backend.
+//!@todo: files_t: Make file rotation.
