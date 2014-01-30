e108ce983f6adb77452ae34cf6be74cc3f60dc0c
Evgeny Safronov <division494@gmail.com>
2014-01-30 17:59:23 +0400

    Fixed bug with extracting char from exhausted iterator.

--- src/blackhole/sink/files/rotation/naming/helpers.hpp
+++ src/blackhole/sink/files/rotation/naming/helpers.hpp
@@ -12,12 +12,9 @@ namespace matching {
 
 inline bool all_digits(std::string::const_iterator& it, std::string::const_iterator end, int n) {
     for (; n > 0; --n) {
-        const char c = *it;
-        if (!std::isdigit(c) || it == end) {
+        if (it == end || !std::isdigit(*it++)) {
             return false;
         }
-
-        it++;
     }
 
     return true;
