619c8cb8cc6e8b48c7dfbacbd5bcc9fa0154bfea
Evgeny Safronov <division494@gmail.com>
2014-02-10 14:21:22 +0400

    Task reordering. Also some new tasks has been added.

--- src/blackhole/log.cpp
+++ src/blackhole/log.cpp
@@ -1,6 +1,3 @@
-//!@todo: feature: verbosity filter from file.
-//!@todo: swarm: implement logger_interface
-//!@todo: thevoid: redirect json config.
 //!@todo: files_t: Rotation condition.
 //! Sample config:
 /*!
@@ -20,13 +17,19 @@ Rotate both:
 }
 */
 
+//!@todo: api: Renaming repository methods.
+//!@todo: stream_t: Implement stream sink.
 //!@todo: example: Make stdout/string example with demonstration of formatting other attribute.
+
+//!@todo: feature: Verbosity filter from file.
+//!@todo: api: Ability to set global attribute mapper (?)
+//!@todo: api: Microseconds support in timestamps.
 
 //!@todo: benchmark: File logging comparing with boost::log.
 //!@todo: benchmark: Socket logging with json.
 
 //!@todo: files_t: Make file naming by pattern.
-//!@todo: files_t: logrotate support - ability to handle signals to reopen current file (SIGHUP).
+//!@todo: files_t: logrotate support - ability to handle signals to reopen current file (SIGHUP) (?).
 
 //!@todo: performance: Experiment with std::ostringstream or format library for performance check.
 //!@todo: performance: Current naive implementation of timestamp formatter is suck and have large performance troubles. Fix it.
