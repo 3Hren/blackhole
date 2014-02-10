fa745ba701cd9a40e0345f4cce75043db011308c
Evgeny Safronov <division494@gmail.com>
2014-02-10 18:53:44 +0400

    Testing data is now strictly located in source code.
    This commit should also fix travis build.

--- src/tests/test_ConfigJsonInitialization.cpp
+++ src/tests/test_ConfigJsonInitialization.cpp
@@ -2,13 +2,100 @@
 
 #include "blackhole/repository/config/parser/rapidjson.hpp"
 
+namespace testing {
+
+namespace config {
+
+namespace valid {
+
+static const std::string JSON = "{\
+    'root': [\
+        {\
+            'formatter': {\
+                'type': 'string',\
+                'pattern': '[%(level)s]: %(message)s'\
+            },\
+            'sink': {\
+                'type': 'files',\
+                'path': 'test.log',\
+                'rotation': {\
+                    'pattern': 'test.log.%N',\
+                    'backups': 5,\
+                    'size': 1000000\
+                }\
+            }\
+        }\
+    ]\
+}";
+
+static const std::string MULTIPLE_FRONTENDS = "{\
+    'root': [\
+        {\
+            'formatter': {\
+                'type': 'string',\
+                'pattern': '[%(level)s]: %(message)s'\
+            },\
+            'sink': {\
+                'type': 'files',\
+                'path': 'test.log'\
+            }\
+        },\
+        {\
+            'formatter': {\
+                'type': 'json'\
+            },\
+            'sink': {\
+                'type': 'tcp',\
+                'host': 'localhost',\
+                'port': 50030\
+            }\
+        }\
+    ]\
+}";
+
+} // namespace vaid
+
+namespace invalid {
+
+static const std::string ABSENT_FORMATTER = "{\
+    'root': [\
+        {\
+            'sink': {\
+                'type': 'files',\
+                'path': 'test.log',\
+                'rotation': {\
+                    'pattern': 'test.log.%N',\
+                    'backups': 5,\
+                    'size': 1000000\
+                }\
+            }\
+        }\
+    ]\
+}";
+
+static const std::string ABSENT_SINK = "{\
+    'root': [\
+        {\
+            'formatter': {\
+                'type': 'string',\
+                'pattern': '[%(level)s]: %(message)s'\
+            }\
+        }\
+    ]\
+}";
+
+} // namespace invalid
+
+} // namespace config
+
+} // namespace testing
+
 class parser_test_case_t : public Test {
 protected:
     std::vector<log_config_t> configs;
 
     void SetUp() {
-        std::ifstream stream("config/valid.json");
-        std::string valid((std::istreambuf_iterator<char>(stream)), std::istreambuf_iterator<char>());
+        std::string valid(boost::algorithm::replace_all_copy(config::valid::JSON, "'", "\""));
         rapidjson::Document doc;
         doc.Parse<0>(valid.c_str());
         ASSERT_FALSE(doc.HasParseError());
@@ -43,26 +130,23 @@ TEST_F(parser_test_case_t, CheckSinkConfigAfterParsing) {
 }
 
 TEST(parser_t, ThrowsExceptionIfFormatterSectionIsAbsent) {
-    std::ifstream stream("config/no-valid-absent-formatter.json");
-    std::string nonvalid((std::istreambuf_iterator<char>(stream)), std::istreambuf_iterator<char>());
+    std::string invalid(boost::algorithm::replace_all_copy(config::invalid::ABSENT_FORMATTER, "'", "\""));
     rapidjson::Document doc;
-    doc.Parse<0>(nonvalid.c_str());
+    doc.Parse<0>(invalid.c_str());
     ASSERT_FALSE(doc.HasParseError());
     EXPECT_THROW(repository::config::parser_t<std::vector<log_config_t>>::parse(doc), blackhole::error_t);
 }
 
 TEST(parser_t, ThrowsExceptionIfSinkSectionIsAbsent) {
-    std::ifstream stream("config/no-valid-absent-sink.json");
-    std::string nonvalid((std::istreambuf_iterator<char>(stream)), std::istreambuf_iterator<char>());
+    std::string invalid(boost::algorithm::replace_all_copy(config::invalid::ABSENT_SINK, "'", "\""));
     rapidjson::Document doc;
-    doc.Parse<0>(nonvalid.c_str());
+    doc.Parse<0>(invalid.c_str());
     ASSERT_FALSE(doc.HasParseError());
     EXPECT_THROW(repository::config::parser_t<std::vector<log_config_t>>::parse(doc), blackhole::error_t);
 }
 
 TEST(parser_t, MultipleFrontends) {
-    std::ifstream stream("config/valid-multiple-frontends.json");
-    std::string valid((std::istreambuf_iterator<char>(stream)), std::istreambuf_iterator<char>());
+    std::string valid(boost::algorithm::replace_all_copy(config::valid::MULTIPLE_FRONTENDS, "'", "\""));
     rapidjson::Document doc;
     doc.Parse<0>(valid.c_str());
     ASSERT_FALSE(doc.HasParseError());
