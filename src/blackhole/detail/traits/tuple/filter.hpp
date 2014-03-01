930b1119c1ac1b9e1b179c79ebf5825bc94cf908
Evgeny Safronov <division494@gmail.com>
2014-03-02 02:01:30 +0400

    [Code style] Removed unnecessary entity.

--- src/blackhole/detail/traits/tuple/filter.hpp
+++ src/blackhole/detail/traits/tuple/filter.hpp
@@ -10,28 +10,20 @@ namespace blackhole {
 namespace tuple {
 
 template<template<typename> class F, class Sequence>
-struct filter_impl;
+struct filter;
 
 template<template<typename> class F>
-struct filter_impl<F, std::tuple<>> {
+struct filter<F, std::tuple<>> {
     typedef std::tuple<> type;
 };
 
 template<template<typename> class F, typename T, typename... Args>
-struct filter_impl<F, std::tuple<T, Args...>> {
+struct filter<F, std::tuple<T, Args...>> {
     typedef typename std::conditional<
         F<T>::value,
-        typename concat<std::tuple<T>, typename filter_impl<F, std::tuple<Args...>>::type>::type,
-        typename filter_impl<F, std::tuple<Args...>>::type
+        typename concat<std::tuple<T>, typename filter<F, std::tuple<Args...>>::type>::type,
+        typename filter<F, std::tuple<Args...>>::type
     >::type type;
-};
-
-template<template<typename> class F, class Sequence>
-struct filter;
-
-template<template<typename> class F, typename... Args>
-struct filter<F, std::tuple<Args...>> {
-    typedef typename filter_impl<F, std::tuple<Args...>>::type type;
 };
 
 } // namespace tuple
