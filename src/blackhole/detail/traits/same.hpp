9a5e9a2aee05673870ef9331fb23aae1e3e6562e
Evgeny Safronov <division494@gmail.com>
2014-03-05 18:24:11 +0400

    [Aux] Trait `are_same` now inherits `std::true_type` or `std::false_type` in all cases.

--- src/blackhole/detail/traits/same.hpp
+++ src/blackhole/detail/traits/same.hpp
@@ -13,9 +13,13 @@ template<class T>
 struct are_same<T> : public std::true_type {};
 
 template<class T, class Arg, class... Args>
-struct are_same<T, Arg, Args...> {
-    static const bool value = std::is_same<T, Arg>::value && are_same<T, Args...>::value;
-};
+struct are_same<T, Arg, Args...> :
+    public std::conditional<
+        std::is_same<T, Arg>::value && are_same<T, Args...>::value,
+        std::true_type,
+        std::false_type
+    >::type
+{};
 
 } // namespace aux
 
