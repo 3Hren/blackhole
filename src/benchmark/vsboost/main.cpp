faa292205de8c0ef6e0ed6963bffdabba3a6462a
Evgeny Safronov <division494@gmail.com>
2014-03-27 18:16:34 +0400

    [Code Style] Cleaning up code for `vsboost` benchmark.

--- src/benchmark/vsboost/main.cpp
+++ src/benchmark/vsboost/main.cpp
@@ -1,3 +1,4 @@
+#include <fstream>
 #include <memory>
 
 #include "celero/Celero.h"
@@ -7,35 +8,19 @@
 #include <blackhole/sink/files.hpp>
 #include <blackhole/repository.hpp>
 
-using namespace blackhole;
-
-#include <boost/log/utility/setup.hpp>
+#include <boost/log/core.hpp>
+#include <boost/log/expressions.hpp>
 #include <boost/log/sources/logger.hpp>
 #include <boost/log/sources/record_ostream.hpp>
 #include <boost/log/sources/severity_logger.hpp>
-#include <boost/log/expressions/formatters.hpp>
-
-#include <fstream>
-#include <boost/shared_ptr.hpp>
+#include <boost/log/utility/setup.hpp>
 #include <boost/make_shared.hpp>
-#include <boost/log/core.hpp>
-#include <boost/log/trivial.hpp>
-#include <boost/log/expressions.hpp>
-#include <boost/log/sinks/sync_frontend.hpp>
-#include <boost/log/sinks/text_ostream_backend.hpp>
-#include <boost/log/sources/severity_logger.hpp>
-#include <boost/log/sources/record_ostream.hpp>
-#include <boost/log/utility/setup/common_attributes.hpp>
-#include <boost/log/support/date_time.hpp>
+#include <boost/shared_ptr.hpp>
 
-namespace logging = boost::log;
-namespace src = boost::log::sources;
-namespace expr = boost::log::expressions;
-namespace sinks = boost::log::sinks;
-namespace attrs = boost::log::attributes;
-namespace keywords = boost::log::keywords;
+using namespace blackhole;
 
-enum severity_level {
+//! Severity level.
+enum level {
     debug,
     info,
     warning,
@@ -43,66 +28,62 @@ enum severity_level {
     critical
 };
 
-BOOST_LOG_ATTRIBUTE_KEYWORD(severity, "Severity", severity_level);
-BOOST_LOG_ATTRIBUTE_KEYWORD(timestamp, "TimeStamp", boost::posix_time::ptime);
+static const char* DESCRIPTION[] = {
+    "DEBUG",
+    "INFO",
+    "WARNING",
+    "ERROR",
+    "CRITICAL"
+};
 
-std::ostream& operator<< (std::ostream& strm, severity_level level) {
-    static const char* strings[] = {
-        "DEBUG",
-        "INFO",
-        "WARNING",
-        "ERROR",
-        "CRITICAL"
-    };
+//! Severity mapping functions.
+//!     - for boost ...
+std::ostream& operator<<(std::ostream& stream, level lvl) {
+    if (static_cast< std::size_t >(lvl) < sizeof(DESCRIPTION) / sizeof(*DESCRIPTION)) {
+        stream << DESCRIPTION[lvl];
+    } else {
+        stream << static_cast<int>(lvl);
+    }
 
-    if (static_cast< std::size_t >(level) < sizeof(strings) / sizeof(*strings))
-        strm << strings[level];
-    else
-        strm << static_cast<int>(level);
-
-    return strm;
+    return stream;
 }
 
+//!     - and for blackhole ...
+std::string map_severity(level lvl) {
+    if (static_cast< std::size_t >(lvl) < sizeof(DESCRIPTION) / sizeof(*DESCRIPTION)) {
+        return DESCRIPTION[lvl];
+    }
+    return std::to_string(static_cast<int>(lvl));
+}
+
+//! Register severity keyword for boost logger.
+BOOST_LOG_ATTRIBUTE_KEYWORD(severity, "Severity", level)
+
+//! Initialialize boost logger.
 void init_boost_log() {
-    typedef sinks::synchronous_sink<sinks::text_ostream_backend> text_sink;
-    boost::shared_ptr<text_sink> sink = boost::make_shared<text_sink>();
+    typedef boost::log::sinks::synchronous_sink<boost::log::sinks::text_ostream_backend> text_sink_t;
+
+    // Create and configure files sink.
+    boost::shared_ptr<text_sink_t> sink(boost::make_shared<text_sink_t>());
     sink->locked_backend()->add_stream(boost::make_shared<std::ofstream>("boost.log"));
     sink->locked_backend()->auto_flush();
-    sink->set_formatter(expr::format("[%1%]: %2%")
-                        % expr::attr<severity_level>("Severity")
-                        % expr::smessage);
+    sink->set_formatter(boost::log::expressions::format("[%1%]: %2%")
+                        % boost::log::expressions::attr<level>("Severity")
+                        % boost::log::expressions::smessage);
 
-    logging::core::get()->add_sink(sink);
-    logging::core::get()->set_filter(severity >= info);
+    boost::log::core::get()->add_sink(sink);
+    boost::log::core::get()->set_filter(severity >= info);
 
-    // Add attributes
-    logging::add_common_attributes();
+    // Add common attributes.
+    boost::log::add_common_attributes();
 }
 
-const int N = 50000;
-
-boost::log::sources::severity_logger<severity_level> slg;
-verbose_logger_t<severity_level> *log_;
-
-std::string map_severity(const severity_level& level) {
-    static const char* strings[] = {
-        "DEBUG",
-        "INFO",
-        "WARNING",
-        "ERROR",
-        "CRITICAL"
-    };
-
-    if (static_cast< std::size_t >(level) < sizeof(strings) / sizeof(*strings))
-        return strings[level];
-    return std::to_string(static_cast<int>(level));
-}
-
+//! Initialialize blackhole logger.
 void init_blackhole_log() {
-    repository_t<severity_level>::instance().configure<sink::files_t<>, formatter::string_t>();
+    repository_t<level>::instance().configure<sink::files_t<>, formatter::string_t>();
 
     mapping::value_t mapper;
-    mapper.add<severity_level>("severity", &map_severity);
+    mapper.add<level>("severity", &map_severity);
 
     formatter_config_t formatter("string", mapper);
     formatter["pattern"] = "[%(severity)s]: %(message)s";
@@ -114,24 +95,30 @@ void init_blackhole_log() {
     frontend_config_t frontend = { formatter, sink };
     log_config_t config{ "root", { frontend } };
 
-    repository_t<severity_level>::instance().add_config(config);
+    repository_t<level>::instance().add_config(config);
 }
+
+//! Create logger objects.
+boost::log::sources::severity_logger<level> boost_log;
+verbose_logger_t<level> *blackhole_log; // Cannot create logger object if it isn't initialized.
 
 int main(int argc, char** argv) {
     init_boost_log();
     init_blackhole_log();
-    auto log = repository_t<severity_level>::instance().root();
-    log_ = &log;
+    auto log = repository_t<level>::instance().root();
+    blackhole_log = &log;
 
     celero::Run(argc, argv);
     return 0;
 }
 
+static const int SAMPLES = 30;
+static const int CALLS = 50000;
 
-BASELINE(CeleroBenchTest, BoostLog, 0, N) {
-    BOOST_LOG_SEV(slg, warning) << "Something bad is going on but I can handle it";
+BASELINE(Benchmark, BoostLog, SAMPLES, CALLS) {
+    BOOST_LOG_SEV(boost_log, warning) << "Something bad is going on but I can handle it";
 }
 
-BENCHMARK(CeleroBenchTest, BlackholeLog, 0, N) {
-    BH_LOG((*log_), warning, "Something bad is going on but I can handle it");
+BENCHMARK(Benchmark, BlackholeLog, SAMPLES, CALLS) {
+    BH_LOG((*blackhole_log), warning, "Something bad is going on but I can handle it");
 }
