bd1d1e4d2ad39f2676bfce86b0f25496c621f62c
Evgeny Safronov <division494@gmail.com>
2014-03-28 13:32:06 +0400

    [Code Style] Tiny refactoring over value mappers and extractors.

--- src/blackhole/formatter/map/value.hpp
+++ src/blackhole/formatter/map/value.hpp
@@ -16,21 +16,21 @@ namespace mapping {
 
 template<typename T>
 struct extracter {
-    typedef std::function<void(blackhole::aux::attachable_ostringstream&, const T&)> function_type;
+    typedef std::function<void(aux::attachable_ostringstream&, const T&)> function_type;
     function_type func;
 
-    extracter(std::function<void(blackhole::aux::attachable_ostringstream&, const T&)> func) :
+    extracter(std::function<void(aux::attachable_ostringstream&, const T&)> func) :
         func(func)
     {}
 
-    void operator()(blackhole::aux::attachable_ostringstream& stream, const log::attribute_value_t& value) const {
+    void operator()(aux::attachable_ostringstream& stream, const log::attribute_value_t& value) const {
         typedef typename aux::underlying_type<T>::type underlying_type;
         func(stream, static_cast<T>(boost::get<underlying_type>(value)));
     }
 };
 
 class value_t {
-    typedef std::function<void(blackhole::aux::attachable_ostringstream&, const log::attribute_value_t&)> mapping_t;
+    typedef std::function<void(aux::attachable_ostringstream&, const log::attribute_value_t&)> mapping_t;
     std::unordered_map<std::string, mapping_t> m_mappings;
 
 public:
@@ -40,12 +40,12 @@ public:
     }
 
     template<typename Keyword>
-    void add(std::function<void(blackhole::aux::attachable_ostringstream&, const typename Keyword::type&)> handler) {
+    void add(typename extracter<typename Keyword::type>::function_type handler) {
         add<typename Keyword::type>(Keyword::name(), handler);
     }
 
     template<typename T>
-    void operator ()(blackhole::aux::attachable_ostringstream& stream, const std::string& key, T&& value) const {
+    void operator ()(aux::attachable_ostringstream& stream, const std::string& key, T&& value) const {
         auto it = m_mappings.find(key);
         if (it != m_mappings.end()) {
             const mapping_t& action = it->second;
@@ -61,7 +61,7 @@ public:
         if (it != m_mappings.end()) {
             const mapping_t& action = it->second;
             std::string buffer;
-            blackhole::aux::attachable_ostringstream stream;
+            aux::attachable_ostringstream stream;
             stream.attach(buffer);
             action(stream, std::forward<T>(value));
             stream.flush();
