ce33598f221f27d8dc19902d0acd277c87581339
Evgeny Safronov <division494@gmail.com>
2014-03-31 17:19:45 +0400

    [Examples] More comments. Added example how to use logger without macro.

--- src/examples/simple/main.cpp
+++ src/examples/simple/main.cpp
@@ -1,21 +1,27 @@
-#include <blackhole/log.hpp>
-#include <blackhole/repository.hpp>
+#include <blackhole/blackhole.hpp>
+
+//! This example demonstrates the a bit extended blackhole logging library usage and its features.
+
+/*! - detailed formatters and sinks configuration;
+ *  - logger usage without macro.
+ */
 
 using namespace blackhole;
 
+// As always specify severity enumeration.
 enum class level {
-    debug,
-    info,
-    warning,
-    error
+    debug
 };
 
-//! Initialization stage.
-//! Manually or from file - whatever. The main aim - is to get initialized `log_config_t` object.
+// Here we are going to configure our string/stream frontend and to register it.
 void init() {
+    // Configure string formatter.
+    // Pattern syntax behaves like usual substitution for placeholder. For example if attribute
+    // named `severity` has value `2`, then pattern [%(severity)s] will produce: [2].
     formatter_config_t formatter("string");
     formatter["pattern"] = "[%(timestamp)s] [%(severity)s]: %(message)s";
 
+    // Configure stream sink to write into stdout (also stderr can be configured).
     sink_config_t sink("stream");
     sink["output"] = "stdout";
 
@@ -25,14 +31,30 @@ void init() {
     repository_t<level>::instance().add_config(config);
 }
 
+// Here it's an example how to create and handle log events without using any macros at all.
+template<typename Log>
+void debug(Log& log, level lvl, const char* message) {
+    // Tries to create log record. Returns invalid record object if created log record couldn't
+    // pass filtering stage.
+    // For our case it will be created anyway, because we don't have any filters registered now.
+    log::record_t record = log.open_record(lvl);
+    if (record.valid()) {
+        // Manually insert message attribute into log record attributes set using keyword API.
+        // Formatter will break up if some attributes it needed don't exist where formatting
+        // occures.
+        record.attributes.insert({
+            keyword::message() = message
+        });
+        log.push(std::move(record));
+    }
+}
+
 int main(int, char**) {
     init();
     verbose_logger_t<level> log = repository_t<level>::instance().root();
 
-    BH_LOG(log, level::debug,   "[%d] %s - done", 0, "debug");
-    BH_LOG(log, level::info,    "[%d] %s - done", 1, "info");
-    BH_LOG(log, level::warning, "[%d] %s - done", 2, "warning");
-    BH_LOG(log, level::error,   "[%d] %s - done", 3, "error");
+    BH_LOG(log, level::debug, "log message using macro API");
+    debug(log, level::debug, "log message using log object directly");
 
     return 0;
 }
