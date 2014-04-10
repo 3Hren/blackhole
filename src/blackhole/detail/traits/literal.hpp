6760956c683a6217ca4b6f940c4c15c328177d54
Evgeny Safronov <division494@gmail.com>
2014-04-10 11:35:38 +0400

    [Aux] Is string literal metafunction now inherits from `or_` instead of using it.

--- src/blackhole/detail/traits/literal.hpp
+++ src/blackhole/detail/traits/literal.hpp
@@ -7,17 +7,15 @@
 namespace blackhole {
 
 template<typename T>
-struct is_string_literal_type {
-    typedef typename or_<
-        typename std::is_same<
-            const char*,
-            typename std::decay<T>::type
-        >::type,
-        typename std::is_same<
-            char*,
-            typename std::decay<T>::type
-        >::type
-    >::type type;
-};
+struct is_string_literal_type : public or_<
+    typename std::is_same<
+        const char*,
+        typename std::decay<T>::type
+    >::type,
+    typename std::is_same<
+        char*,
+        typename std::decay<T>::type
+    >::type
+> {};
 
 } // namespace blackhole
