317a038ac78373807bd857d077c76da3255865f1
Evgeny Safronov <division494@gmail.com>
2014-04-14 21:36:12 +0400

    [Refactoring] No need for defining separate type for attributes feeding.

--- src/blackhole/log.hpp
+++ src/blackhole/log.hpp
@@ -15,14 +15,23 @@ namespace blackhole {
 
 namespace aux {
 
-class feeder_t {
-protected:
+template<typename Log>
+class scoped_pump {
+    const Log& log;
     log::record_t& record;
 
 public:
-    feeder_t(log::record_t& record) :
+    template<typename... Args>
+    scoped_pump(const Log& log, log::record_t& record, Args&&... args) :
+        log(log),
         record(record)
-    {}
+    {
+        record.attributes.insert(keyword::message() = aux::format(record.attributes, std::forward<Args>(args)...));
+    }
+
+    ~scoped_pump() {
+        log.push(std::move(record));
+    }
 
     // Using initializer lists is problematic, since GCC 4.6 has an extension which can
     // decude { { "value", "42" } } expressions in function call to the initializer list
@@ -30,40 +39,22 @@ public:
     // allows such deducing only for `auto` keyword (including range-based `for` loop).
     // Please, specify `attribute::list` type explicitly when using initializer lists to
     // provide attributes.
-    feeder_t operator()(std::initializer_list<std::pair<std::string, log::attribute_value_t>>&& args) {
+    scoped_pump& operator()(std::initializer_list<std::pair<std::string, log::attribute_value_t>>&& args) {
         for (auto it = args.begin(); it != args.end(); ++it) {
             record.attributes.insert(std::make_pair(it->first, log::attribute_t(it->second)));
         }
 
-        return feeder_t { record };
+        return *this;
     }
 
     template<typename... Args>
-    feeder_t operator()(Args&&... args) {
+    scoped_pump& operator()(Args&&... args) {
         static_assert((is_keyword_pack<Args...>::value || is_emplace_pack<Args...>::value),
                       "parameter pack must be either attribute pack or emplace pack");
         typedef typename pack_determiner<Args...>::type pack_type;
         pack_feeder<pack_type>::feed(record, std::forward<Args>(args)...);
 
-        return feeder_t { record };
-    }
-};
-
-template<typename Log>
-class scoped_pump : public feeder_t {
-    const Log& log;
-
-public:
-    template<typename... Args>
-    scoped_pump(const Log& log, log::record_t& record, Args&&... args) :
-        feeder_t(record),
-        log(log)
-    {
-        record.attributes.insert(keyword::message() = aux::format(record.attributes, std::forward<Args>(args)...));
-    }
-
-    ~scoped_pump() {
-        log.push(std::move(record));
+        return *this;
     }
 };
 
