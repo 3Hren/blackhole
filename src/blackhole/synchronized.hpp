01ca192dbb5b8ba0cf6b10f605f57453e123f410
Evgeny Safronov <division494@gmail.com>
2014-04-14 21:01:30 +0400

    [API] Logger synchronized wrapped can now be moved.

--- src/blackhole/synchronized.hpp
+++ src/blackhole/synchronized.hpp
@@ -4,58 +4,71 @@
 
 namespace blackhole {
 
-template<class Logger>
+template<class Logger, class Mutex = std::mutex>
 class synchronized {
-    Logger logger;
-    mutable std::mutex mutex;
 public:
-    synchronized(Logger&& logger) :
+    typedef Logger logger_type;
+    typedef Mutex mutex_type;
+
+private:
+    logger_type logger;
+    mutable mutex_type mutex;
+
+public:
+    synchronized() {}
+    explicit synchronized(logger_type&& logger) :
         logger(std::move(logger))
     {}
 
+    synchronized& operator=(synchronized&& other) {
+        std::lock_guard<mutex_type>(other.mutex);
+        logger = std::move(other.logger);
+        return *this;
+    }
+
     bool enabled() const {
-        std::lock_guard<std::mutex> lock(mutex);
+        std::lock_guard<mutex_type> lock(mutex);
         return logger.enabled();
     }
 
     void enable() {
-        std::lock_guard<std::mutex> lock(mutex);
+        std::lock_guard<mutex_type> lock(mutex);
         logger.enable();
     }
 
     void disable() {
-        std::lock_guard<std::mutex> lock(mutex);
+        std::lock_guard<mutex_type> lock(mutex);
         logger.disable();
     }
 
     void set_filter(filter_t&& filter) {
-        std::lock_guard<std::mutex> lock(mutex);
+        std::lock_guard<mutex_type> lock(mutex);
         logger.set_filter(std::move(filter));
     }
 
     void add_attribute(const log::attribute_pair_t& attr) {
-        std::lock_guard<std::mutex> lock(mutex);
+        std::lock_guard<mutex_type> lock(mutex);
         logger.add_attribute(attr);
     }
 
     void add_frontend(std::unique_ptr<base_frontend_t> frontend) {
-        std::lock_guard<std::mutex> lock(mutex);
+        std::lock_guard<mutex_type> lock(mutex);
         logger.add_frontend(std::move(frontend));
     }
 
     void set_exception_handler(log::exception_handler_t&& handler) {
-        std::lock_guard<std::mutex> lock(mutex);
+        std::lock_guard<mutex_type> lock(mutex);
         logger.set_exception_handler(std::move(handler));
     }
 
     template<typename... Args>
     log::record_t open_record(Args&&... args) const {
-        std::lock_guard<std::mutex> lock(mutex);
+        std::lock_guard<mutex_type> lock(mutex);
         return logger.open_record(std::forward<Args>(args)...);
     }
 
     void push(log::record_t&& record) const {
-        std::lock_guard<std::mutex> lock(mutex);
+        std::lock_guard<mutex_type> lock(mutex);
         logger.push(std::move(record));
     }
 };
--- src/tests/test_LoggerSynchronized.cpp
+++ src/tests/test_LoggerSynchronized.cpp
@@ -6,7 +6,19 @@
 using namespace blackhole;
 
 TEST(SynchronizedLogger, Class) {
+    synchronized<verbose_logger_t<level>> logger;
+    UNUSED(logger);
+}
+
+TEST(SynchronizedLogger, ExplicitMoveConstructorForLogger) {
     verbose_logger_t<level> log;
     synchronized<verbose_logger_t<level>> logger(std::move(log));
     UNUSED(logger);
+}
+
+TEST(SynchronizedLogger, MoveAssignment) {
+    verbose_logger_t<level> log;
+    synchronized<verbose_logger_t<level>> logger(std::move(log));
+    synchronized<verbose_logger_t<level>> other;
+    other = std::move(logger);
 }
