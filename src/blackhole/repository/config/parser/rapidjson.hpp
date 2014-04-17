06350851c7b8ded184c41ad0643b2ecaa037783b
Evgeny Safronov <division494@gmail.com>
2014-04-17 14:09:43 +0400

    [Aux] Tiny refactoring over rapidjson parser.

--- src/blackhole/repository/config/parser/rapidjson.hpp
+++ src/blackhole/repository/config/parser/rapidjson.hpp
@@ -12,95 +12,150 @@ namespace repository {
 
 namespace config {
 
+template<typename T>
+struct convert_adapter;
+
+template<>
+struct convert_adapter<rapidjson::Value> {
+    typedef rapidjson::Value value_type;
+    typedef typename rapidjson::Value::ConstValueIterator const_iterator;
+    typedef typename rapidjson::Value::ConstMemberIterator const_object_iterator;
+
+    static const_iterator begin(const value_type& value) {
+        return value.Begin();
+    }
+
+    static const_iterator end(const value_type& value) {
+        return value.End();
+    }
+
+    static const_object_iterator object_begin(const value_type& value) {
+        return value.MemberBegin();
+    }
+
+    static const_object_iterator object_end(const value_type& value) {
+        return value.MemberEnd();
+    }
+
+    static std::string name(const const_object_iterator& it) {
+        return std::string(it->name.GetString());
+    }
+
+    static const value_type& value(const const_object_iterator& it) {
+        return it->value;
+    }
+
+    static bool has(const std::string& name, const value_type& value) {
+        return value.HasMember(name.c_str());
+    }
+};
+
 namespace aux {
 
-template<typename T>
-static void fill(T& builder, const rapidjson::Value& node, const std::string& path) {
-    for (auto it = node.MemberBegin(); it != node.MemberEnd(); ++it) {
-        const std::string& name = it->name.GetString();
-        const rapidjson::Value& value = it->value;
+struct filler {
+    typedef rapidjson::Value value_type;
+    typedef convert_adapter<value_type> conv;
 
-        if (value.IsObject()) {
-            auto nested_builder = builder[name];
-            fill(nested_builder, value, path + "/" + name);
-        } else if (value.IsArray() || value.IsNull()) {
-            throw blackhole::error_t("array and null parsing is not implemented yet");
-        } else {
-            if (name == "type") {
-                continue;
-            }
+    template<typename T>
+    static void fill(T& builder, const value_type& node, const std::string& path) {
+        for (auto it = conv::object_begin(node); it != conv::object_end(node); ++it) {
+            const auto& name = conv::name(it);
+            const auto& value = conv::value(it);
 
-            if (value.IsBool()) {
-                builder[name] = value.GetBool();
-            } else if (value.IsInt()) {
-                builder[name] = value.GetInt();
-            } else if (value.IsUint()) {
-                builder[name] = value.GetUint();
-            } else if (value.IsInt64()) {
-                builder[name] = value.GetInt64();
-            } else if (value.IsUint64()) {
-                builder[name] = value.GetUint64();
-            } else if (value.IsDouble()) {
-                builder[name] = value.GetDouble();
-            } else if (value.IsString()) {
-                builder[name] = std::string(value.GetString());
+            if (value.IsObject()) {
+                auto recursive = builder[name];
+                filler::fill(recursive, value, path + "/" + name);
+            } else if (value.IsNull() || value.IsArray()) {
+                throw blackhole::error_t("array and null parsing is not implemented yet");
+            } else {
+                if (name == "type") {
+                    continue;
+                }
+
+                if (value.IsBool()) {
+                    builder[name] = value.GetBool();
+                } else if (value.IsInt()) {
+                    builder[name] = value.GetInt();
+                } else if (value.IsUint()) {
+                    builder[name] = value.GetUint();
+                } else if (value.IsInt64()) {
+                    builder[name] = value.GetInt64();
+                } else if (value.IsUint64()) {
+                    builder[name] = value.GetUint64();
+                } else if (value.IsDouble()) {
+                    builder[name] = value.GetDouble();
+                } else if (value.IsString()) {
+                    builder[name] = std::string(value.GetString());
+                }
             }
         }
     }
-}
+};
 
 } // namespace aux
 
 template<>
 class parser_t<repository::config::base_t> {
+    typedef rapidjson::Value value_type;
+
 public:
     template<typename T>
-    static T parse(const std::string& path, const rapidjson::Value& value) {
+    static T parse(const std::string& path, const value_type& value) {
         const std::string& type = value["type"].GetString();
         T config(type);
-        aux::fill(config, value, path + "/" + type);
+        aux::filler::fill(config, value, path + "/" + type);
         return config;
     }
 };
 
 template<>
 class parser_t<formatter_config_t> {
+    typedef rapidjson::Value value_type;
+
 public:
-    static formatter_config_t parse(const rapidjson::Value& value) {
+    static formatter_config_t parse(const value_type& value) {
         return parser_t<repository::config::base_t>::parse<formatter_config_t>("formatter", value);
     }
 };
 
 template<>
 class parser_t<sink_config_t> {
+    typedef rapidjson::Value value_type;
+
 public:
-    static sink_config_t parse(const rapidjson::Value& value) {
+    static sink_config_t parse(const value_type& value) {
         return parser_t<repository::config::base_t>::parse<sink_config_t>("sink", value);
     }
 };
 
 template<>
 class parser_t<frontend_config_t> {
+    typedef rapidjson::Value value_type;
+    typedef convert_adapter<value_type> conv;
+
 public:
-    static frontend_config_t parse(const rapidjson::Value& value) {
-        if (!(value.HasMember("formatter") && value.HasMember("sink"))) {
+    static frontend_config_t parse(const value_type& value) {
+        if (!(conv::has("formatter", value) && conv::has("sink", value))) {
             throw blackhole::error_t("both 'formatter' and 'sink' section must be specified");
         }
 
-        const formatter_config_t& formatter = parser_t<formatter_config_t>::parse(value["formatter"]);
-        const sink_config_t& sink = parser_t<sink_config_t>::parse(value["sink"]);
+        const auto& formatter = parser_t<formatter_config_t>::parse(value["formatter"]);
+        const auto& sink = parser_t<sink_config_t>::parse(value["sink"]);
         return frontend_config_t { formatter, sink };
     }
 };
 
 template<>
 class parser_t<log_config_t> {
+    typedef rapidjson::Value value_type;
+    typedef convert_adapter<value_type> conv;
+
 public:
-    static log_config_t parse(const std::string& name, const rapidjson::Value& value) {
+    static log_config_t parse(const std::string& name, const value_type& value) {
         log_config_t config;
         config.name = name;
-        for (auto it = value.Begin(); it != value.End(); ++it) {
-            const frontend_config_t& frontend = parser_t<frontend_config_t>::parse(*it);
+        for (auto it = conv::begin(value); it != conv::end(value); ++it) {
+            const auto& frontend = parser_t<frontend_config_t>::parse(*it);
             config.frontends.push_back(frontend);
         }
 
@@ -110,11 +165,14 @@ public:
 
 template<>
 class parser_t<std::vector<log_config_t>> {
+    typedef rapidjson::Value value_type;
+    typedef convert_adapter<value_type> conv;
+
 public:
-    static std::vector<log_config_t> parse(const rapidjson::Value& root) {
+    static std::vector<log_config_t> parse(const value_type& root) {
         std::vector<log_config_t> configs;
-        for (auto it = root.MemberBegin(); it != root.MemberEnd(); ++it) {
-            const log_config_t& config = parser_t<log_config_t>::parse(it->name.GetString(), it->value);
+        for (auto it = conv::object_begin(root); it != conv::object_end(root); ++it) {
+            const auto& config = parser_t<log_config_t>::parse(conv::name(it), conv::value(it));
             configs.push_back(config);
         }
 
