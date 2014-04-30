96996d3651feec993957a091853ec370309c4ed1
Evgeny Safronov <division494@gmail.com>
2014-04-30 18:41:45 +0400

    [API] Added header-file with forward declarations.

---
+++ src/blackhole/forwards.hpp
@@ -0,0 +1,13 @@
+#pragma once
+
+namespace blackhole {
+
+class logger_base_t;
+
+template<typename Level>
+class verbose_logger_t;
+
+template<class T, class Mutex>
+class synchronized;
+
+} // namespace blackhole
