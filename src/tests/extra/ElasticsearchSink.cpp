dae26deae0755ddacffbd9da67036f065232f7cf
Evgeny Safronov <division494@gmail.com>
2014-05-07 01:55:22 +0400

    [Elasticsearch] Using logging macro without severity level. [Elasticsearch] Introduced sink config. [Elasticsearch] Elasticsearch client now strictly starts after sink's worker thread started.

--- src/tests/extra/ElasticsearchSink.cpp
+++ src/tests/extra/ElasticsearchSink.cpp
@@ -3,6 +3,7 @@
 #include <boost/asio.hpp>
 
 #include <blackhole/formatter/string.hpp>
+#include <blackhole/log.hpp>
 #include <blackhole/logger.hpp>
 #include <blackhole/sink/stream.hpp>
 #include <blackhole/synchronized.hpp>
@@ -10,6 +11,10 @@
 #include <blackhole/utils/format.hpp>
 
 #include "../global.hpp"
+
+#define LOG(__log__, ...) \
+    if (blackhole::log::record_t record = __log__.open_record()) \
+        blackhole::aux::make_scoped_pump(__log__, record, __VA_ARGS__)
 
 namespace blackhole {
 
@@ -69,19 +74,55 @@ private:
 
 namespace elasticsearch {
 
-struct endpoint_t {
-    std::string host;
-    std::uint16_t port;
+//!@todo: Dummy yet.
+struct config_t {
+};
+
+struct settings_t {
+    typedef boost::asio::ip::tcp::endpoint endpoint_type;
+
+    std::string index;
+    std::vector<endpoint_type> endpoints;
+    struct {
+        struct {
+            bool start;
+        } when;
+    } sniffer;
+
+    settings_t() :
+        index("log"),
+        endpoints(std::vector<endpoint_type>({
+            { endpoint_type(boost::asio::ip::address_v4(), 9200) }
+        })),
+        sniffer({ { true } })
+    {}
 };
 
 class client_t {
 public:
+    typedef boost::asio::io_service loop_type;
+    typedef synchronized<logger_base_t> logger_type;
     typedef std::function<void()> callback_type;
     typedef std::function<
         void(const boost::system::error_code&)
     > errback_type;
+
+private:
+    settings_t settings;
+    loop_type& loop;
+    logger_type& log;
+
 public:
-    client_t(const std::vector<endpoint_t>& endpoints) {
+    client_t(const settings_t& settings, loop_type& loop, logger_type& log) :
+        settings(settings),
+        loop(loop),
+        log(log)
+    {
+//        transport.add_nodes(settings.endpoints);
+        if (settings.sniffer.when.start) {
+            LOG(log, "sniff.when.start is true - preparing to update nodes list");
+//            transport.sniff();
+        }
     }
 
     void bulk_write(std::string message,
@@ -117,34 +158,32 @@ public:
 };
 
 class elasticsearch_t {
-    boost::posix_time::milliseconds interval;
-    elasticsearch::client_t client;
+    synchronized<logger_base_t> log;
 
-    std::thread thread;
-    bulk::queue_t<std::string> queue;
+    boost::posix_time::milliseconds interval;
 
     std::atomic<bool> stopped;
     boost::asio::io_service loop;
     boost::asio::deadline_timer timer;
 
-    synchronized<logger_base_t> logger;
+    std::thread thread;
+    bulk::queue_t<std::string> queue;
+    elasticsearch::client_t client;
+
 public:
-    elasticsearch_t(std::uint16_t bulk = 100, std::uint32_t interval = 1000) :
+    elasticsearch_t(std::uint16_t bulk = 100,
+                    std::uint32_t interval = 1000,
+                    elasticsearch::settings_t settings = elasticsearch::settings_t()) :
+        log(logger_factory_t::create()),
         interval(interval),
-        client(std::vector<elasticsearch::endpoint_t>({{ "localhost", 9200}})),
-        queue(bulk, std::bind(&elasticsearch_t::on_bulk, this, std::placeholders::_1)),
         stopped(true),
         timer(loop),
-        logger(logger_factory_t::create())
-    {
-        std::mutex mutex;
-        std::unique_lock<std::mutex> lock(mutex);
-        std::condition_variable started;
-        thread = std::thread(
-            std::bind(&elasticsearch_t::run, this, std::ref(started))
-        );
-        started.wait(lock);
-    }
+        thread(start()),
+        queue(bulk, std::bind(&elasticsearch_t::on_bulk, this, std::placeholders::_1)),
+        client(settings, loop, log)
+    {}
+
+    //!@todo: Config constructor.
 
     ~elasticsearch_t() {
         stop();
@@ -152,19 +191,30 @@ public:
 
     void consume(std::string&& message) {
         if (stopped) {
-            log("dropping '%s', because worker thread is stopped", message);
+            LOG(log, "dropping '%s', because worker thread is stopped", message);
             return;
         }
 
-        log("pushing '%s' to the queue", message);
+        LOG(log, "pushing '%s' to the queue", message);
         queue.push(std::move(message));
     }
 
 private:
+    std::thread start() {
+        std::mutex mutex;
+        std::unique_lock<std::mutex> lock(mutex);
+        std::condition_variable started;
+        std::thread thread(
+            std::bind(&elasticsearch_t::run, this, std::ref(started))
+        );
+        started.wait(lock);
+        return std::move(thread);
+    }
+
     void run(std::condition_variable& started) {
         BOOST_ASSERT(stopped);
 
-        log("starting elasticsearch sink ...");
+        LOG(log, "starting elasticsearch sink ...");
         loop.post(
             std::bind(&elasticsearch_t::on_started, this, std::ref(started))
         );
@@ -174,7 +224,7 @@ private:
         );
         boost::system::error_code ec;
         loop.run(ec);
-        log("elasticsearch sink has been stopped: %s", ec ? ec.message() : "ok");
+        LOG(log, "elasticsearch sink has been stopped: %s", ec ? ec.message() : "ok");
     }
 
     void stop() {
@@ -182,33 +232,33 @@ private:
         stopped = true;
 
         if (thread.joinable()) {
-            log("stopping worker thread ...");
+            LOG(log, "stopping worker thread ...");
             thread.join();
         }
     }
 
     void on_started(std::condition_variable& started) {
-        log("elasticsearch sink has been started");
+        LOG(log, "elasticsearch sink has been started");
         stopped = false;
         started.notify_all();
     }
 
     void on_bulk(std::vector<std::string>&& result) {
-        log("processing bulk event ...");
+        LOG(log, "processing bulk event ...");
         process(std::move(result));
     }
 
     void on_timer(const boost::system::error_code& ec) {
         if (ec) {
-            log("processing timer event failed: %s", ec.message());
+            LOG(log, "processing timer event failed: %s", ec.message());
         } else {
-            log("processing timer event ...");
+            LOG(log, "processing timer event ...");
             process(std::move(queue.dump()));
         }
     }
 
     void process(std::vector<std::string>&& result) {
-        log("processing bulk containing %d messages ...", result.size());
+        LOG(log, "processing bulk containing %d messages ...", result.size());
         std::string message = boost::algorithm::join(result, "");
         client.bulk_write(
             message,
@@ -223,24 +273,11 @@ private:
     }
 
     void on_success() {
-        log("success!");
+        LOG(log, "success!");
     }
 
     void on_error(const boost::system::error_code& ec) {
-        log("ec: %s", ec.message());
-    }
-
-    template<class... Args>
-    void log(std::string&& message, Args&&... args) {
-        if (auto record = logger.open_record()) {
-            record.attributes.insert(
-                keyword::message() = utils::format(
-                    message,
-                    std::forward<Args>(args)...
-                )
-            );
-            logger.push(std::move(record));
-        }
+        LOG(log, "ec: %s", ec.message());
     }
 };
 
