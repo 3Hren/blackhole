d546e9fdc8e0676dd3af0c407a2b0cdaaeafec79
Evgeny Safronov <division494@gmail.com>
2014-05-07 20:30:18 +0400

    [Elasticsearch] Added connection skeleton. [Elasticsearch] Fixed warnings.

--- src/tests/extra/client.hpp
+++ src/tests/extra/client.hpp
@@ -9,15 +9,17 @@
 #include "settings.hpp"
 #include "transport.hpp"
 
-using blackhole::logger_base_t;
-using blackhole::synchronized;
+//!@todo: Tmp.
+#include "../global.hpp"
+
+//!@todo: Temporary.
 
 namespace elasticsearch {
 
 class client_t {
 public:
     typedef boost::asio::io_service loop_type;
-    typedef synchronized<logger_base_t> logger_type;
+    typedef blackhole::synchronized<blackhole::logger_base_t> logger_type;
 
     typedef std::function<void()> callback_type;
     typedef std::function<
@@ -26,7 +28,6 @@ public:
 
 private:
     settings_t settings;
-    loop_type& loop;
     logger_type& log;
 
     http_transport_t transport;
@@ -34,19 +35,23 @@ private:
 public:
     client_t(const settings_t& settings, loop_type& loop, logger_type& log) :
         settings(settings),
-        loop(loop),
-        log(log)
+        log(log),
+        transport(loop, log)
     {
-//        transport.add_nodes(settings.endpoints);
+        transport.add_nodes(settings.endpoints);
         if (settings.sniffer.when.start) {
             LOG(log, "sniff.when.start is true - preparing to update nodes list");
-//            transport.sniff();
+            transport.sniff();
         }
     }
 
     void bulk_write(std::string message,
                     callback_type callback,
                     errback_type errback) {
+        LOG(log, "requesting 'bulk_write' ...");
+        UNUSED(message);
+        UNUSED(callback);
+        UNUSED(errback);
     }
 };
 
--- src/tests/extra/transport.hpp
+++ src/tests/extra/transport.hpp
@@ -1,19 +1,73 @@
 #pragma once
 
 #include <memory>
+#include <mutex>
 
 #include "balancing.hpp"
+#include "log.hpp"
 #include "pool.hpp"
 
 namespace elasticsearch {
 
+class http_connection_t {
+public:
+    typedef boost::asio::io_service loop_type;
+    typedef boost::asio::ip::tcp protocol_type;
+    typedef protocol_type::endpoint endpoint_type;
+
+public:
+    http_connection_t(endpoint_type, loop_type&)
+    {}
+};
+
 class http_transport_t {
 public:
-    typedef pool_t<http_transport_t> pool_type;
+    typedef blackhole::synchronized<blackhole::logger_base_t> logger_type;
+
+    typedef boost::asio::io_service loop_type;
+    typedef http_connection_t connection_type;
+    typedef pool_t<connection_type> pool_type;
+    typedef pool_type::endpoint_type endpoint_type;
 
 private:
+    loop_type& loop;
+    logger_type& log;
+
     pool_type pool;
     std::unique_ptr<balancing::strategy<pool_type>> balancer;
+
+    mutable std::mutex mutex;
+
+public:
+    http_transport_t(loop_type& loop, logger_type& log) :
+        loop(loop),
+        log(log)
+    {}
+
+    void add_nodes(const std::vector<endpoint_type>& endpoints) {
+        for (auto it = endpoints.begin(); it != endpoints.end(); ++it) {
+            add_node(*it);
+        }
+    }
+
+    void add_node(const endpoint_type& endpoint) {
+        boost::system::error_code ec;
+        auto address = endpoint.address().to_string(ec);
+        if (ec) {
+            //!@todo: Do something useful.
+        }
+
+        LOG(log, "adding '%s:%d' to the pool", address, endpoint.port());
+
+        std::lock_guard<std::mutex> lock(mutex);
+        if (pool.contains(endpoint)) {
+            LOG(log, "adding endpoint to the pool is rejected - already exists");
+            return;
+        }
+        pool.insert(endpoint, std::make_shared<connection_type>(endpoint, loop));
+    }
+
+    void sniff() {}
 };
 
 } // namespace elasticsearch
