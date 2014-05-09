8ddbeae2766c0b4abd3458e781fb7d7c579b36f9
Evgeny Safronov <division494@gmail.com>
2014-05-09 23:09:09 +0400

    [Elasticsearch] Connection class moved into separate file.

--- src/blackhole/sink/elasticsearch/transport.hpp
+++ src/blackhole/sink/elasticsearch/transport.hpp
@@ -5,39 +5,13 @@
 #include <tuple>
 
 #include "balancer.hpp"
+#include "connection.hpp"
 #include "log.hpp"
 #include "pool.hpp"
 #include "result.hpp"
 #include "request/info.hpp"
 
 namespace elasticsearch {
-
-template<class Action>
-struct callback {
-    typedef std::function<void(typename Action::result_type)> type;
-};
-
-class http_connection_t {
-public:
-    typedef boost::asio::io_service loop_type;
-    typedef boost::asio::ip::tcp protocol_type;
-    typedef protocol_type::endpoint endpoint_type;
-
-private:
-    endpoint_type endpoint_;
-
-public:
-    http_connection_t(endpoint_type endpoint, loop_type&) :
-        endpoint_(endpoint)
-    {}
-
-    endpoint_type endpoint() const {
-        return endpoint_;
-    }
-
-    template<class Action>
-    void perform(Action&&, typename callback<Action>::type) {}
-};
 
 class http_transport_t {
 public:
