afc5af2abc17b9b687015d053babe0476794c8a7
Evgeny Safronov <division494@gmail.com>
2014-05-10 20:45:21 +0400

    [Elasticsearch] Fix delegating initialization.

--- src/blackhole/sink/elasticsearch/result.hpp
+++ src/blackhole/sink/elasticsearch/result.hpp
@@ -1,14 +1,30 @@
 #pragma once
 
+#include <type_traits>
+
 #include <boost/system/error_code.hpp>
 #include <boost/variant.hpp>
 
 namespace elasticsearch {
 
 template<class T>
-struct result_t : public boost::variant<T, boost::system::error_code> {};
+struct result_t : public boost::variant<T, boost::system::error_code> {
+    typedef boost::variant<T, boost::system::error_code> base_type;
+
+    template<typename C>
+    explicit result_t(C&& c) :
+        base_type(std::forward<C>(c))
+    {}
+};
 
 template<>
-struct result_t<void> : public boost::variant<boost::system::error_code> {};
+struct result_t<void> : public boost::variant<boost::system::error_code> {
+    typedef boost::variant<boost::system::error_code> base_type;
+
+    template<typename C>
+    explicit result_t(C&& c) :
+        base_type(std::forward<C>(c))
+    {}
+};
 
 } // namespace elasticsearch
