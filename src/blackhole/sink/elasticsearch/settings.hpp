764dc49942b7544773b41bee4206b98f8b1460c3
Evgeny Safronov <division494@gmail.com>
2014-05-12 16:27:41 +0400

    [Elasticsearch] More settings added. Added defaults.

--- src/blackhole/sink/elasticsearch/settings.hpp
+++ src/blackhole/sink/elasticsearch/settings.hpp
@@ -7,29 +7,42 @@
 
 namespace elasticsearch {
 
+namespace defaults {
+
+typedef boost::asio::ip::tcp::endpoint endpoint_type;
+
+const endpoint_type endpoint(boost::asio::ip::address_v4(), 9200);
+
+} // namespace defaults
+
 struct settings_t {
     typedef boost::asio::ip::tcp::endpoint endpoint_type;
 
     std::string index;
     std::vector<endpoint_type> endpoints;
-    struct {
+
+    struct sniffer_t {
+        int retries;
         struct {
             bool start;
             bool error;
         } when;
+
+        long timeout;
+        long invertal;
     } sniffer;
+
+    struct limits_t {
+        int connections;
+        long timeout;
+    } limits;
+
 
     settings_t() :
         index("log"),
-        endpoints(std::vector<endpoint_type>({
-            { endpoint_type(boost::asio::ip::address_v4(), 9200) }
-        })),
-        sniffer({
-            {
-                true,
-                true
-            }
-        })
+        endpoints(std::vector<endpoint_type>({ defaults::endpoint })),
+        sniffer({ 3, { true, true }, 10, 60000 }),
+        limits({ 20, 30000 })
     {}
 };
 
