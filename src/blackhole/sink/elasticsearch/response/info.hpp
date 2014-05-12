2b61e855d9390a01ff3dd72f719e3573d950302a
Evgeny Safronov <division494@gmail.com>
2014-05-12 13:27:00 +0400

    [Elasticsearch] Actual nodes info unpacking.

--- src/blackhole/sink/elasticsearch/response/info.hpp
+++ src/blackhole/sink/elasticsearch/response/info.hpp
@@ -21,9 +21,27 @@ struct nodes_info_t {
 } // namespace response
 
 template<>
+struct extractor_t<response::node_t> {
+    static response::node_t extract(const rapidjson::Value& object) {
+        aux::extract_helper_t extract(object);
+        response::node_t node;
+        extract.find(
+            filter::endswith_t { "_address" },
+            mapped::substring_t { "_address" },
+            node.addresses
+        );
+        return node;
+    }
+};
+
+template<>
 struct extractor_t<response::nodes_info_t> {
-    static response::nodes_info_t extract(const rapidjson::Value&) {
-        return response::nodes_info_t();
+    static response::nodes_info_t extract(const rapidjson::Value& object) {
+        aux::extract_helper_t extract(object);
+        response::nodes_info_t nodes_info;
+        extract.to("cluster_name", nodes_info.cluster_name);
+        extract.to("nodes", nodes_info.nodes);
+        return nodes_info;
     }
 };
 
