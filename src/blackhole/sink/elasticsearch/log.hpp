f7941906fb94cebaf89a650c0fcba6717c4e359c
Evgeny Safronov <division494@gmail.com>
2014-05-14 18:51:35 +0400

    [Fix] Starting from global namespace.

--- src/blackhole/sink/elasticsearch/log.hpp
+++ src/blackhole/sink/elasticsearch/log.hpp
@@ -7,8 +7,8 @@
 #include "blackhole/synchronized.hpp"
 
 #define LOG(__log__, ...) \
-    if (blackhole::log::record_t record = __log__.open_record()) \
-        blackhole::aux::make_scoped_pump(__log__, record, __VA_ARGS__)
+    if (::blackhole::log::record_t record = __log__.open_record()) \
+        ::blackhole::aux::make_scoped_pump(__log__, record, __VA_ARGS__)
 
 namespace elasticsearch {
 
