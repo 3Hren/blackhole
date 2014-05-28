e158d93bd5b94887a32c5fbec6419da53ee26784
Evgeny Safronov <division494@gmail.com>
2014-05-28 18:56:18 +0400

    [Testing] Simplify.

--- src/tests/extra/ElasticsearchSink.cpp
+++ src/tests/extra/ElasticsearchSink.cpp
@@ -3,16 +3,20 @@
 #include "../global.hpp"
 
 using namespace blackhole;
-using blackhole::sink::elasticsearch_t;
 
 TEST(elasticsearch_t, Class) {
+    using blackhole::sink::elasticsearch_t;
     elasticsearch_t sink;
     UNUSED(sink);
 }
 
 TEST(elasticsearch_t, Manual) {
+    using blackhole::sink::elasticsearch_t;
     elasticsearch_t sink;
+    std::string msg = "{}";
+    boost::algorithm::replace_all(msg, "'", "\"");
     for (int i = 0; i < 200; ++i) {
-        sink.consume((boost::format("{\"key\":%d}") % i).str());
+        sink.consume(msg);
     }
 }
+
