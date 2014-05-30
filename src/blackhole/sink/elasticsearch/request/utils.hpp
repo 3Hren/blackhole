7d033a67b33cec08a90873d6ab2ff4e6874a96bd
Evgeny Safronov <division494@gmail.com>
2014-05-30 23:49:43 +0400

    [Elasticsearch] Passing by const reference instead of value.

--- src/blackhole/sink/elasticsearch/request/utils.hpp
+++ src/blackhole/sink/elasticsearch/request/utils.hpp
@@ -14,6 +14,7 @@ inline void substitute(std::ostringstream&) {}
 template<typename... Args>
 inline void substitute(std::ostringstream& stream,
                        const std::string arg,
+                       const std::string& arg,
                        const Args&... args) {
     if (!arg.empty()) {
         stream << "/" << arg;
