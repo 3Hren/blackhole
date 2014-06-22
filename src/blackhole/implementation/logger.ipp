c1230fcc7982d0d66525ca2be4736448be5b18b0
Evgeny Safronov <division494@gmail.com>
2014-06-22 23:50:34 +0400

    [Aux] Massive refactoring done over main logger class.

---
+++ src/blackhole/implementation/logger.ipp
@@ -0,0 +1,197 @@
+#pragma once
+
+#include "blackhole/config.hpp"
+#include "blackhole/utils/noexcept.hpp"
+
+namespace blackhole {
+
+namespace aux {
+
+namespace guard {
+
+inline void no_deleter(scoped_attributes_concept_t*) { }
+
+} // namespace guard
+
+} //namespace detail
+
+BLACKHOLE_DECL
+logger_base_t::logger_base_t() :
+    m_enabled(true),
+    m_filter(default_filter_t::instance()),
+    m_exception_handler(log::default_exception_handler_t()),
+    m_scoped_attributes(&aux::guard::no_deleter)
+{}
+
+BLACKHOLE_DECL
+logger_base_t::logger_base_t(logger_base_t&& other) BLACKHOLE_NOEXCEPT {
+    *this = std::move(other);
+}
+
+BLACKHOLE_DECL
+logger_base_t&
+logger_base_t::operator=(logger_base_t&& other) BLACKHOLE_NOEXCEPT {
+    swap(*this, other);
+    return *this;
+}
+
+BLACKHOLE_DECL
+bool
+logger_base_t::enabled() const {
+    return m_enabled;
+}
+
+BLACKHOLE_DECL
+void
+logger_base_t::enable() {
+    m_enabled = true;
+}
+
+BLACKHOLE_DECL
+void
+logger_base_t::disable() {
+    m_enabled = false;
+}
+
+BLACKHOLE_DECL
+void
+logger_base_t::set_filter(filter_t&& filter) {
+    m_filter = std::move(filter);
+}
+
+BLACKHOLE_DECL
+void
+logger_base_t::add_attribute(const log::attribute_pair_t& attribute) {
+    m_global_attributes.insert(attribute);
+}
+
+BLACKHOLE_DECL
+void
+logger_base_t::add_frontend(std::unique_ptr<base_frontend_t> frontend) {
+    m_frontends.push_back(std::move(frontend));
+}
+
+BLACKHOLE_DECL
+void
+logger_base_t::set_exception_handler(log::exception_handler_t&& handler) {
+    m_exception_handler = std::move(handler);
+}
+
+BLACKHOLE_DECL
+log::record_t
+logger_base_t::open_record() const {
+    return open_record(log::attributes_t());
+}
+
+BLACKHOLE_DECL
+log::record_t
+logger_base_t::open_record(log::attribute_pair_t local_attribute) const {
+    return open_record(log::attributes_t({ std::move(local_attribute) }));
+}
+
+BLACKHOLE_DECL
+log::record_t
+logger_base_t::open_record(log::attributes_t local_attributes) const {
+    if (enabled() && !m_frontends.empty()) {
+        log::attributes_t attributes = merge({
+            universe_storage_t::instance().dump(),  // Program global.
+            get_thread_attributes(),                // Thread local.
+            m_global_attributes,                    // Logger object specific.
+            get_event_attributes(),                 // Event specific, e.g. timestamp.
+            std::move(local_attributes),            // Any user attributes.
+            m_scoped_attributes.get() ? m_scoped_attributes->attributes() : log::attributes_t()
+        });
+
+        if (m_filter(attributes)) {
+            log::record_t record;
+            record.attributes = std::move(attributes);
+            return record;
+        }
+    }
+
+    return log::record_t();
+}
+
+BLACKHOLE_DECL
+void
+logger_base_t::push(log::record_t&& record) const {
+    for (auto it = m_frontends.begin(); it != m_frontends.end(); ++it) {
+        try {
+            const std::unique_ptr<base_frontend_t>& frontend = *it;
+            frontend->handle(record);
+        } catch (...) {
+            m_exception_handler();
+        }
+    }
+}
+
+BLACKHOLE_DECL
+log::attributes_t
+logger_base_t::get_event_attributes() const {
+    timeval tv;
+    gettimeofday(&tv, nullptr);
+    log::attributes_t attributes = {
+        keyword::timestamp() = tv
+    };
+    return attributes;
+}
+
+BLACKHOLE_DECL
+log::attributes_t
+logger_base_t::get_thread_attributes() const {
+    log::attributes_t attributes = {
+        keyword::tid() = this_thread::get_id<std::string>()
+    };
+    return attributes;
+}
+
+BLACKHOLE_DECL
+scoped_attributes_concept_t::scoped_attributes_concept_t(logger_base_t& log) :
+    m_logger(&log),
+    m_previous(log.m_scoped_attributes.get())
+{
+    log.m_scoped_attributes.reset(this);
+}
+
+BLACKHOLE_DECL
+scoped_attributes_concept_t::~scoped_attributes_concept_t() {
+    BOOST_ASSERT(m_logger);
+    BOOST_ASSERT(m_logger->m_scoped_attributes.get() == this);
+    m_logger->m_scoped_attributes.reset(m_previous);
+}
+
+BLACKHOLE_DECL
+bool
+scoped_attributes_concept_t::has_parent() const {
+    return m_previous;
+}
+
+BLACKHOLE_DECL
+const scoped_attributes_concept_t&
+scoped_attributes_concept_t::parent() const {
+    return *m_previous;
+}
+
+BLACKHOLE_DECL
+void swap(logger_base_t& lhs, logger_base_t& rhs) BLACKHOLE_NOEXCEPT {
+    using std::swap;
+    std::swap(lhs.m_enabled, rhs.m_enabled);
+    std::swap(lhs.m_filter, rhs.m_filter);
+    std::swap(lhs.m_exception_handler, rhs.m_exception_handler);
+    std::swap(lhs.m_frontends, rhs.m_frontends);
+    std::swap(lhs.m_global_attributes, rhs.m_global_attributes);
+
+    auto lhs_operation_attributes = lhs.m_scoped_attributes.get();
+    lhs.m_scoped_attributes.reset(rhs.m_scoped_attributes.get());
+    rhs.m_scoped_attributes.reset(lhs_operation_attributes);
+
+    if (lhs.m_scoped_attributes.get()) {
+        lhs.m_scoped_attributes->m_logger = &lhs;
+    }
+
+    if (rhs.m_scoped_attributes.get()) {
+        rhs.m_scoped_attributes->m_logger = &rhs;
+    }
+}
+
+} // namespace blackhole
--- src/blackhole/logger.hpp
+++ src/blackhole/logger.hpp
@@ -24,12 +24,6 @@ namespace blackhole {
 
 class scoped_attributes_concept_t;
 
-namespace detail {
-
-inline void dummy_guard_deleter(scoped_attributes_concept_t*) { }
-
-} //namespace detail
-
 class logger_base_t {
     bool m_enabled;
 
@@ -48,116 +42,31 @@ protected:
     friend void swap(logger_base_t&, logger_base_t&) BLACKHOLE_NOEXCEPT;
 
 public:
-    logger_base_t() :
-        m_enabled(true),
-        m_filter(default_filter_t::instance()),
-        m_exception_handler(log::default_exception_handler_t()),
-        m_scoped_attributes(&detail::dummy_guard_deleter)
-    {}
+    logger_base_t();
 
-    // Blaming GCC 4.4 - it needs explicit move constructor definition,
-    // because it cannot define default move constructor for derived class.
-    logger_base_t(logger_base_t&& other) BLACKHOLE_NOEXCEPT {
-        *this = std::move(other);
-    }
+    //! @compat GCC4.4
+    //! Blaming GCC4.4 - it needs explicit move constructor definition,
+    //! because it cannot define default move constructor for derived class.
+    logger_base_t(logger_base_t&& other) BLACKHOLE_NOEXCEPT;
+    logger_base_t& operator=(logger_base_t&& other) BLACKHOLE_NOEXCEPT;
 
-    logger_base_t& operator=(logger_base_t&& other) BLACKHOLE_NOEXCEPT {
-        swap(*this, other);
-        return *this;
-    }
+    bool enabled() const;
+    void enable();
+    void disable();
+    void set_filter(filter_t&& filter);
+    void add_attribute(const log::attribute_pair_t& attribute);
+    void add_frontend(std::unique_ptr<base_frontend_t> frontend);
+    void set_exception_handler(log::exception_handler_t&& handler);
 
-    bool enabled() const {
-        return m_enabled;
-    }
+    log::record_t open_record() const;
+    log::record_t open_record(log::attribute_pair_t local_attribute) const;
+    log::record_t open_record(log::attributes_t local_attributes) const;
 
-    void enable() {
-        m_enabled = true;
-    }
-
-    void disable() {
-        m_enabled = false;
-    }
-
-    void set_filter(filter_t&& filter) {
-        m_filter = std::move(filter);
-    }
-
-    void add_attribute(const log::attribute_pair_t& attr) {
-        m_global_attributes.insert(attr);
-    }
-
-    void add_frontend(std::unique_ptr<base_frontend_t> frontend) {
-        m_frontends.push_back(std::move(frontend));
-    }
-
-    void set_exception_handler(log::exception_handler_t&& handler) {
-        m_exception_handler = std::move(handler);
-    }
-
-    log::record_t open_record() const {
-        return open_record(log::attributes_t());
-    }
-
-    log::record_t open_record(log::attribute_pair_t local_attribute) const {
-        return open_record(log::attributes_t({ std::move(local_attribute) }));
-    }
-
-    log::record_t open_record(log::attributes_t local_attributes) const {
-        return open_record(std::move(local_attributes), m_scoped_attributes);
-    }
-
-protected:
-    template<class ScopedAttributes>
-    log::record_t open_record(log::attributes_t&& local_attributes,
-                              const ScopedAttributes& scoped_attributes) const {
-        if (enabled() && !m_frontends.empty()) {
-            log::attributes_t attributes = merge({
-                universe_storage_t::instance().dump(),  // Program global.
-                get_thread_attributes(),                // Thread local.
-                m_global_attributes,                    // Logger object specific.
-                get_event_attributes(),                 // Event specific, e.g. timestamp.
-                std::move(local_attributes),            // Any user attributes.
-                scoped_attributes.get() ? scoped_attributes->attributes() : log::attributes_t()
-            });
-
-            if (m_filter(attributes)) {
-                log::record_t record;
-                record.attributes = std::move(attributes);
-                return record;
-            }
-        }
-
-        return log::record_t();
-    }
-
-public:
-    void push(log::record_t&& record) const {
-        for (auto it = m_frontends.begin(); it != m_frontends.end(); ++it) {
-            try {
-                const std::unique_ptr<base_frontend_t>& frontend = *it;
-                frontend->handle(record);
-            } catch (...) {
-                m_exception_handler();
-            }
-        }
-    }
+    void push(log::record_t&& record) const;
 
 private:
-    log::attributes_t get_event_attributes() const {
-        timeval tv;
-        gettimeofday(&tv, nullptr);
-        log::attributes_t attributes = {
-            keyword::timestamp() = tv
-        };
-        return attributes;
-    }
-
-    log::attributes_t get_thread_attributes() const {
-        log::attributes_t attributes = {
-            keyword::tid() = this_thread::get_id<std::string>()
-        };
-        return attributes;
-    }
+    log::attributes_t get_event_attributes() const;
+    log::attributes_t get_thread_attributes() const;
 };
 
 // NOTE: It's not movable to avoid moving to another thread.
@@ -170,55 +79,15 @@ class scoped_attributes_concept_t {
     friend void swap(logger_base_t&, logger_base_t&) BLACKHOLE_NOEXCEPT;
 
 public:
-    scoped_attributes_concept_t(logger_base_t &logger) :
-        m_logger(&logger),
-        m_previous(logger.m_scoped_attributes.get())
-    {
-        logger.m_scoped_attributes.reset(this);
-    }
+    scoped_attributes_concept_t(logger_base_t& log);
+    ~scoped_attributes_concept_t();
 
-    ~scoped_attributes_concept_t() {
-        BOOST_ASSERT(m_logger);
-        BOOST_ASSERT(m_logger->m_scoped_attributes.get() == this);
-        m_logger->m_scoped_attributes.reset(m_previous);
-    }
-
-    virtual
-    const log::attributes_t&
-    attributes() const = 0;
+    virtual const log::attributes_t& attributes() const = 0;
 
 protected:
-    bool
-    has_parent() const {
-        return m_previous;
-    }
-
-    const scoped_attributes_concept_t&
-    parent() const {
-        return *m_previous;
-    }
+    bool has_parent() const;
+    const scoped_attributes_concept_t& parent() const;
 };
-
-inline void swap(logger_base_t& lhs, logger_base_t& rhs) BLACKHOLE_NOEXCEPT {
-    using std::swap;
-    std::swap(lhs.m_enabled, rhs.m_enabled);
-    std::swap(lhs.m_filter, rhs.m_filter);
-    std::swap(lhs.m_exception_handler, rhs.m_exception_handler);
-    std::swap(lhs.m_frontends, rhs.m_frontends);
-    std::swap(lhs.m_global_attributes, rhs.m_global_attributes);
-
-    auto lhs_operation_attributes = lhs.m_scoped_attributes.get();
-    lhs.m_scoped_attributes.reset(rhs.m_scoped_attributes.get());
-    rhs.m_scoped_attributes.reset(lhs_operation_attributes);
-
-    if (lhs.m_scoped_attributes.get()) {
-        lhs.m_scoped_attributes->m_logger = &lhs;
-    }
-
-    if (rhs.m_scoped_attributes.get()) {
-        rhs.m_scoped_attributes->m_logger = &rhs;
-    }
-}
 
 template<typename Level>
 class verbose_logger_t : public logger_base_t {
@@ -250,3 +119,7 @@ public:
 };
 
 } // namespace blackhole
+
+#if defined(BLACKHOLE_HEADER_ONLY)
+#include "blackhole/implementation/logger.ipp"
+#endif
