b122f94126060fc1dde747bee022100b0bd176a8
Evgeny Safronov <division494@gmail.com>
2014-06-22 23:22:57 +0400

    [Config] Function/method export macro variable has been added.

--- src/blackhole/config.hpp
+++ src/blackhole/config.hpp
@@ -6,4 +6,13 @@
 #define BLACKHOLE_VERSION_PATCH 0
 #define BLACKHOLE_DEBUG
 
+#if defined(BLACKHOLE_HEADER_ONLY)
+    #define BLACKHOLE_DECL inline
+#endif
+
+// If BLACKHOLE_DECL isn't defined yet define it now.
+#if !defined(BLACKHOLE_DECL)
+    #define BLACKHOLE_DECL
+#endif
+
 #endif // CONFIG_H
