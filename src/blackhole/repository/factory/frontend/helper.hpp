edfd589e361c225fcdb0da8d4cbac91cf43f9251
Evgeny Safronov <division494@gmail.com>
2014-07-28 11:32:03 +0400

    [Code Style] Style.

--- src/blackhole/repository/factory/frontend/helper.hpp
+++ src/blackhole/repository/factory/frontend/helper.hpp
@@ -28,12 +28,16 @@ create(const formatter_config_t& formatter_config, std::unique_ptr<Sink> sink);
 template<class Sink>
 static
 std::unique_ptr<base_frontend_t>
-create(const frontend_factory_t& factory, const formatter_config_t& formatter_config, std::unique_ptr<Sink> sink);
+create(const frontend_factory_t& factory,
+       const formatter_config_t& formatter_config,
+       std::unique_ptr<Sink> sink);
 
 template<class Sink>
 static
 std::unique_ptr<base_frontend_t>
-create(const frontend_factory_t& factory, const formatter_config_t& formatter_config, const sink_config_t& sink_config);
+create(const frontend_factory_t& factory,
+       const formatter_config_t& formatter_config,
+       const sink_config_t& sink_config);
 
 } // namespace frontend
 
@@ -54,7 +58,9 @@ namespace frontend {
 template<class Formatter, class Sink>
 std::unique_ptr<base_frontend_t>
 create(std::unique_ptr<Formatter> formatter, std::unique_ptr<Sink> sink) {
-    return utils::make_unique<frontend_t<Formatter, Sink>>(std::move(formatter), std::move(sink));
+    return utils::make_unique<
+        frontend_t<Formatter, Sink>
+    >(std::move(formatter), std::move(sink));
 }
 
 template<class Formatter, class Sink>
@@ -68,13 +74,19 @@ create(const formatter_config_t& formatter_config, std::unique_ptr<Sink> sink) {
 
 template<class Sink>
 std::unique_ptr<base_frontend_t>
-create(const frontend_factory_t& factory, const formatter_config_t& formatter_config, std::unique_ptr<Sink> sink) {
+create(const frontend_factory_t& factory,
+       const formatter_config_t& formatter_config,
+       std::unique_ptr<Sink> sink)
+{
     return factory.create(formatter_config, std::move(sink));
 }
 
 template<class Sink>
 std::unique_ptr<base_frontend_t>
-create(const frontend_factory_t& factory, const formatter_config_t& formatter_config, const sink_config_t& sink_config) {
+create(const frontend_factory_t& factory,
+       const formatter_config_t& formatter_config,
+       const sink_config_t& sink_config)
+{
     auto config = aux::config_mapper<Sink>::map(sink_config.config());
     auto sink = utils::make_unique<Sink>(config);
     return create(factory, formatter_config, std::move(sink));
