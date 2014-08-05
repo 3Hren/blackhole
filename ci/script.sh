a48183b6258b6c6229cf26bead78a1dc59e6635a
Evgeny Safronov <division494@gmail.com>
2014-08-05 19:26:23 +0400

    [CI] Enable benchmarking.

--- ci/script.sh
+++ ci/script.sh
@@ -2,11 +2,13 @@
 
 if [ "$SUITE" = "tests" ]; then
     echo "cmake -DENABLE_ELASTICSEARCH=ON -DENABLE_TESTING=ON -DENABLE_EXAMPLES=OFF .. && make"
-    cmake -DENABLE_ELASTICSEARCH=ON -DENABLE_TESTING=ON -DENABLE_EXAMPLES=OFF .. && make
+    cmake -DENABLE_ELASTICSEARCH=ON -DENABLE_TESTING=ON .. && make
     ./src/tests/blackhole-tests
-elif [ "$SUITE" = "examples" ]; then
-    echo "cmake -DENABLE_ELASTICSEARCH=ON -DENABLE_TESTING=OFF -DENABLE_EXAMPLES=ON .. && make"
-    cmake -DENABLE_ELASTICSEARCH=ON -DENABLE_TESTING=OFF -DENABLE_EXAMPLES=ON .. && make
+elif [ "$SUITE" = "examples" ]; then    
+    cmake -DENABLE_ELASTICSEARCH=ON -DENABLE_EXAMPLES=ON .. && make
+elif [ "$SUITE" = "benchmarks" ]; then
+    cmake -DENABLE_ELASTICSEARCH=ON -DENABLE_BENCHMARKING=ON .. && make
+    ./src/benchmark/blackhole-benchmark
 else
     echo "Unknown suite variable - '$SUITE'. Terminating ..."
     exit 1
