ff1e7509ff9076cfe32712658a8cd942dcb02a29
Evgeny Safronov <division494@gmail.com>
2014-08-17 17:10:14 +0400

    [Aux] Platform macro update.

--- src/blackhole/utils/noexcept.hpp
+++ src/blackhole/utils/noexcept.hpp
@@ -2,8 +2,8 @@
 
 #include "blackhole/platform.hpp"
 
-#if defined(__clang__) || defined(BLACKHOLE_HAVE_AT_LEAST_GCC46)
-#define BLACKHOLE_NOEXCEPT noexcept
+#if defined(BLACKHOLE_HAVE_CLANG) || defined(BLACKHOLE_HAVE_AT_LEAST_GCC46)
+    #define BLACKHOLE_NOEXCEPT noexcept
 #else
-#define BLACKHOLE_NOEXCEPT throw()
+    #define BLACKHOLE_NOEXCEPT throw()
 #endif
