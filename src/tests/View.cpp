fb3faeceff874b549cd051ab890b4ca3af6169e2
Evgeny Safronov <division494@gmail.com>
2014-08-19 15:50:43 +0400

    [Unit Testing] Refactoring.

--- src/tests/View.cpp
+++ src/tests/View.cpp
@@ -5,24 +5,13 @@
 
 #include "global.hpp"
 
-using namespace blackhole::aux::iterator;
+using blackhole::aux::iterator::join_t;
+using blackhole::aux::iterator::invalidate_tag;
 
-TEST(join_t, DefaultConstructor) {
-    typedef std::vector<int> container_type;
-    join_t<container_type, true> it;
-    UNUSED(it);
-}
+#define TEST_JOIN_ITERATOR(Suite, Case) \
+    TEST(join_t##_##Suite, Case)
 
-TEST(join_t, EmptyIteratorEqualsIteratorWithEmptyContainers) {
-    typedef std::vector<int> container_type;
-    container_type c1, c2;
-
-    join_t<container_type, true> it1;
-    join_t<container_type, true> it2({ &c1, &c2 });
-    EXPECT_TRUE(it1 == it2);
-}
-
-TEST(join_t, ConvertingConstructor) {
+TEST_JOIN_ITERATOR(Cons, Converting) {
     typedef std::vector<int> container_type;
     container_type c1, c2;
 
@@ -30,17 +19,72 @@ TEST(join_t, ConvertingConstructor) {
     UNUSED(it);
 }
 
-TEST(join_t, Forward) {
+TEST_JOIN_ITERATOR(Cons, Invalidation) {
+    typedef std::vector<int> container_type;
+    container_type c1, c2;
+
+    join_t<container_type, true> it({ &c1, &c2 }, invalidate_tag);
+    UNUSED(it);
+}
+
+TEST_JOIN_ITERATOR(Cons, Copy) {
     typedef std::vector<int> container_type;
     container_type c1 = { 0, 1, 2 };
     container_type c2 = { 3, 4, 5 };
 
-    join_t<container_type, true> it({ &c1, &c2 });
+    join_t<container_type, true> it1({ &c1, &c2 });
+    join_t<container_type, true> it2 = it1;
+    EXPECT_TRUE(it1 == it2);
+
     container_type expected = { 0, 1, 2, 3, 4, 5 };
-    EXPECT_TRUE(std::equal(expected.begin(), expected.end(), it));
+    EXPECT_TRUE(std::equal(expected.begin(), expected.end(), it2));
 }
 
-TEST(join_t, ForwardWithEmptyFirst) {
+TEST_JOIN_ITERATOR(Cons, Move) {
+    typedef std::vector<int> container_type;
+    container_type c1 = { 0, 1, 2 };
+    container_type c2 = { 3, 4, 5 };
+
+    join_t<container_type, true> it1({ &c1, &c2 });
+    join_t<container_type, true> it2 = std::move(it1);
+
+    container_type expected = { 0, 1, 2, 3, 4, 5 };
+    EXPECT_TRUE(std::equal(expected.begin(), expected.end(), it2));
+}
+
+TEST_JOIN_ITERATOR(Equality, EmptyContainers) {
+    typedef std::vector<int> container_type;
+    container_type c1, c2;
+
+    join_t<container_type, true> it1({ &c1, &c2 });
+    join_t<container_type, true> it2({ &c1, &c2 });
+    EXPECT_TRUE(it1 == it2);
+    EXPECT_FALSE(it1 != it2);
+}
+
+TEST_JOIN_ITERATOR(Equality, EqualContainers) {
+    typedef std::vector<int> container_type;
+    container_type c1 = { 0, 1, 2 };
+    container_type c2 = { 0, 1, 2 };
+
+    join_t<container_type, true> it1({ &c1, &c2 });
+    join_t<container_type, true> it2({ &c1, &c2 });
+    EXPECT_TRUE(it1 == it2);
+    EXPECT_FALSE(it1 != it2);
+}
+
+TEST_JOIN_ITERATOR(Equality, NotEqualContainers) {
+    typedef std::vector<int> container_type;
+    container_type c1 = { 0, 1, 2 };
+    container_type c2 = { 0, 1, 2 };
+
+    join_t<container_type, true> it1({ &c1, &c2 });
+    join_t<container_type, true> it2({ &c2, &c1 });
+    EXPECT_FALSE(it1 == it2);
+    EXPECT_TRUE(it1 != it2);
+}
+
+TEST_JOIN_ITERATOR(Forward, EmptyFirst) {
     typedef std::vector<int> container_type;
     container_type c1 = {};
     container_type c2 = { 3, 4, 5 };
@@ -50,17 +94,28 @@ TEST(join_t, ForwardWithEmptyFirst) {
     EXPECT_TRUE(std::equal(expected.begin(), expected.end(), it));
 }
 
-TEST(join_t, ForwardWithEmptyLast) {
+TEST_JOIN_ITERATOR(Forward, EmptyMiddle) {
     typedef std::vector<int> container_type;
-    container_type c1 = { 1, 2, 3 };
+    container_type c1 = { 0, 1, 2 };
     container_type c2 = {};
+    container_type c3 = { 3, 4, 5 };
 
-    join_t<container_type, true> it({ &c1, &c2 });
-    container_type expected = { 1, 2, 3 };
+    join_t<container_type, true> it({ &c1, &c2, &c3 });
+    container_type expected = { 0, 1, 2, 3, 4, 5 };
     EXPECT_TRUE(std::equal(expected.begin(), expected.end(), it));
 }
 
-TEST(join_t, ForwardLoop) {
+TEST_JOIN_ITERATOR(Forward, EmptyLast) {
+    typedef std::vector<int> container_type;
+    container_type c1 = { 0, 1, 2 };
+    container_type c2 = {};
+
+    join_t<container_type, true> it({ &c1, &c2 });
+    container_type expected = { 0, 1, 2 };
+    EXPECT_TRUE(std::equal(expected.begin(), expected.end(), it));
+}
+
+TEST_JOIN_ITERATOR(Forward, Loop) {
     typedef std::vector<int> container_type;
     container_type c1 = { 0, 1, 2 };
     container_type c2 = { 3, 4, 5 };
@@ -74,7 +129,7 @@ TEST(join_t, ForwardLoop) {
     }
 }
 
-TEST(join_t, ForwardLoopPostIncrement) {
+TEST_JOIN_ITERATOR(Forward, LoopPostIncrement) {
     typedef std::vector<int> container_type;
     container_type c1 = { 0, 1, 2 };
     container_type c2 = { 3, 4, 5 };
@@ -88,7 +143,7 @@ TEST(join_t, ForwardLoopPostIncrement) {
     }
 }
 
-TEST(join_t, ForwardMap) {
+TEST_JOIN_ITERATOR(Forward, Map) {
     typedef std::unordered_map<std::string, int> container_type;
     container_type c1 = { {"0", 0}, {"1", 1}, {"2", 2} };
     container_type c2 = { {"3", 0}, {"4", 1}, {"5", 2} };
