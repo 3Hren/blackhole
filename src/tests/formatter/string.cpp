1d2623cbc7c52873508771d2f1670e71db9cc91f
Evgeny Safronov <division494@gmail.com>
2014-08-28 14:29:29 +0400

    [Code Style] Reordering.

--- src/tests/formatter/string.cpp
+++ src/tests/formatter/string.cpp
@@ -48,42 +48,6 @@ TEST_STRING(Required, ThrowsExceptionWhenAttributeNotFound) {
     EXPECT_THROW(fmt.format(record), blackhole::error_t);
 }
 
-TEST(string_t, FormatOtherLocalAttribute) {
-    record_t record;
-    record.insert({ "uuid", attribute_t("123-456") });
-
-    std::string pattern("[%(...L)s]");
-    formatter::string_t formatter(pattern);
-    EXPECT_EQ("['uuid': '123-456']", formatter.format(record));
-}
-
-TEST(string_t, FormatOtherLocalAttributes) {
-    record_t record;
-    record.insert({ "uuid", attribute_t("123-456") });
-    record.insert({ "answer to life the universe and everything", attribute_t(42) });
-    std::string pattern("[%(...L)s]");
-    formatter::string_t formatter(pattern);
-    std::string actual = formatter.format(record);
-    EXPECT_TRUE(actual.find("'answer to life the universe and everything': 42") != std::string::npos);
-    EXPECT_TRUE(actual.find("'uuid': '123-456'") != std::string::npos);
-}
-
-TEST(string_t, ComplexFormatWithOtherLocalAttributes) {
-    record_t record;
-    record.insert({ "timestamp", attribute_t("1960-01-01 00:00:00", attribute::scope_t::event) });
-    record.insert({ "level", attribute_t("INFO", attribute::scope_t::event) });
-    record.insert(keyword::message() = "le message");
-    record.insert({ "uuid", attribute_t("123-456") });
-    record.insert({ "answer to life the universe and everything", attribute_t(42) });
-
-    std::string pattern("[%(timestamp)s] [%(level)s]: %(message)s [%(...L)s]");
-    formatter::string_t formatter(pattern);
-    std::string actual = formatter.format(record);
-    EXPECT_TRUE(boost::starts_with(actual, "[1960-01-01 00:00:00] [INFO]: le message ["));
-    EXPECT_TRUE(actual.find("'answer to life the universe and everything': 42") != std::string::npos);
-    EXPECT_TRUE(actual.find("'uuid': '123-456'") != std::string::npos);
-}
-
 namespace testing {
 
 void map_timestamp(blackhole::aux::attachable_ostringstream& stream, const timeval& tv) {
@@ -296,6 +260,38 @@ TEST_STRING(Optional, AbsentWithPrefixSuffixSquareBracketsReversed) {
     EXPECT_EQ("<>: [le message]", fmt.format(record));
 }
 
-TEST_STRING(Variadic, _) {
+TEST(string_t, FormatOtherLocalAttribute) {
+    record_t record;
+    record.insert({ "uuid", attribute_t("123-456") });
 
+    std::string pattern("[%(...L)s]");
+    formatter::string_t formatter(pattern);
+    EXPECT_EQ("['uuid': '123-456']", formatter.format(record));
+}
+
+TEST(string_t, FormatOtherLocalAttributes) {
+    record_t record;
+    record.insert({ "uuid", attribute_t("123-456") });
+    record.insert({ "answer to life the universe and everything", attribute_t(42) });
+    std::string pattern("[%(...L)s]");
+    formatter::string_t formatter(pattern);
+    std::string actual = formatter.format(record);
+    EXPECT_TRUE(actual.find("'answer to life the universe and everything': 42") != std::string::npos);
+    EXPECT_TRUE(actual.find("'uuid': '123-456'") != std::string::npos);
+}
+
+TEST(string_t, ComplexFormatWithOtherLocalAttributes) {
+    record_t record;
+    record.insert({ "timestamp", attribute_t("1960-01-01 00:00:00", attribute::scope_t::event) });
+    record.insert({ "level", attribute_t("INFO", attribute::scope_t::event) });
+    record.insert(keyword::message() = "le message");
+    record.insert({ "uuid", attribute_t("123-456") });
+    record.insert({ "answer to life the universe and everything", attribute_t(42) });
+
+    std::string pattern("[%(timestamp)s] [%(level)s]: %(message)s [%(...L)s]");
+    formatter::string_t formatter(pattern);
+    std::string actual = formatter.format(record);
+    EXPECT_TRUE(boost::starts_with(actual, "[1960-01-01 00:00:00] [INFO]: le message ["));
+    EXPECT_TRUE(actual.find("'answer to life the universe and everything': 42") != std::string::npos);
+    EXPECT_TRUE(actual.find("'uuid': '123-456'") != std::string::npos);
 }
