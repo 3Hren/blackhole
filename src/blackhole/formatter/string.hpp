62530a1de5124d6218df9e003e784485d5dc9809
Evgeny Safronov <division494@gmail.com>
2014-09-01 19:30:48 +0400

    [Formatter.String] Introducing brand new pattern parser.
    This commit closes #6.

--- src/blackhole/formatter/string.hpp
+++ src/blackhole/formatter/string.hpp
@@ -12,6 +12,7 @@
 #include "blackhole/formatter/map/value.hpp"
 #include "blackhole/formatter/string/builder.hpp"
 #include "blackhole/formatter/string/config.hpp"
+#include "blackhole/formatter/string/parser.hpp"
 #include "blackhole/record.hpp"
 #include "blackhole/repository/factory/traits.hpp"
 
@@ -19,9 +20,66 @@ namespace blackhole {
 
 namespace formatter {
 
+class visitor_t : public boost::static_visitor<> {
+    typedef aux::attachable_ostringstream stickystream_t;
+
+    stickystream_t& stream;
+    const mapping::value_t& mapper;
+    const attribute::set_view_t& view;
+public:
+    visitor_t(stickystream_t& stream,
+              const mapping::value_t& mapper,
+              const attribute::set_view_t& view) :
+        stream(stream),
+        mapper(mapper),
+        view(view)
+    {}
+
+    void operator()(const string::literal_t& literal) {
+        stream.rdbuf()->storage()->append(literal.value);
+    }
+
+    void operator()(const string::placeholder::required_t& ph) {
+        if (auto attribute = view.find(ph.name)) {
+            mapper(stream, ph.name, attribute->value);
+            return;
+        }
+
+        throw error_t("required attribute '%s' was not provided", ph.name);
+    }
+
+    void operator()(const string::placeholder::optional_t& ph) {
+        if (auto attribute = view.find(ph.name)) {
+            stream.rdbuf()->storage()->append(ph.prefix);
+            mapper(stream, ph.name, attribute->value);
+            stream.rdbuf()->storage()->append(ph.suffix);
+        }
+    }
+
+    void operator()(const string::placeholder::variadic_t& /*ph*/) {
+        std::vector<std::string> passed;
+        passed.reserve(view.upper_size());
+
+        for (auto it = view.begin(); it != view.end(); ++it) {
+            const std::string& name = it->first;
+            const attribute_t& attribute = it->second;
+
+            //!@todo: This code needs some optimization love.
+            std::ostringstream stream;
+            stream << "'" << name << "': ";
+            string::builder::variadic_visitor_t visitor(stream);
+            boost::apply_visitor(visitor, attribute.value);
+            passed.push_back(stream.str());
+        }
+
+        stream.rdbuf()->storage()->append(boost::algorithm::join(passed, ", "));
+    }
+};
+
 class string_t : public base_t {
     const std::string pattern;
     const std::vector<string::builder::type> formatters;
+    std::vector<string::token_t> tokens;
 
 public:
     typedef string::config_t config_type;
@@ -33,21 +91,37 @@ public:
     string_t(const std::string& pattern) :
         pattern(pattern),
         formatters(string::formatter_builder_t::build(pattern))
-    {}
+    {
+        try {
+            string::parser_t parser(pattern);
+            while (true) {
+                tokens.push_back(parser.next());
+            }
+        } catch (const string::parser::exhausted_t&) {
+        }
+    }
 
     string_t(const config_type& config) :
         pattern(config.pattern),
         formatters(string::formatter_builder_t::build(config.pattern))
-    {}
+    {
+        try {
+            string::parser_t parser(pattern);
+            while (true) {
+                tokens.push_back(parser.next());
+            }
+        } catch (const string::parser::exhausted_t&) {
+        }
+    }
 
     std::string format(const record_t& record) const {
         std::string buffer;
         blackhole::aux::attachable_ostringstream stream;
         stream.attach(buffer);
         try {
-            for (auto it = formatters.begin(); it != formatters.end(); ++it) {
-                const string::builder::type& formatter = *it;
-                formatter(stream, mapper, record.attributes());
+            visitor_t visitor(stream, mapper, record.attributes());
+            for (auto it = tokens.begin(); it != tokens.end(); ++it) {
+                boost::apply_visitor(visitor, *it);
                 stream.flush();
             }
             return buffer;
