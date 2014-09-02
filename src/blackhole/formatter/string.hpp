fac8abb3de800516e3566a9a11c40d0df41024f0
Evgeny Safronov <division494@gmail.com>
2014-09-02 15:11:42 +0400

    [Aux] Decomposition over pattern generator.

--- src/blackhole/formatter/string.hpp
+++ src/blackhole/formatter/string.hpp
@@ -11,6 +11,7 @@
 #include "blackhole/formatter/base.hpp"
 #include "blackhole/formatter/map/value.hpp"
 #include "blackhole/formatter/string/config.hpp"
+#include "blackhole/formatter/string/generator.hpp"
 #include "blackhole/formatter/string/parser.hpp"
 #include "blackhole/record.hpp"
 #include "blackhole/repository/factory/traits.hpp"
@@ -18,80 +19,6 @@
 namespace blackhole {
 
 namespace formatter {
-
-class variadic_visitor_t : public boost::static_visitor<> {
-    std::ostringstream& stream;
-
-public:
-    variadic_visitor_t(std::ostringstream& stream) :
-        stream(stream)
-    {}
-
-    template<typename T>
-    void operator()(const T& value) const {
-        stream << value;
-    }
-
-    void operator()(const std::string& value) const {
-        stream << "'" << value << "'";
-    }
-};
-
-
-class visitor_t : public boost::static_visitor<> {
-    blackhole::aux::attachable_ostringstream& stream;
-    const mapping::value_t& mapper;
-    const attribute::set_view_t& view;
-
-public:
-    visitor_t(blackhole::aux::attachable_ostringstream& stream,
-              const mapping::value_t& mapper,
-              const attribute::set_view_t& view) :
-        stream(stream),
-        mapper(mapper),
-        view(view)
-    {}
-
-    void operator()(const string::literal_t& literal) {
-        stream.rdbuf()->storage()->append(literal.value);
-    }
-
-    void operator()(const string::placeholder::required_t& ph) {
-        if (auto attribute = view.find(ph.name)) {
-            mapper(stream, ph.name, attribute->value);
-            return;
-        }
-
-        throw error_t("required attribute '%s' was not provided", ph.name);
-    }
-
-    void operator()(const string::placeholder::optional_t& ph) {
-        if (auto attribute = view.find(ph.name)) {
-            stream.rdbuf()->storage()->append(ph.prefix);
-            mapper(stream, ph.name, attribute->value);
-            stream.rdbuf()->storage()->append(ph.suffix);
-        }
-    }
-
-    void operator()(const string::placeholder::variadic_t& /*ph*/) {
-        std::vector<std::string> passed;
-        passed.reserve(view.upper_size());
-
-        for (auto it = view.begin(); it != view.end(); ++it) {
-            const std::string& name = it->first;
-            const attribute_t& attribute = it->second;
-
-            //!@todo: This code needs some optimization love.
-            std::ostringstream stream;
-            stream << "'" << name << "': ";
-            variadic_visitor_t visitor(stream);
-            boost::apply_visitor(visitor, attribute.value);
-            passed.push_back(stream.str());
-        }
-
-        stream.rdbuf()->storage()->append(boost::algorithm::join(passed, ", "));
-    }
-};
 
 class string_t : public base_t {
     const std::string pattern;
@@ -119,7 +46,7 @@ public:
         blackhole::aux::attachable_ostringstream stream;
         stream.attach(buffer);
         try {
-            visitor_t visitor(stream, mapper, record.attributes());
+            string::visitor_t visitor(stream, mapper, record.attributes());
             for (auto it = tokens.begin(); it != tokens.end(); ++it) {
                 boost::apply_visitor(visitor, *it);
                 stream.flush();
---
+++ src/blackhole/formatter/string/generator.hpp
@@ -0,0 +1,98 @@
+#pragma once
+
+#include <ostream>
+#include <string>
+
+#include <boost/algorithm/string.hpp>
+#include <boost/variant.hpp>
+
+#include "blackhole/detail/stream/stream.hpp"
+#include "blackhole/formatter/map/value.hpp"
+#include "blackhole/formatter/string/parser.hpp"
+#include "blackhole/record.hpp"
+
+namespace blackhole {
+
+namespace formatter {
+
+namespace string {
+
+class variadic_visitor_t : public boost::static_visitor<> {
+    std::ostringstream& stream;
+
+public:
+    variadic_visitor_t(std::ostringstream& stream) :
+        stream(stream)
+    {}
+
+    template<typename T>
+    void operator()(const T& value) const {
+        stream << value;
+    }
+
+    void operator()(const std::string& value) const {
+        stream << "'" << value << "'";
+    }
+};
+
+class visitor_t : public boost::static_visitor<> {
+    blackhole::aux::attachable_ostringstream& stream;
+    const mapping::value_t& mapper;
+    const attribute::set_view_t& view;
+
+public:
+    visitor_t(blackhole::aux::attachable_ostringstream& stream,
+              const mapping::value_t& mapper,
+              const attribute::set_view_t& view) :
+        stream(stream),
+        mapper(mapper),
+        view(view)
+    {}
+
+    void operator()(const literal_t& literal) {
+        stream.rdbuf()->storage()->append(literal.value);
+    }
+
+    void operator()(const placeholder::required_t& ph) {
+        if (auto attribute = view.find(ph.name)) {
+            mapper(stream, ph.name, attribute->value);
+            return;
+        }
+
+        throw error_t("required attribute '%s' was not provided", ph.name);
+    }
+
+    void operator()(const placeholder::optional_t& ph) {
+        if (auto attribute = view.find(ph.name)) {
+            stream.rdbuf()->storage()->append(ph.prefix);
+            mapper(stream, ph.name, attribute->value);
+            stream.rdbuf()->storage()->append(ph.suffix);
+        }
+    }
+
+    //!@todo: Test prefix/suffix/separator/pattern.
+    void operator()(const placeholder::variadic_t& /*ph*/) {
+        std::vector<std::string> passed;
+        passed.reserve(view.upper_size());
+
+        for (auto it = view.begin(); it != view.end(); ++it) {
+            const std::string& name = it->first;
+            const attribute_t& attribute = it->second;
+
+            //!@todo: This code needs some optimization love.
+            std::ostringstream stream;
+            stream << "'" << name << "': ";
+            variadic_visitor_t visitor(stream);
+            boost::apply_visitor(visitor, attribute.value);
+            passed.push_back(stream.str());
+        }
+
+        stream.rdbuf()->storage()->append(boost::algorithm::join(passed, ", "));
+    }
+};
+
+} // namespace string
+
+} // namespace formatter
+
+} // namespace blackhole
