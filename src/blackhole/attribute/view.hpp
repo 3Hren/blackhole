b1e5b547a39af787b31560e59d208be742e77a8a
Evgeny Safronov <division494@gmail.com>
2014-09-03 17:34:14 +0400

    [Code Style] Decomposition.

--- src/blackhole/attribute/view.hpp
+++ src/blackhole/attribute/view.hpp
@@ -125,28 +125,6 @@ struct tuple_extractor_t<T, Arg, Args...> {
 
 } // namespace set_view
 
-/// A type that represents a parameter pack of zero or more integers.
-template<unsigned... indices>
-struct index_tuple {
-    /// Generate an index_tuple with an additional element.
-    template<unsigned N>
-    struct append {
-        typedef index_tuple<indices..., N> type;
-    };
-};
-
-/// Unary metafunction that generates an index_tuple containing [0, Size)
-template<unsigned size>
-struct make_index_tuple {
-    typedef typename make_index_tuple<size - 1>::type::template append<size - 1>::type type;
-};
-
-/// Terminal case of the recursive metafunction.
-template<>
-struct make_index_tuple<0u> {
-    typedef index_tuple<> type;
-};
-
 template<class T, unsigned size>
 struct all_empty_t {
     static bool empty(const T& tuple) {
--- src/blackhole/detail/traits/tuple.hpp
+++ src/blackhole/detail/traits/tuple.hpp
@@ -7,3 +7,25 @@
 #include "tuple/index/remove.hpp"
 #include "tuple/map.hpp"
 #include "tuple/slice.hpp"
+
+/// A type that represents a parameter pack of zero or more integers.
+template<unsigned... indices>
+struct index_tuple {
+    /// Generate an index_tuple with an additional element.
+    template<unsigned N>
+    struct append {
+        typedef index_tuple<indices..., N> type;
+    };
+};
+
+/// Unary metafunction that generates an index_tuple containing [0, Size)
+template<unsigned size>
+struct make_index_tuple {
+    typedef typename make_index_tuple<size - 1>::type::template append<size - 1>::type type;
+};
+
+/// Terminal case of the recursive metafunction.
+template<>
+struct make_index_tuple<0u> {
+    typedef index_tuple<> type;
+};
