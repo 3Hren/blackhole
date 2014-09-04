0182e37a9866acfb2045f5424eb0226aa8d2edd6
Evgeny Safronov <division494@gmail.com>
2014-09-04 17:54:15 +0400

    [Feature] Use compiler extension (cross-platform nonetheless) to check log message format correctness in compile time.

--- src/blackhole/macro.hpp
+++ src/blackhole/macro.hpp
@@ -3,6 +3,15 @@
 #include "blackhole/logger.hpp"
 #include "blackhole/detail/logger/pusher.hpp"
 
+inline
+bool
+__attribute__((always_inline))
+__attribute__((format(__printf__, 1, 2)))
+check(const char*, ...) {
+    return true;
+}
+
 #define BH_LOG(__log__, __level__, ...) \
     if (auto record = (__log__).open_record((__level__))) \
-        blackhole::aux::logger::make_pusher((__log__), record, __VA_ARGS__)
+        if (check(__VA_ARGS__)) \
+            blackhole::aux::logger::make_pusher((__log__), record, __VA_ARGS__)
