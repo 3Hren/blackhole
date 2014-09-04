d058091199ec24aa37c40ef114a0240be7d7c26f
Evgeny Safronov <division494@gmail.com>
2014-09-04 14:39:11 +0400

    [GCC 4.4] Fuck you. Workaround variadic pack expansion.
    It evades GCC 4.4 message: "sorry, unimplemented: cannot expand ‘Args ...’ into a fixed-length argument list"

--- src/blackhole/detail/traits/unique.hpp
+++ src/blackhole/detail/traits/unique.hpp
@@ -8,13 +8,42 @@
 
 namespace blackhole {
 
+/// General definition of the helper class
+template<typename... Args> struct from_variadic;
+
+/// This one handles the case when no types passed.
+template<>
+struct from_variadic<> {
+    typedef boost::mpl::vector<> type;
+};
+
+/// This one is a specialization for the case when only one type is passed.
+template<typename T>
+struct from_variadic<T> {
+    typedef boost::mpl::vector<T> type;
+};
+
+/*!
+ * This specialization does the actual job.
+ * It splits the whole pack into 2 parts: one with single type T and the rest
+ * of types Args.
+ * As soon as it is done T is added to an mpl::vector.
+ */
+template<typename T, typename... Args>
+struct from_variadic<T, Args...> {
+    typedef typename boost::mpl::push_front<
+        typename from_variadic<Args...>::type,
+        T
+    >::type type;
+};
+
 /// Metafunction, that detects whether a parameter pack has duplicate items.
 template<typename... Args>
 struct unique :
     public std::conditional<
         sizeof...(Args) == boost::mpl::size<
             typename boost::mpl::unique<
-                boost::mpl::vector<Args...>,
+                typename from_variadic<Args...>::type,
                 std::is_same<boost::mpl::_1, boost::mpl::_2>
             >::type
         >::value,
