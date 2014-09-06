155daa29e22e69b1c035b4efbc072d1cdff8deb4
Evgeny Safronov <division494@gmail.com>
2014-09-06 18:36:24 +0400

    [Aux] Syntax checker is enabled again.

--- src/blackhole/macro.hpp
+++ src/blackhole/macro.hpp
@@ -6,4 +6,5 @@
 
 #define BH_LOG(__log__, __level__, ...) \
     if (auto record = (__log__).open_record((__level__))) \
-        blackhole::aux::logger::make_pusher((__log__), record, __VA_ARGS__)
+        if (blackhole::aux::syntax_check(__VA_ARGS__)) \
+            blackhole::aux::logger::make_pusher((__log__), record, __VA_ARGS__)
