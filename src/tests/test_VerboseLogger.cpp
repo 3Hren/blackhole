217b4f6a0a4a3e71a9cfad80c31a98e39689aaaa
Evgeny Safronov <division494@gmail.com>
2014-11-10 17:25:34 +0300

    [Unit Testing] Dead code elimination.

--- src/tests/test_VerboseLogger.cpp
+++
@@ -1,8 +0,0 @@
-#include <blackhole/formatter/string.hpp>
-#include <blackhole/logger.hpp>
-#include <blackhole/frontend/files.hpp>
-
-#include "global.hpp"
-#include "mocks/frontend.hpp"
-
-using namespace blackhole;
