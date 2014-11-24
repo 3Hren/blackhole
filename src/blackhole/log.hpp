2c51b2df2b97ec955330edda15668202158b2b17
Evgeny Safronov <division494@gmail.com>
2014-11-24 15:09:00 +0300

    [Deprecation] Remove deprecated file just as planned.

--- src/blackhole/log.hpp
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-//! This file is here for backward compatibility.
-//! It will be removed when version 0.3 comes.
-
-#include "blackhole/detail/logger/pusher.hpp"
-#include "blackhole/macro.hpp"
