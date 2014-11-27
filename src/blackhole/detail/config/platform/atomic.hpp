12678b241cd586bbfbd3db83967dcb5d31130ee5
Evgeny Safronov <division494@gmail.com>
2014-11-27 16:28:13 +0300

    [Common] Clang support on non OS X targets.

--- src/blackhole/detail/config/platform/atomic.hpp
+++ src/blackhole/detail/config/platform/atomic.hpp
@@ -2,6 +2,6 @@
 
 #include "compiler.hpp"
 
-#if (defined(BLACKHOLE_HAS_CLANG) && defined(TARGET_OS_MAC)) || defined(BLACKHOLE_HAS_AT_LEAST_GCC46)
+#if defined(BLACKHOLE_HAS_CLANG) || defined(BLACKHOLE_HAS_AT_LEAST_GCC46)
     #define BLACKHOLE_HAS_FEATURE_ATOMIC
 #endif
