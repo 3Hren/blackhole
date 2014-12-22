e6c4ef2102a2ceca298707abce7249f1bcee9566
Evgeny Safronov <division494@gmail.com>
2014-12-22 11:52:46 +0300

    [Common] Syntax checking can be disabled now.

--- src/blackhole/detail/syntax.hpp
+++ src/blackhole/detail/syntax.hpp
@@ -1,5 +1,6 @@
 #pragma once
 
+#include "blackhole/config.hpp"
 #include "blackhole/detail/config/inline.hpp"
 
 namespace blackhole {
@@ -9,7 +10,9 @@ namespace aux {
 inline
 bool
 BLACKHOLE_ALWAYS_INLINE
+#if BLACKHOLE_CHECK_FORMAT_SYNTAX == 1
 __attribute__((format(__printf__, 1, 2)))
+#endif
 syntax_check(const char*, ...) {
     return true;
 }
