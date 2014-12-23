042bf5691b8b9a8a9b8c4c964a8b72f9d1a98da6
Evgeny Safronov <division494@gmail.com>
2014-12-23 19:22:03 +0300

    [Detail] Mangle internal macro variables.

--- src/blackhole/macro.hpp
+++ src/blackhole/macro.hpp
@@ -5,6 +5,6 @@
 #include "blackhole/detail/syntax.hpp"
 
 #define BH_LOG(__log__, __level__, ...) \
-    if (auto record = (__log__).open_record((__level__))) \
+    if (auto __record__ = (__log__).open_record((__level__))) \
         if (blackhole::aux::syntax_check(__VA_ARGS__)) \
-            blackhole::aux::logger::make_pusher((__log__), record, __VA_ARGS__)
+            blackhole::aux::logger::make_pusher((__log__), __record__, __VA_ARGS__)
