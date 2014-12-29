e0882abbc24c0eea94fed4ace85b9d48b9c30dfd
Evgeny Safronov <division494@gmail.com>
2014-12-29 12:52:49 +0300

    [Compatibility] Header file is returned back.
    But now it will spam warning messages if included.

---
+++ src/blackhole/log.hpp
@@ -0,0 +1,7 @@
+#pragma once
+
+//! \deprecated This file will be removed when 1.0 comes.
+#warning <blackhole/log.hpp> file is deprecated, use <blackhole/logger.hpp> plus <blackhole/macro.hpp> instead
+
+#include "blackhole/logger.hpp"
+#include "blackhole/macro.hpp"
