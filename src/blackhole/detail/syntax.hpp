69d107a8a3cf8c44f644a634366b031dfb5558e4
Evgeny Safronov <division494@gmail.com>
2015-06-09 19:12:04 +0300

    [Bug Fix] Allowing to suppress syntax checking.

--- src/blackhole/detail/syntax.hpp
+++ src/blackhole/detail/syntax.hpp
@@ -8,15 +8,23 @@ BLACKHOLE_BEG_NS
 
 namespace aux {
 
-inline
-bool
-BLACKHOLE_ALWAYS_INLINE
 #if BLACKHOLE_CHECK_FORMAT_SYNTAX == 1
-__attribute__((format(__printf__, 1, 2)))
+    inline
+    bool
+    BLACKHOLE_ALWAYS_INLINE
+    __attribute__((format(__printf__, 1, 2)))
+    syntax_check(const char*, ...) {
+        return true;
+    }
+#else
+    template<typename... Args>
+    inline
+    bool
+    BLACKHOLE_ALWAYS_INLINE
+    syntax_check(const char*, Args&&...) {
+        return true;
+    }
 #endif
-syntax_check(const char*, ...) {
-    return true;
-}
 
 } // namespace aux
 
