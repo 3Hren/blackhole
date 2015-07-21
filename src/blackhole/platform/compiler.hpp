0fa9351e58b98233cfcf9a5fb22d35bc8df921b7
Evgeny Safronov <division494@gmail.com>
2015-07-21 11:44:52 +0300

    Bug fix: GCC 5.* support for platform macro.

--- src/blackhole/platform/compiler.hpp
+++ src/blackhole/platform/compiler.hpp
@@ -1,7 +1,7 @@
 #pragma once
 
 #if defined(__GNUC__)
-    #if __GNUC__ == 4 && __GNUC_MINOR__ >= 6
+    #if __GNUC__ >= 5 || (__GNUC__ == 4 && __GNUC_MINOR__ >= 6)
         #define BLACKHOLE_HAVE_AT_LEAST_GCC46
     #endif
 #endif
