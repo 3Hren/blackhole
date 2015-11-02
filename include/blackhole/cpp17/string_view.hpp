bb51fdb048e9100320fad34b257a8a5a671aff5f
Evgeny Safronov <division494@gmail.com>
2015-11-02 20:11:08 +0300

    refactor(view): make string view template

--- include/blackhole/cpp17/string_view.hpp
+++ include/blackhole/cpp17/string_view.hpp
@@ -6,9 +6,13 @@
 namespace blackhole {
 namespace cpp17 {
 
-class string_view {
+template<typename Char, typename Traits = std::char_traits<Char>>
+class basic_string_view {
 public:
     static constexpr std::size_t npos = std::numeric_limits<std::size_t>::max();
+
+    typedef Char value_type;
+    typedef Traits traits_type;
 
 private:
     const char* data_;
@@ -17,30 +21,32 @@ private:
 
 public:
     /// Constructs an empty `string_view`.
-    constexpr string_view() noexcept:
+    constexpr
+    basic_string_view() noexcept:
         data_(nullptr),
         size_(0)
     {}
 
     template<std::size_t N>
-    constexpr string_view(const char(&literal)[N]) noexcept:
+    constexpr
+    basic_string_view(const char(&literal)[N]) noexcept:
         data_(literal),
         size_(N - 1)
     {}
 
     constexpr
-    string_view(const char* literal, std::size_t size) noexcept:
+    basic_string_view(const char* literal, std::size_t size) noexcept:
         data_(literal),
         size_(size)
     {}
 
-    string_view(const std::string& message) noexcept:
+    basic_string_view(const std::string& message) noexcept:
         data_(message.data()),
         size_(message.size())
     {}
 
     constexpr
-    string_view(const string_view& other) = default;
+    basic_string_view(const basic_string_view& other) = default;
 
     constexpr
     auto data() const noexcept -> const char* {
@@ -61,28 +67,33 @@ public:
         throw std::out_of_range("out of range");
     }
 
+    /// Operations.
+
     /// Creates a `std::string` with a copy of the content of the current view.
-    auto to_string() const -> std::string {
+    template<class Allocator = std::allocator<Char>>
+    auto to_string(const Allocator& a = Allocator()) const -> std::basic_string<Char, Traits, Allocator> {
         return {data(), size()};
     }
 
     /// Returns a view of the substring [pos, pos + rcount), where rcount is the smaller of count
     /// and `size() - pos`.
     constexpr
-    auto substr(std::size_t pos = 0, std::size_t count = npos) const -> string_view {
+    auto substr(std::size_t pos = 0, std::size_t count = npos) const -> basic_string_view {
         if (pos > size()) {
             throw std::out_of_range("out of range");
         }
 
-        return string_view(data() + pos, std::min(count, size() - pos));
-    }
-
-    friend
-    std::ostream&
-    operator<<(std::ostream& stream, const string_view& value) {
-        return stream.write(value.data(), static_cast<std::streamsize>(value.size()));
+        return basic_string_view(data() + pos, std::min(count, size() - pos));
     }
 };
+
+template<class Char, class Traits>
+std::basic_ostream<Char, Traits>&
+operator<<(std::basic_ostream<Char, Traits>& stream, const basic_string_view<Char, Traits>& value) {
+    return stream.write(value.data(), static_cast<std::streamsize>(value.size()));
+}
+
+typedef basic_string_view<char> string_view;
 
 }  // namespace cpp17
 
