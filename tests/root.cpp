56a5bdb1a21c16a50debd880a7f80982e415cf28
Evgeny Safronov <division494@gmail.com>
2015-11-07 12:30:55 +0300

    chore(testing): remove commented code

--- tests/root.cpp
+++ tests/root.cpp
@@ -64,38 +64,5 @@ TEST(RootLogger, ForwardToHandlers) {
     logger.log(0, "GET /porn.png HTTP/1.1");
 }
 
-// TEST(wrapper, call) {
-//     using attribute::value_t;
-//     using attribute::owned_t;
-//
-//     root_logger_t root({});
-//
-//     wrapper_t wrapper1{root, {
-//         {"key#0", owned_t(0)},
-//         {"key#1", owned_t("value#1")}
-//     }};
-//
-//     wrapper_t wrapper2{wrapper1, {
-//         {"key#2", owned_t(2)},
-//         {"key#3", owned_t("value#3")}
-//     }};
-//
-//     logger_facade<wrapper_t> logger(wrapper2);
-//
-//     logger.log(0,
-//         {
-//             {"key#4", value_t(42)},
-//             {"key#5", value_t(3.1415)},
-//             {"key#6", value_t("value")}
-//         }, "{} - {} [{}] 'GET {} HTTP/1.0' {} {}",
-//         "[::]",
-//         "esafronov",
-//         "10/Oct/2000:13:55:36 -0700",
-//         "/porn.png",
-//         200,
-//         2326
-//     );
-// }
-
 }  // namespace testing
 }  // namespace blackhole
