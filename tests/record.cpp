4b396685088202380d27de0c4e58582a67c3c14c
Evgeny Safronov <division494@gmail.com>
2015-11-10 22:47:35 +0300

    chore(tests): add stub for record testing

---
+++ tests/record.cpp
@@ -0,0 +1,12 @@
+#include <gtest/gtest.h>
+
+#include <blackhole/record.hpp>
+
+namespace blackhole {
+namespace testing {
+
+TEST(Record, _) {
+}
+
+}  // namespace testing
+}  // namespace blackhole
