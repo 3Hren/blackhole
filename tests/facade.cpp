58029e6aa435b8338168f7e4c82686bb9f8e40cc
Evgeny Safronov <division494@gmail.com>
2015-11-10 20:09:14 +0300

    chore(tests): add format function call checks

--- tests/facade.cpp
+++ tests/facade.cpp
@@ -9,6 +9,8 @@
 namespace blackhole {
 namespace testing {
 
+using ::testing::ByRef;
+using ::testing::InvokeArgument;
 using ::testing::_;
 
 typedef mock::logger_t logger_type;
@@ -42,10 +44,14 @@ TEST(Facade, FormattedLog) {
     const logger_facade<logger_type> logger(inner);
 
     range_t expected;
+    writer_t writer;
     EXPECT_CALL(inner, log(0, string_view("GET /porn.png HTTP/1.0 - {}"), expected, _))
-        .Times(1);
+        .Times(1)
+        .WillOnce(InvokeArgument<3>(ByRef(writer)));
 
     logger.log(0, "GET /porn.png HTTP/1.0 - {}", 42);
+
+    EXPECT_EQ("GET /porn.png HTTP/1.0 - 42", writer.inner.str());
 }
 
 TEST(Facade, FormattedAttributeLog) {
@@ -54,12 +60,16 @@ TEST(Facade, FormattedAttributeLog) {
 
     const attribute_list attributes{{"key#1", {42}}};
     range_t expected{attributes};
-    EXPECT_CALL(inner, log(0, string_view("GET /porn.png HTTP/1.0"), expected, _))
-        .Times(1);
+    writer_t writer;
+    EXPECT_CALL(inner, log(0, string_view("GET /porn.png HTTP/1.0 - {}"), expected, _))
+        .Times(1)
+        .WillOnce(InvokeArgument<3>(ByRef(writer)));
 
-    logger.log(0, "GET /porn.png HTTP/1.0", 2345, attribute_list{
+    logger.log(0, "GET /porn.png HTTP/1.0 - {}", 2345, attribute_list{
         {"key#1", {42}}
     });
+
+    EXPECT_EQ("GET /porn.png HTTP/1.0 - 2345", writer.inner.str());
 }
 
 }  // namespace testing
