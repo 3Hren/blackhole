60a572324d1ba867dd9fb45c85d2f5b6f0017b93
Evgeny Safronov <division494@gmail.com>
2015-11-18 19:11:11 +0300

    feat(format): add basic string formatter

--- include/blackhole/formatter/string.hpp
+++ include/blackhole/formatter/string.hpp
@@ -1,22 +1,25 @@
 #pragma once
 
+#include <functional>
 #include <map>
 #include <string>
 #include <vector>
 
-#include <boost/variant/variant.hpp>
+#include <boost/variant/variant_fwd.hpp>
 
 #include "blackhole/formatter.hpp"
-#include "blackhole/record.hpp"
 
-#include "blackhole/detail/formatter/string/parser.hpp"
+namespace blackhole {
+
+class record_t;
+class writer_t;
+
+}  // namespace blackhole
 
 namespace blackhole {
 namespace formatter {
 
-using detail::formatter::string::parser_t;
-
-using namespace detail::formatter::string;
+class token_t;
 
 namespace option {
 
@@ -40,79 +43,98 @@ typedef boost::variant<
     option::remaining_t
 > option_t;
 
-typedef std::function<void(int, writer_t&)> severity_formatter;
+typedef std::map<std::string, option_t> options_t;
 
-class token_visitor_t : public boost::static_visitor<> {
-    const severity_formatter& fn;
-    const record_t& record;
-    writer_t& writer;
+/// Severity mapping function.
+///
+/// Default value just writes an integer representation.
+typedef std::function<void(int, writer_t&)> severity_map;
 
-public:
-    token_visitor_t(const severity_formatter& fn, const record_t& record, writer_t& writer) :
-        fn(fn),
-        record(record),
-        writer(writer)
-    {}
-
-    auto operator()(const literal_t& token) -> void {
-        writer.inner << token.value;
-    }
-
-    auto operator()(const message_t& token) -> void {
-        writer.write("{" + token.spec + "}", record.formatted());
-    }
-
-    auto operator()(const severity_t& token) -> void {
-        if (token.spec.back() == 'd') {
-            // writer.inner << record.severity();
-        } else {
-            fn(record.severity(), writer);
-        }
-    }
-
-    // timestamp_t,
-    // placeholder_t,
-    // placeholder::leftover_t
-
-    template<typename T>
-    auto operator()(const T& token) -> void {
-        throw std::runtime_error("unimplemented");
-    }
-};
-
+/// The string formatter is responsible for effective converting the given record to a string using
+/// precompiled pattern and options.
+///
+/// This formatter allows to specify the pattern using python-like syntax with braces and attribute
+/// names.
+///
+/// For example, the given pattern `{severity:d}, [{timestamp}]: {message}` would result in
+/// something like this: `1, [2015-11-18 15:50:12.630953]: HTTP1.1 - 200 OK`.
+///
+/// Let's explain what's going on when a log record passed such pattern.
+///
+/// There are three named arguments or attributes: severity, timestamp and message. The severity
+/// is represented as signed integer, because of `:d` format specifier. Other tho arguments haven't
+/// such specifiers, so they are represented using default types for each attribute. In our case
+/// the timestamp and message attributes are formatted as strings.
+///
+/// Considering message argument almost everything is clear, but for timestamp there are internal
+/// magic comes. There is default `%Y-%m-%d %H:%M:%S.%f` pattern for timestamp attributes which
+/// reuses `strftime` standard placeholders with an extension of microseconds - `%f`. It means
+/// that the given timestamp is formatted with 4-digit decimal year, 1-12 decimal month and so on.
+///
+/// See \ref http://en.cppreference.com/w/cpp/chrono/c/strftime for more details.
+///
+/// The formatter uses python-like syntax with all its features, like aligning, floating point
+/// precision etc.
+///
+/// For example the `{re:+.3f}; {im:+.6f}` pattern is valid and results in `+3.140; -3.140000`
+/// message with `re: 3.14` and `im: -3.14` attributes provided.
+///
+/// For more information see \ref http://cppformat.github.io/latest/syntax.html resource.
+///
+/// With a few predefined exceptions the formatter supports all userspace attributes. The exceptions
+/// are: message, severity, timestamp, process and thread. For these attributes there are special
+/// rules and it's impossible to override then even with the same name attribute.
+///
+/// For message attribute there are no special rules. It's still allowed to extend the specification
+/// using fill, align and other specifiers.
+///
+/// With timestamp attribute there is an extension of either explicitly providing formatting pattern
+/// or forcing the attibute to be printed as an integer.
+/// In the first case for example the pattern may be declared as `{timestamp:{%Y}s}` which results
+/// in only year formattinh using YYYY style.
+/// In the second case one can force the timestamp to be printed as microseconds passed since epoch.
+///
+/// Severity attribute can be formatted either as an integer or using the provided callback with the
+/// following signature: `auto(int, writer_t&) -> void` where the first argument means an actual
+/// severity level, the second one - streamed writer.
+///
+/// Process attribute can be represented as either an PID or process name using `:d` and `:s` types
+/// respectively: `{process:s}` and `{process:d}`.
+///
+/// At last the thread attribute can be formatted as either thread id in platform-independent hex
+/// representation by default or explicitly with `:x` type, thread id in platform-dependent way
+/// using `:d` type or as a thread name if specified, nil otherwise.
+///
+/// The formatter will throw an exception if an attribute name specified in pattern won't be found
+/// in the log record. Of course Blackhole catches this, but it results in dropping the entire
+/// message.
+/// To avoid this the formatter supports optional generic attributes, which can be specified using
+/// the `optional_t` option with an optional prefix and suffix literals printed if an attribute
+/// exists.
+/// For example an `{id}` pattern with the `[` prefix and `]` suffix options results in empty
+/// message if there is no `source` attribute in the record, `[42]` otherwise (where id = 42).
+///
+/// Blackhole also supports the leftover placeholder starting with `...` and meaning to print all
+/// userspace attributes in a reverse order they were provided.
+///
+/// # Performance
+///
+/// Internally the given pattern is compiled into the list of tokens during construction time. All
+/// further operations are performed using that list to achieve maximum performance.
+///
+/// All visited tokens are written directly into the given writer instance with an internal small
+/// stack-allocated buffer, growing using the heap on overflow.
 class string_t : public formatter_t {
     const std::string pattern;
-    const severity_formatter fn;
-    const std::vector<parser_t::token_t> tokens;
+    const severity_map sevmap;
+    const std::vector<token_t> tokens;
 
 public:
-    string_t(std::string pattern, std::map<std::string, option_t> options = {}) :
-        pattern(std::move(pattern)),
-        fn([](int value, writer_t& writer) { writer.inner << value; }),
-        tokens(tokenize(this->pattern))
-    {}
-    // pattern -> [token].
-    // token.each -> find options[token.name] -> double match & attach options.
+    string_t(std::string pattern, options_t options = options_t());
+    string_t(std::string pattern, severity_map sevmap, options_t options = options_t());
+    ~string_t();
 
-    auto format(const record_t& record, writer_t& writer) -> void {
-        token_visitor_t visitor(fn, record, writer);
-
-        for (const auto& token : tokens) {
-            boost::apply_visitor(visitor, token);
-        }
-    }
-
-private:
-    static auto tokenize(const std::string& pattern) -> std::vector<parser_t::token_t> {
-        std::vector<parser_t::token_t> tokens;
-
-        parser_t parser(pattern);
-        while (auto token = parser.next()) {
-            tokens.emplace_back(token.get());
-        }
-
-        return tokens;
-    }
+    auto format(const record_t& record, writer_t& writer) -> void;
 };
 
 }  // namespace formatter
---
+++ src/formatter/string.cpp
@@ -0,0 +1,96 @@
+#include "blackhole/formatter/string.hpp"
+
+#include <boost/variant/variant.hpp>
+
+#include "blackhole/extensions/writer.hpp"
+#include "blackhole/record.hpp"
+
+#include "blackhole/detail/formatter/string/parser.hpp"
+#include "blackhole/detail/formatter/string/token.hpp"
+#include "blackhole/detail/unimplemented.hpp"
+
+namespace blackhole {
+namespace formatter {
+
+namespace string = blackhole::detail::formatter::string;
+namespace ph = string::ph;
+
+using string::literal_t;
+
+class token_t {
+    string::token_t inner;
+
+public:
+    token_t(string::token_t inner) :
+        inner(std::move(inner))
+    {}
+
+    auto operator*() const noexcept -> const string::token_t& {
+        return inner;
+    }
+};
+
+namespace {
+
+class visitor_t : public boost::static_visitor<> {
+    writer_t& writer;
+    const record_t& record;
+    const severity_map& sevmap;
+
+public:
+    visitor_t(writer_t& writer, const record_t& record, const severity_map& sevmap) noexcept :
+        writer(writer),
+        record(record),
+        sevmap(sevmap)
+    {}
+
+    auto operator()(const literal_t& token) const -> void {
+        writer.inner << token.value;
+    }
+
+    auto operator()(const ph::message_t& token) const -> void {
+        writer.write(token.spec, record.formatted());
+    }
+
+    template<typename T>
+    auto operator()(const T& token) const -> void {
+        BLACKHOLE_UNIMPLEMENTED();
+    }
+};
+
+static auto tokenize(const std::string& pattern) -> std::vector<token_t> {
+    std::vector<token_t> tokens;
+
+    string::parser_t parser(pattern);
+    while (auto token = parser.next()) {
+        tokens.emplace_back(token.get());
+    }
+
+    return tokens;
+}
+
+}  // namespace
+
+string_t::string_t(std::string pattern, options_t options) :
+    pattern(std::move(pattern)),
+    tokens(tokenize(this->pattern))
+{}
+
+string_t::string_t(std::string pattern, severity_map sevmap, options_t options) {}
+
+string_t::~string_t() {}
+
+auto
+string_t::format(const record_t& record, writer_t& writer) -> void {
+    const visitor_t visitor(writer, record, sevmap);
+
+    for (const auto& token : tokens) {
+        boost::apply_visitor(visitor, *token);
+    }
+}
+
+}  // namespace formatter
+}  // namespace blackhole
+
+// TODO: Check error when setting an option to reserved name, i.e. timestamp or message.
+// TODO: Check error when ph in pattern was not found.
