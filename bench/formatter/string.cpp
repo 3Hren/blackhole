d165bb1a7e519ffc07b98e86cf19768f7eafa6ed
Evgeny Safronov <division494@gmail.com>
2015-11-19 19:11:53 +0300

    chore(bench): add pid format benchmark

--- bench/formatter/string.cpp
+++ bench/formatter/string.cpp
@@ -28,6 +28,23 @@ format_literal(::benchmark::State& state) {
 
 static
 void
+format_pid(::benchmark::State& state) {
+    formatter::string_t formatter("{process}");
+
+    const string_view message("-");
+    const attribute_pack pack;
+    record_t record(0, message, pack);
+    writer_t writer;
+
+    while (state.KeepRunning()) {
+        formatter.format(record, writer);
+    }
+
+    state.SetItemsProcessed(state.iterations());
+}
+
+static
+void
 format_message(::benchmark::State& state) {
     formatter::string_t formatter("message: {message}");
 
@@ -45,7 +62,7 @@ format_message(::benchmark::State& state) {
 
 static
 void
-format_severity_with_message(::benchmark::State& state) {
+format_severity_message(::benchmark::State& state) {
     formatter::string_t formatter("{severity:d}: {message}");
 
     const string_view message("value");
@@ -61,8 +78,9 @@ format_severity_with_message(::benchmark::State& state) {
 }
 
 BENCHMARK(format_literal);
+BENCHMARK(format_pid);
 BENCHMARK(format_message);
-BENCHMARK(format_severity_with_message);
+BENCHMARK(format_severity_message);
 
 }  // namespace benchmark
 }  // namespace blackhole
