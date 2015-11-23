22213f420abd6ceb89dcc6cc6e1b1b2e660b359b
Evgeny Safronov <division494@gmail.com>
2015-11-23 21:36:33 +0300

    feat(builder): add POC of logger builder
    Also an example added.

---
+++ examples/2.simple.cpp
@@ -0,0 +1,60 @@
+/// This example demonstrates the a bit extended the Blackhole logging library usage and its
+/// features, like:
+///  - Custom severity mapping.
+///  - Logger construction using builder pattern.
+
+#include <array>
+
+#include <blackhole/builder.hpp>
+#include <blackhole/extensions/writer.hpp>
+#include <blackhole/formatter/string.hpp>
+#include <blackhole/handler/blocking.hpp>
+#include <blackhole/logger.hpp>
+#include <blackhole/root.hpp>
+#include <blackhole/sink/console.hpp>
+
+/// As always specify severity enumeration.
+enum severity {
+    debug   = 0,
+    info    = 1,
+    warning = 2,
+    error   = 3
+};
+
+/// Severity mapping function.
+static auto sevmap(int severity, const std::string& spec, blackhole::writer_t& writer) -> void {
+    static const std::array<const char*, 4> mapping = {"D", "I", "W", "E"};
+
+    if (severity < mapping.size()) {
+        writer.write(spec, mapping[severity]);
+    } else {
+        writer.write(spec, severity);
+    }
+}
+
+auto main(int, char**) -> int {
+    /// Here we are going to configure our string/console handler and to build the logger.
+    auto log = blackhole::builder<blackhole::root_logger_t>()
+        /// Add the blocking handler.
+        .handler<blackhole::handler::blocking_t>()
+            /// Configure string formatter.
+            ///
+            /// Pattern syntax behaves like as usual substitution for placeholder. For example if
+            /// the attribute named `severity` has value `2`, then pattern `{severity}` will invoke
+            /// severity mapping function provided and the result will be `W`.
+            .set<blackhole::formatter::string_t>("{severity}, [{timestamp}]: {message}", &sevmap)
+            /// Configure console sink to write into stdout (also stderr can be configured).
+            .add<blackhole::sink::console_t>()
+            /// And build the handler. Multiple handlers can be added to a single logger, but right
+            /// now we confine ourselves with a single handler.
+            .build()
+        /// Build the logger.
+        .build();
+
+    log.log(severity::debug, "GET /static/image.png HTTP/1.1 404 347");
+    log.log(severity::info, "nginx/1.6 configured");
+    log.log(severity::warning, "client stopped connection before send body completed");
+    log.log(severity::error, "file does not exist: /var/www/favicon.ico");
+
+    return 0;
+}
--- include/blackhole/builder.hpp
+++ include/blackhole/builder.hpp
@@ -1,5 +1,91 @@
 #pragma once
 
+#include <memory>
+#include <vector>
+
 namespace blackhole {
 
+template<typename> class builder; // temp.
+class handler_t;
+class logger_t;
+
+}
+
+namespace blackhole {
+
+template<typename L, typename T>
+class handler_builder {
+public:
+    typedef L logger_type;
+    typedef T handler_type;
+    typedef builder<logger_type> parent_type;
+    typedef handler_builder<logger_type, handler_type> self_type;
+
+private:
+    parent_type& parent;
+    std::unique_ptr<handler_type> handler;
+    std::vector<std::unique_ptr<handler_t>>& handlers;
+
+public:
+    constexpr
+    handler_builder(parent_type& parent, std::unique_ptr<handler_type> handler,
+        std::vector<std::unique_ptr<handler_t>>& handlers) noexcept :
+        parent(parent),
+        handler(std::move(handler)),
+        handlers(handlers)
+    {}
+
+    template<typename F, typename... Args>
+    auto set(Args&&... args) -> self_type& {
+        handler->set(std::unique_ptr<F>(new F(std::forward<Args>(args)...)));
+        return *this;
+    }
+
+    template<typename S, typename... Args>
+    auto add(Args&&... args) -> self_type& {
+        handler->add(std::unique_ptr<S>(new S(std::forward<Args>(args)...)));
+        return *this;
+    }
+
+    auto build() noexcept -> parent_type& {
+        // TODO: Check handler correctness. It's possible to build without formatter or sinks.
+        // Can be fixed using strong-typed builder.
+        handlers.emplace_back(std::move(handler));
+        return parent;
+    }
+};
+
+template<typename L>
+class builder {
+public:
+    typedef L logger_type;
+
+private:
+    std::vector<std::unique_ptr<handler_t>> handlers;
+
+public:
+    template<typename H, typename... Args>
+    auto handler(Args&&... args) -> handler_builder<logger_type, H> {
+        return handler_builder<logger_type, H>{
+            *this,
+            std::unique_ptr<H>(new H(std::forward<Args>(args)...)),
+            handlers
+        };
+    }
+
+    // TODO: Filter.
+
+    auto build() -> logger_type {
+        return {std::move(handlers)};
+    }
+};
+
 }  // namespace blackhole
+
+// XML  ->
+// YAML ->
+// JSON -> dynamic -> factory -> handlers -> root_logger_t.
+// d["type"].value_or("") == "string" ...
+
+// builder<T, From = manual>().build()
+// #include <...json.hpp> => builder<T, json>("filename.json").build() -> T
