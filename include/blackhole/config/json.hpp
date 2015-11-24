da42b380d7bdaea7cf8ad30e652dbf517306942b
Evgeny Safronov <division494@gmail.com>
2015-11-24 22:08:37 +0300

    feat(config/json): make decomposition

---
+++ include/blackhole/config/json.hpp
@@ -0,0 +1,127 @@
+#pragma once
+
+#include "blackhole/config.hpp"
+#include "blackhole/config/monadic.hpp"
+
+namespace blackhole {
+namespace config {
+
+class json_t : public config_t {
+    const rapidjson::Value& value;
+
+public:
+    explicit json_t(const rapidjson::Value& value) :
+        value(value)
+    {}
+
+    auto operator[](const std::size_t& idx) const -> config::monadic<config_t> {
+        if (value.IsArray() && idx < value.Size()) {
+            return make_monadic<json_t>(value[idx]);
+        }
+
+        return {};
+    }
+
+    auto operator[](const std::string& key) const -> config::monadic<config_t> {
+        if (value.IsObject() && value.HasMember(key.c_str())) {
+            return make_monadic<json_t>(value[key.c_str()]);
+        }
+
+        return {};
+    }
+
+    auto is_nil() const -> bool {
+        return value.IsNull();
+    }
+
+    auto is_bool() const -> bool {
+        return value.IsBool();
+    }
+
+    auto is_i64() const -> bool {
+        return value.IsInt64();
+    }
+
+    auto is_u64() const -> bool {
+        return value.IsUint64();
+    }
+
+    auto is_double() const -> bool {
+        return value.IsDouble();
+    }
+
+    auto is_string() const -> bool {
+        return value.IsString();
+    }
+
+    auto is_vector() const -> bool {
+        return value.IsArray();
+    }
+
+    auto is_object() const -> bool {
+        return value.IsObject();
+    }
+
+    auto to_bool() const -> bool {
+        if (value.IsBool()) {
+            return value.GetBool();
+        }
+
+        throw bad_cast();
+    }
+
+    auto to_i64() const -> std::int64_t {
+        if (value.IsInt64()) {
+            return value.GetInt64();
+        }
+
+        throw bad_cast();
+    }
+
+    auto to_u64() const -> std::uint64_t {
+        if (value.IsUint64()) {
+            return value.GetUint64();
+        }
+
+        throw bad_cast();
+    }
+
+    auto to_double() const -> double {
+        if (value.IsDouble()) {
+            return value.GetDouble();
+        }
+
+        throw bad_cast();
+    }
+
+    auto to_string() const -> std::string {
+        if (value.IsString()) {
+            return value.GetString();
+        }
+
+        throw bad_cast();
+    }
+
+    auto each(const each_function& fn) -> void {
+        if (!value.IsArray()) {
+            throw bad_cast();
+        }
+
+        for (auto it = value.Begin(); it != value.End(); ++it) {
+            fn(*make_monadic<json_t>(*it));
+        }
+    }
+
+    auto each_map(const member_function& fn) -> void {
+        if (!value.IsObject()) {
+            throw bad_cast();
+        }
+
+        for (auto it = value.MemberBegin(); it != value.MemberEnd(); ++it) {
+            fn(it->name.GetString(), *make_monadic<json_t>(it->value));
+        }
+    }
+};
+
+}  // namespace config
+}  // namespace blackhole
