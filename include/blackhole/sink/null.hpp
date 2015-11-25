0bfaeb2d1a2219f41ad66d3d0dd29001f5544b32
Evgeny Safronov <division494@gmail.com>
2015-11-25 12:11:56 +0300

    docs(sink/null): add docs

--- include/blackhole/sink/null.hpp
+++ include/blackhole/sink/null.hpp
@@ -14,10 +14,18 @@ struct factory;
 namespace blackhole {
 namespace sink {
 
-/// Null sink implementation that drops all incoming events.
+/// A null sink merely exists, it never outputs a message to any device.
+///
+/// This class exists primarily for benchmarking reasons to measure the entire logging processing
+/// pipeline. It never fails and never throws, because it does nothing.
+///
+/// All methods of this class are thread safe.
 class null_t : public sink_t {
 public:
+    /// Returns `false` regardless of a log record value, causing any log event to be dropped.
     auto filter(const record_t& record) -> bool;
+
+    /// Drops any incoming log event.
     auto execute(const record_t& record, const string_view& formatted) -> void;
 };
 
