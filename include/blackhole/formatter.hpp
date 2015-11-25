9b2bd4a9bae25d9409e89cab542009dc3a340a87
Evgeny Safronov <division494@gmail.com>
2015-11-25 20:49:47 +0300

    feat(formatter): add default move constructor

--- include/blackhole/formatter.hpp
+++ include/blackhole/formatter.hpp
@@ -10,6 +10,7 @@ class formatter_t {
 public:
     formatter_t() = default;
     formatter_t(const formatter_t& other) = default;
+    formatter_t(formatter_t&& other) = default;
 
     virtual ~formatter_t() {}
 
