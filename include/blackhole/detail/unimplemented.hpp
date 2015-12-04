950b0d77e25161a9a5bcdf54491305a55634cfe8
Evgeny Safronov <division494@gmail.com>
2015-12-04 12:58:20 +0300

    chore(): implement the UNIMPLEMENTED macro

--- include/blackhole/detail/unimplemented.hpp
+++ include/blackhole/detail/unimplemented.hpp
@@ -2,5 +2,8 @@
 
 #include <boost/assert.hpp>
 
+/// Three ways of ignorance.
 #define BLACKHOLE_UNIMPLEMENTED() \
-    BOOST_ASSERT(false && "not implemented yet")
+    BOOST_ASSERT(false && "not implemented yet"); \
+    throw std::runtime_error("not implemented yet"); \
+    std::terminate()
