4518cbc2dfc7c5c0145dbac3359ece7f9640ba6d
Evgeny Safronov <division494@gmail.com>
2015-12-07 21:29:18 +0300

    chore(bench): add more benchmarks

--- bench/formatter/json.cpp
+++ bench/formatter/json.cpp
@@ -9,12 +9,26 @@
 namespace blackhole {
 namespace benchmark {
 
-static void format_json_0(::benchmark::State& state) {
-    formatter::json_t formatter(formatter::routing_t().spec("/fields", {"endpoint"}));
+static void format_json(::benchmark::State& state) {
+    formatter::json_t formatter;;
 
     const string_view message("value");
-    const attribute_list attributes{{"endpoint", "127.0.0.1:8080"}};
-    const attribute_pack pack{};
+    const attribute_pack pack;
+    record_t record(0, message, pack);
+    writer_t writer;
+
+    while (state.KeepRunning()) {
+        formatter.format(record, writer);
+    }
+
+    state.SetItemsProcessed(state.iterations());
+}
+
+static void format_json_message_routed(::benchmark::State& state) {
+    formatter::json_t formatter(formatter::routing_t().spec("/fields", {"message"}));
+
+    const string_view message("value");
+    const attribute_pack pack;
     record_t record(0, message, pack);
     writer_t writer;
 
@@ -41,7 +55,8 @@ static void format_json_1(::benchmark::State& state) {
     state.SetItemsProcessed(state.iterations());
 }
 
-BENCHMARK(format_json_0);
+BENCHMARK(format_json);
+BENCHMARK(format_json_message_routed);
 BENCHMARK(format_json_1);
 
 }  // namespace benchmark
