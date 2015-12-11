bab56ea7abe50bd96764b598e066990b562037e1
Evgeny Safronov <division494@gmail.com>
2015-12-11 21:20:35 +0300

    docs(formatter/json): add some documentation

--- include/blackhole/formatter/json.hpp
+++ include/blackhole/formatter/json.hpp
@@ -21,21 +21,37 @@ class json_t : public formatter_t {
     std::unique_ptr<inner_t> inner;
 
 public:
+    /// Represents a JSON formatter object builder to ease its configuration.
     class builder_t;
 
+    /// Constructs a defaultly configured JSON formatter, which will produce plain trees with no
+    /// filtering without adding a separator character at the end.
     json_t();
-    json_t(const json_t& other) = delete;
-    json_t(json_t&& other);
 
+    /// Copy constructing is explicitly prohibited.
+    json_t(const json_t& other) = delete;
+
+    /// Constructs a JSON formatter using the given other JSON formatter by moving its content.
+    json_t(json_t&& other) noexcept;
+
+    /// Destroys the current JSON formatter instance, freeing all its resources.
     ~json_t();
 
+    /// Copy assignment is explicitly prohibited.
+    auto operator=(const json_t& other) -> json_t& = delete;
+
+    /// Assigns the given JSON formatter to the current one by moving its content.
+    auto operator=(json_t&& other) noexcept -> json_t&;
+
+    /// Formats the given record by constructing a JSON tree with further serializing into the
+    /// specified writer.
     auto format(const record_t& record, writer_t& writer) -> void;
 
 private:
     json_t(properties_t properties);
 };
 
-/// Represents a builder to ease JSON formatter configuration.
+/// Represents a JSON formatter object builder to ease its configuration.
 ///
 /// Exists mainly for both avoiding hundreds of formatter constructors and keep its semantics
 /// immutable. It's quite convenient to build up different formatter objects just by chaining the
