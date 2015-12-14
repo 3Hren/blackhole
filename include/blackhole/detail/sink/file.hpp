3edfe7d9af4dc293505c46019662995fb29c052e
Evgeny Safronov <division494@gmail.com>
2015-12-14 21:17:10 +0300

    fix(sink/file): make compilable with GCC 4.6
    For some reasons there is no move constructor for `std::ofstream` defined. Stubbed it
    with unique pointer.

--- include/blackhole/detail/sink/file.hpp
+++ include/blackhole/detail/sink/file.hpp
@@ -14,25 +14,25 @@ namespace sink {
 struct backend_t {
     std::size_t counter;
     std::size_t interval;
-    std::ofstream stream;
+    std::unique_ptr<std::ofstream> stream;
 
     backend_t(const std::string& filename, std::size_t interval) :
         counter(0),
         interval(interval > 0 ? interval : std::numeric_limits<std::size_t>::max()),
-        stream(filename)
+        stream(new std::ofstream(filename))
     {}
 
     backend_t(const backend_t& other) = delete;
     backend_t(backend_t&& other) = default;
 
-    virtual ~backend_t() = default;
+    virtual ~backend_t() {}
 
     auto operator=(const backend_t& other) -> backend_t& = delete;
     auto operator=(backend_t&& other) -> backend_t& = default;
 
     virtual auto write(const string_view& message) -> void {
-        stream.write(message.data(), static_cast<std::streamsize>(message.size()));
-        stream.put('\n');
+        stream->write(message.data(), static_cast<std::streamsize>(message.size()));
+        stream->put('\n');
 
         counter = (counter + 1) % interval;
 
@@ -42,7 +42,7 @@ struct backend_t {
     }
 
     virtual auto flush() -> void {
-        stream.flush();
+        stream->flush();
     }
 };
 
@@ -59,7 +59,7 @@ public:
         data.interval = interval;
     }
 
-    virtual ~inner_t() = default;
+    virtual ~inner_t() {}
 
     virtual auto filename(const record_t&) const -> std::string {
         // TODO: Generate path from tokens, for now just return static path.
