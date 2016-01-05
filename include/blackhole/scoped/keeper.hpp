bf9ea55adf1ba86195da1f5f24c76d819b3e0158
Evgeny Safronov <division494@gmail.com>
2016-01-05 18:17:24 +0300

    refactor(scoped): rule of zero

--- include/blackhole/scoped/keeper.hpp
+++ include/blackhole/scoped/keeper.hpp
@@ -20,20 +20,6 @@ public:
     /// \note creating multiple scoped guards results in attributes stacking.
     keeper_t(logger_t& logger, attributes_t attributes);
 
-    // TODO: Try to delete these. Rule of zero.
-    /// Copying scoped guards is deliberately prohibited.
-    keeper_t(const keeper_t& other) = delete;
-
-    /// Move constructor is left default for enabling copy elision. It never takes place in fact.
-    ///
-    /// \warning you should never move scoped guard instances manually, otherwise the behavior is
-    ///     undefined.
-    keeper_t(keeper_t&& other) = default;
-
-    /// Assignment is deliberately prohibited.
-    auto operator=(const keeper_t& other) -> keeper_t& = delete;
-    auto operator=(keeper_t&& other) -> keeper_t& = delete;
-
     /// Returns an immutable reference to the internal attribute list.
     auto attributes() const -> const attribute_list&;
 };
