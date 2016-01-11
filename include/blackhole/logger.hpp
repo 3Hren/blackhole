38a78daa7250bce3d119c4aa1d587e214cfcf2e6
Evgeny Safronov <division494@gmail.com>
2016-01-11 22:21:35 +0300

    docs(logger): update

--- include/blackhole/logger.hpp
+++ include/blackhole/logger.hpp
@@ -15,7 +15,19 @@ inline namespace v1 {
 class scoped_t;
 
 struct lazy_message_t {
+    /// Initial unformatted message pattern.
     string_view pattern;
+
+    /// Message supplier callback, that should be called only when the logging event passes
+    /// filtering to obtain the final formatted message.
+    ///
+    /// Sometimes obtaining a logging message requires heavyweight operation to be performed and
+    /// there is no guarantee that the result of this operation won't be immediately dropped because
+    /// of failed filtering for example. For these cases we allow the client code to provide
+    /// messages lazily.
+    /// Note, that string view semantics requires the real message storage that is pointed by view
+    /// to outlive the function call. Default implementation in the facade just allocates large
+    /// buffer on stack and fills it on function invocation.
     std::function<auto() -> string_view> supplier;
 };
 
@@ -43,18 +55,6 @@ struct lazy_message_t {
 class logger_t {
 public:
     typedef int severity_t;
-
-    /// Message supplier callback, that should be called only when the logging event passes
-    /// filtering to obtain the final formatted message.
-    ///
-    /// Sometimes obtaining a logging message requires heavyweight operation to be performed and
-    /// there is no guarantee that the result of this operation won't be immediately dropped because
-    /// of failed filtering for example. For these cases we allow the client code to provide
-    /// messages lazily.
-    /// Note, that string view semantics requires the real message storage that is pointed by view
-    /// to outlive the function call. Default implementation in the facade just allocates large
-    /// buffer on stack and fills it on function invocation.
-    typedef std::function<auto() -> string_view> supplier_t;
 
 public:
     virtual ~logger_t() = 0;
