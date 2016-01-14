03ec03f465a9c247b6c9e8f49b8bffc60789e8b3
Evgeny Safronov <division494@gmail.com>
2016-01-14 18:58:41 +0300

    feat(logger): add typedef for message

--- include/blackhole/logger.hpp
+++ include/blackhole/logger.hpp
@@ -1,9 +1,7 @@
 #pragma once
 
-#include <functional>
-
 #include "blackhole/attributes.hpp"
-#include "blackhole/cpp17/string_view.hpp"
+#include "blackhole/message.hpp"
 #include "blackhole/severity.hpp"
 
 namespace blackhole {
@@ -18,25 +16,6 @@ class manager_t;
 
 namespace blackhole {
 inline namespace v1 {
-
-// TODO: Implement message_t as typedef over string_view.
-// TODO: Move into message.hpp with <functional>.
-struct lazy_message_t {
-    /// Initial unformatted message pattern.
-    string_view pattern;
-
-    /// Message supplier callback, that should be called only when the logging event passes
-    /// filtering to obtain the final formatted message.
-    ///
-    /// Sometimes obtaining a logging message requires heavyweight operation to be performed and
-    /// there is no guarantee that the result of this operation won't be immediately dropped because
-    /// of failed filtering for example. For these cases we allow the client code to provide
-    /// messages lazily.
-    /// Note, that string view semantics requires the real message storage that is pointed by view
-    /// to outlive the function call. Default implementation in the facade just allocates large
-    /// buffer on stack and fills it on function invocation.
-    std::function<auto() -> string_view> supplier;
-};
 
 /// Represents the common logging interface in the library.
 ///
@@ -63,10 +42,10 @@ public:
     virtual ~logger_t() = 0;
 
     /// Logs the given message with the specified severity level.
-    virtual auto log(severity_t severity, const string_view& message) -> void = 0;
+    virtual auto log(severity_t severity, const message_t& message) -> void = 0;
 
     /// Logs the given message with the specified severity level and attributes pack attached.
-    virtual auto log(severity_t severity, const string_view& message, attribute_pack& pack) -> void = 0;
+    virtual auto log(severity_t severity, const message_t& message, attribute_pack& pack) -> void = 0;
 
     /// Logs the given message with the specified severity level, attributes pack attached and with
     /// special message supplier callback.
---
+++ include/blackhole/message.hpp
@@ -0,0 +1,32 @@
+#pragma once
+
+#include <functional>
+
+#include "blackhole/cpp17/string_view.hpp"
+
+namespace blackhole {
+inline namespace v1 {
+
+/// Represents a message that can be logged.
+typedef string_view message_t;
+
+/// Represents a message that can be logged and can be instantiated lazily on demand.
+struct lazy_message_t {
+    /// Initial unformatted message pattern.
+    string_view pattern;
+
+    /// Message supplier callback, that should be called only when the logging event passes
+    /// filtering to obtain the final formatted message.
+    ///
+    /// Sometimes obtaining a logging message requires heavyweight operation to be performed and
+    /// there is no guarantee that the result of this operation won't be immediately dropped because
+    /// of failed filtering for example. For these cases we allow the client code to provide
+    /// messages lazily.
+    /// Note, that string view semantics requires the real message storage that is pointed by view
+    /// to outlive the function call. Default implementation in the facade just allocates large
+    /// buffer on stack and fills it on function invocation.
+    std::function<auto() -> string_view> supplier;
+};
+
+}  // namespace v1
+}  // namespace blackhole
