cdd07ba2d423950bd7e54b1469d01973a779b2cd
Evgeny Safronov <division494@gmail.com>
2016-02-18 10:46:56 +0300

    feat(bench): measure different values
    This benchmark proves that there is no difference between the
    underlying type for attributes conversion. Therefore, there is no
    performance penalty.

--- bench/attribute.cpp
+++ bench/attribute.cpp
@@ -27,8 +27,20 @@ static void from_owned(::benchmark::State& state) {
     state.SetItemsProcessed(state.iterations());
 }
 
+static void from_owned_string(::benchmark::State& state) {
+    blackhole::attribute::value_t owned("le message");
+
+    while (state.KeepRunning()) {
+        blackhole::attribute::view_t view(owned);
+        ::benchmark::DoNotOptimize(view);
+    }
+
+    state.SetItemsProcessed(state.iterations());
+}
+
 NBENCHMARK("attribute.view_t[ctor + get<i64>]", view_ctor_get_int64);
 NBENCHMARK("attribute.view_t[ctor + from owned]", from_owned);
+NBENCHMARK("attribute.view_t[ctor + from owned string]", from_owned_string);
 
 }  // namespace benchmark
 }  // namespace blackhole
