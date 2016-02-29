ffe8d7a5889b2db9f31b8e1891509de3dd7093ad
Evgeny Safronov <division494@gmail.com>
2016-02-29 21:22:06 +0300

    bench(owned): measure owned creation

---
+++ bench/record.owned.cpp
@@ -0,0 +1,29 @@
+#include <benchmark/benchmark.h>
+
+#include <blackhole/attribute.hpp>
+#include <blackhole/record.hpp>
+#include <blackhole/detail/record.owned.hpp>
+
+#include "mod.hpp"
+
+namespace blackhole {
+namespace benchmark {
+
+static void record(::benchmark::State& state) {
+    const string_view message("GET /porn.png HTTP/1.1");
+    const attribute_list attributes{{"key#1", "value#1"}};
+    const attribute_pack pack{attributes};
+
+    record_t record(42, message, pack);
+
+    while (state.KeepRunning()) {
+        detail::owned<record_t> owned(record);
+    }
+
+    state.SetItemsProcessed(state.iterations());
+}
+
+NBENCHMARK("record[into_owned]", record);
+
+}  // namespace benchmark
+}  // namespace blackhole
