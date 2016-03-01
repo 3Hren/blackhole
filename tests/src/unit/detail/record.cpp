dba7c73770615cf9abd50c320def24e3d7c00aaa
Evgeny Safronov <division494@gmail.com>
2016-03-01 16:28:12 +0300

    fix(owned): implement assignment

--- tests/src/unit/detail/record.cpp
+++ tests/src/unit/detail/record.cpp
@@ -128,6 +128,37 @@ TEST(owned, MoveConstructor) {
     EXPECT_EQ(attributes, result->into_view().attributes().at(0).get());
 }
 
+TEST(owned, MoveAssignment) {
+    std::unique_ptr<owned<record_t>> result;
+
+    {
+        const string_view message("GET");
+        const attribute_list attributes{{"key#1", "value#1"}};
+        const attribute_pack pack{attributes};
+        const record_t record(42, message, pack);
+
+        result.reset(new owned<record_t>(record));
+    }
+
+    {
+        const string_view message("POST");
+        const attribute_list attributes{{"key#2", "value#2"}};
+        const attribute_pack pack{attributes};
+        const record_t record(10, message, pack);
+
+        owned<record_t> own(record);
+
+        *result = std::move(own);
+    }
+
+    EXPECT_EQ(string_view("POST"), result->into_view().message());
+    EXPECT_EQ(10, result->into_view().severity());
+
+    const attribute_list attributes{{"key#2", "value#2"}};
+    ASSERT_EQ(1, result->into_view().attributes().size());
+    EXPECT_EQ(attributes, result->into_view().attributes().at(0).get());
+}
+
 }  // namespace
 }  // namespace detail
 }  // namespace v1
