cd803675e2cc482c315047f4d8182538af41caf8
Evgeny Safronov <division494@gmail.com>
2016-03-05 01:46:58 +0300

    feat(win32): make procname cross-platform

--- src/procname.cpp
+++ src/procname.cpp
@@ -1,12 +1,13 @@
 #include "blackhole/detail/procname.hpp"
 
-#ifdef __linux__
+#if defined(__linux__)
 #   include <sys/types.h>
-#elif __APPLE__
+#	include <unistd.h>
+#elif defined(__APPLE__)
 #   include <libproc.h>
+#	include <unistd.h>
+#elif defined(_WIN32)
 #endif
-
-#include <unistd.h>
 
 #include <cstring>
 
@@ -16,28 +17,34 @@ namespace blackhole {
 inline namespace v1 {
 namespace detail {
 
+using cpp17::string_view;
+
 namespace {
 
-auto procname(pid_t pid) -> cpp17::string_view {
-#ifdef __linux__
-    (void)pid;
-    return cpp17::string_view(program_invocation_short_name, ::strlen(program_invocation_short_name));
-#elif __APPLE__
-    static char path[PROC_PIDPATHINFO_MAXSIZE];
+auto __procname() -> string_view {
+#if defined(__linux__)
+	return string_view(program_invocation_short_name, ::strlen(program_invocation_short_name));
+#elif defined(__APPLE__)
+	const auto pid = ::getpid();
 
-    if (::proc_name(pid, path, sizeof(path)) > 0) {
-        return cpp17::string_view(path, ::strlen(path));
-    } else {
-        const auto nwritten = ::snprintf(path, sizeof(path), "%d", pid);
-        return cpp17::string_view(path, static_cast<std::size_t>(nwritten));
-    }
+	static char path[PROC_PIDPATHINFO_MAXSIZE];
+
+	if (::proc_name(pid, path, sizeof(path)) > 0) {
+		return string_view(path, ::strlen(path));
+	} else {
+		const auto nwritten = ::snprintf(path, sizeof(path), "%d", pid);
+		return string_view(path, static_cast<std::size_t>(nwritten));
+	}
+#elif defined(_WIN32)
+	// TODO: Implement.
+	return "";
 #endif
 }
 
 }  // namespace
 
-auto procname() -> cpp17::string_view {
-    static const cpp17::string_view name = procname(::getpid());
+auto procname() -> string_view {
+    static const string_view name = __procname();
     return name;
 }
 
