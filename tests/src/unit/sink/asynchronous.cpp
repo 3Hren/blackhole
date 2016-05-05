1351bdc5cac3a32019e0ac2196d17354a2e779d3
Evgeny Safronov <division494@gmail.com>
2016-05-05 19:50:03 +0300

    feat(tests): extend asynchronous sink test

--- tests/src/unit/sink/asynchronous.cpp
+++ tests/src/unit/sink/asynchronous.cpp
@@ -144,6 +144,7 @@ namespace sink {
 namespace {
 
 using ::testing::_;
+using ::testing::Invoke;
 
 namespace mock = testing::mock;
 
@@ -151,12 +152,17 @@ TEST(asynchronous_t, DelegatesEmit) {
     std::unique_ptr<mock::sink_t> wrapped(new mock::sink_t);
 
     EXPECT_CALL(*wrapped, emit(_, string_view("formatted message")))
-        .Times(1);
+        .Times(1)
+        .WillOnce(Invoke([](const record_t& record, string_view) {
+            ASSERT_EQ(1, record.attributes().size());
+            EXPECT_EQ((attribute_list{{"key#1", {"value#1"}}}), record.attributes().at(0).get());
+        }));
 
     asynchronous_t sink(std::move(wrapped));
 
-    const string_view message("");
-    const attribute_pack pack;
+    const string_view message("unformatted message");
+    const attribute_list attributes{{"key#1", {"value#1"}}};
+    const attribute_pack pack({attributes});
     record_t record(42, message, pack);
 
     sink.emit(record, "formatted message");
