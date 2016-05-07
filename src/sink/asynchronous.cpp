40702cbf5ca19193f61fc1e1c96a0978ef512262
Evgeny Safronov <division494@gmail.com>
2016-05-07 23:52:34 +0300

    feat: factory for asynchronous sink

--- src/sink/asynchronous.cpp
+++ src/sink/asynchronous.cpp
@@ -1,4 +1,11 @@
 #include "blackhole/sink/asynchronous.hpp"
+
+#include <boost/optional/optional.hpp>
+
+#include "blackhole/config/node.hpp"
+#include "blackhole/config/option.hpp"
+#include "blackhole/registry.hpp"
+
 #include "blackhole/detail/sink/asynchronous.hpp"
 
 #include <cmath>
@@ -120,7 +127,8 @@ auto factory<sink::asynchronous_t>::type() const noexcept -> const char* {
 }
 
 auto factory<sink::asynchronous_t>::from(const config::node_t& config) const -> std::unique_ptr<sink_t> {
-
+    auto factory = registry.sink(*config["sink"]["type"].to_string());
+    return std::unique_ptr<sink_t>(new sink::asynchronous_t(factory(*config["sink"].unwrap())));
 }
 
 }  // namespace experimental
