3b4ce3f472ff08a826ed3f1481403f29393c45f8
Evgeny Safronov <division494@gmail.com>
2016-07-13 14:59:28 +0300

    docs: add an example, more docs

--- examples/4.config_facade.cpp
+++ examples/4.config_facade.cpp
@@ -1,11 +1,15 @@
 /// This example demonstrates how to initialize Blackhole from configuration file using JSON
 /// builder.
 /// In this case the entire logging pipeline is initialized from file including severity mapping.
+/// The logging facade is used to allow runtime formatting and attributes provisioning.
 
 #include <fstream>
 #include <iostream>
 
+#include <blackhole/attribute.hpp>
+#include <blackhole/attributes.hpp>
 #include <blackhole/config/json.hpp>
+#include <blackhole/extensions/facade.hpp>
 #include <blackhole/extensions/writer.hpp>
 #include <blackhole/registry.hpp>
 #include <blackhole/root.hpp>
@@ -29,17 +33,26 @@ auto main(int argc, char** argv) -> int {
     /// Here we are going to build the logger using registry. The registry's responsibility is to
     /// track registered handlers, formatter and sinks, but for now we're not going to register
     /// anything else, since there are predefined types.
-    auto log = blackhole::registry::configured()
+    auto inner = blackhole::registry::configured()
         /// Specify the concrete builder type we want to use. It may be JSON, XML, YAML or whatever
         /// else.
         ->builder<blackhole::config::json_t>(std::ifstream(argv[1]))
             /// Build the logger named "root".
             .build("root");
 
-    log.log(severity::debug, "GET /static/image.png HTTP/1.1 404 347");
-    log.log(severity::info, "nginx/1.6 configured");
+    /// Wrap the logger with facade to obtain an ability to format messages and provide attributes.
+    auto log = blackhole::logger_facade<blackhole::root_logger_t>(inner);
+
+    log.log(severity::debug, "{} {} HTTP/1.1 {} {}", "GET", "/static/image.png", 404, 347);
+    log.log(severity::info, "nginx/1.6 configured", {
+        {"elapsed", 32.5}
+    });
     log.log(severity::warning, "client stopped connection before send body completed");
-    log.log(severity::error, "file does not exist: /var/www/favicon.ico");
+    log.log(severity::error, "file does not exist: {}", "/var/www/favicon.ico", blackhole::attribute_list{
+        {"Cache", true},
+        {"Cache-Duration", 10},
+        {"User-Agent", "Mozilla Firefox"}
+    });
 
     return 0;
 }
