7ac28895dd72213d0987ee5b329418bbb7dfd958
Evgeniy Polyakov <zbr@ioremap.net>
2017-02-02 16:41:22 +0300

    types: include <math.h> header to resolve std::double_t issues

--- src/blackhole/utils/types.hpp
+++ src/blackhole/utils/types.hpp
@@ -1,6 +1,11 @@
 #pragma once
 
-#if defined(__clang__) || defined(GCC47)
+#if defined(__clang__) || defined(GCC47) || __GNUC__ >= 5
+# if __GNUC__ >= 5
+// it is needed to resolve <cmath> header which requires std::double_t
+#include <math.h>
+# endif
+
 #else
 namespace std {
 
