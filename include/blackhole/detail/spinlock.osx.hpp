e404e1c03b406e1c9c0e97a56eab0f906decf4a4
Evgeny Safronov <division494@gmail.com>
2017-03-12 13:36:11 +0300

    feat: replace deprecated OSX stuff with modern one

--- include/blackhole/detail/spinlock.osx.hpp
+++ include/blackhole/detail/spinlock.osx.hpp
@@ -1,29 +1,29 @@
 #pragma once
 
-#include <libkern/OSAtomic.h>
+#include <os/lock.h>
 
 namespace blackhole {
 inline namespace v1 {
 namespace detail {
 
 class spinlock_t {
-    OSSpinLock mutex;
+    os_unfair_lock mutex;
 
 public:
     constexpr spinlock_t() noexcept:
-        mutex(0)
+        mutex({0})
     {}
 
     auto lock() noexcept -> void {
-        OSSpinLockLock(&mutex);
+        os_unfair_lock_lock(&mutex);
     }
 
     auto unlock() noexcept -> void {
-        OSSpinLockUnlock(&mutex);
+        os_unfair_lock_unlock(&mutex);
     }
 
     auto trylock() noexcept -> bool {
-        return OSSpinLockTry(&mutex);
+        return os_unfair_lock_trylock(&mutex);
     }
 };
 
